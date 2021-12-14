ffd0a7e80b32f91124c279d3a5526f9575407dbb
Tautvydas Cerniauskas <tautvydas.cerniauskas@hmcts.net>
2021-12-14 12:32:26 +0000

    fix: removed onboarding page and added sample home page

--- src/main/controllers/OnboardingController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-export default class OnboardingController {
-  public get(req: Request, res: Response): void {
-    res.render('onboarding', {
-      ...(req.t('onboarding', { returnObjects: true })),
-    });
-  }
-}
--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -4,13 +4,13 @@ import { infoRequestHandler } from '@hmcts/info-provider';
 const healthcheck = require('@hmcts/nodejs-healthcheck');
 
 export default function (app: Application): void {
-  app.get('/', app.locals.container.cradle.onboardingController.get);
+  app.get('/', app.locals.container.cradle.homeController.get);
   app.get(
     '/info',
     infoRequestHandler({
       extraBuildInfo: {
         host: os.hostname(),
-        name: 'ET-SYA-frontend',
+        name: 'et-sya-frontend',
         uptime: process.uptime(),
       },
       info: {
