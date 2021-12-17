546057d501ee9db4516322be55ffa4c027b2d92e
Brendan McHugh <brendan.mchugh@hmcts.net>
2021-12-17 12:34:22 +0000

    add controllers, views and unit test

---
+++ src/main/controllers/LipOrRepController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+
+export default class LipOrRepController {
+
+  public get(req: Request, res: Response): void {
+    res.render('lip-or-representative', {
+      ...(req.t('lip-or-representative', { returnObjects: true })),
+    });
+  }
+
+  public post(req: Request, res: Response): void {
+    if (req.body['lip-or-representative'] === 'lip') {
+      res.redirect('/single-or-multiple-claim');
+    }
+    else if (req.body['lip-or-representative'] === 'representative') {
+      // redirect to legacy ET1 service
+      const URL = 'https://employmenttribunals.service.gov.uk/apply';
+      res.redirect(URL);
+    } else {
+      const errorMessage = 'Select if you are representing yourself or if you are a representative making a claim for someone else.'
+      res.render('lip-or-representative', {
+        errorMessage,
+        ...(req.t('lip-or-representative', { returnObjects: true })),
+      });
+    }
+  }
+}
---
+++ src/test/routes/lip-or-representative.ts
@@ -0,0 +1,38 @@
+import { expect } from 'chai';
+import request from 'supertest';
+
+import { app } from '../../main/app';
+
+const expectedLegacyURL = 'https://employmenttribunals.service.gov.uk/apply';
+
+describe("GET /lip-or-representative", () => {
+  it("should return the lip or representative page", async () => {
+    const res = await request(app).get("/lip-or-representative");
+    expect(res.type).to.equal('text/html');
+    expect(res.status).to.equal(200);
+  });
+});
+
+describe('on POST /lip-or-representative', () => {
+  test('should return the Single or Multiple claims page when \'representing myself\' is selected', async () => {
+    await request(app)
+      .post('/lip-or-representative')
+      .send({'lip-or-representative': 'lip'})
+      .expect((res) => {
+        expect(res.status).to.equal(302);
+        expect(res.header['location']).to.equal('/single-or-multiple-claim');
+      });
+  });
+});
+
+describe('on POST /lip-or-representative', () => {
+  test('should return the legacy ET1 service when the \'making a claim for someone else\' option is selected', async () => {
+    await request(app)
+      .post('/lip-or-representative')
+      .send({'lip-or-representative': 'representative'})
+      .expect((res) => {
+        expect(res.status).to.equal(302);
+        expect(res.header['location']).to.equal(expectedLegacyURL);
+      });
+  });
+});
