5043d22dd36176dc23260a5674816050052851d3
Brendan McHugh <brendan.mchugh@hmcts.net>
2022-01-17 16:07:22 +0000

    fix: add lines removed by merge, add missing webpack file, rename route test files

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -11,6 +11,7 @@ export default function (app: Application): void {
   app.get('/lip-or-representative', app.locals.container.cradle.lipOrRepController.get);
   app.post('/lip-or-representative', app.locals.container.cradle.lipOrRepController.post);
   app.get('/single-or-multiple-claim', app.locals.container.cradle.singleOrMultipleController.get);
+  app.post('/single-or-multiple-claim', app.locals.container.cradle.singleOrMultipleController.post);
   
   app.get(
     '/info',
