34fd01a9183ff98563b96729844a603bec43659f
lvo700 <lynnette.orr@HMCTS.net>
2022-01-17 11:23:46 +0000

    Merged if statements in UpdatePreferenceController Moved content to translation files

--- src/main/controllers/UpdatePreferenceController.ts
+++ src/main/controllers/UpdatePreferenceController.ts
@@ -9,22 +9,22 @@ export default class UpdatePreferenceController {
   }
 
   public post(req: Request, res: Response): void {
-    if (req.body['update-preference'] === 'email' && req.body['saveButton'] === 'saveContinue') {
-      res.redirect('/would-you-want-to-take-part-in-video-hearings');
+
+    const updatePref = () => {
+      if ((req.body['update-preference'] === 'email' || req.body['update-preference'] === 'post') && req.body['saveButton'] === 'saveContinue') {
+        res.redirect('/would-you-want-to-take-part-in-video-hearings');
+      }
+      else if ((req.body['update-preference'] === 'email' || req.body['update-preference'] === 'post') && req.body['saveButton'] === 'saveForLater') {
+        res.redirect('/your-claim-has-been-saved');
+      }
+      else {
+        res.render('update-preference', {
+          noRadioButtonSelectedError: true,
+          ...(req.t('update-preference', { returnObjects: true })),
+        });
+      }
     }
-    else if (req.body['update-preference'] === 'post' && req.body['saveButton'] === 'saveContinue') {
-      res.redirect('/would-you-want-to-take-part-in-video-hearings');
-    }
-    else if (req.body['update-preference'] === 'email' && req.body['saveButton'] === 'saveForLater') {
-      res.redirect('/your-claim-has-been-saved');
-    }
-    else if (req.body['update-preference'] === 'post' && req.body['saveButton'] === 'saveForLater') {
-      res.redirect('/your-claim-has-been-saved');
-    } else {
-      res.render('update-preference', {
-        noRadioButtonSelectedError: true,
-        ...(req.t('update-preference', { returnObjects: true })),
-      });
-    }
+    updatePref();
   }
+
 }
