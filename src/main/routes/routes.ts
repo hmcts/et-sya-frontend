07c4ec64ba7b42d048673ff30914c7a0b361dd9f
lvo700 <lynnette.orr@HMCTS.net>
2022-01-19 14:14:27 +0000

    Fixed merge conflict marker error

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -8,15 +8,11 @@ export default function(app: Application): void {
   app.get('/lip-or-representative', app.locals.container.cradle.lipOrRepController.get);
   app.post('/lip-or-representative', app.locals.container.cradle.lipOrRepController.post);
   app.get('/single-or-multiple-claim', app.locals.container.cradle.singleOrMultipleController.get);
-<<<<<<< HEAD
   app.post('/how-would-you-like-to-be-updated-about-your-claim', app.locals.container.cradle.updatePreferenceController.post);
   app.get('/how-would-you-like-to-be-updated-about-your-claim', app.locals.container.cradle.updatePreferenceController.get);
   app.get('/would-you-want-to-take-part-in-video-hearings', app.locals.container.cradle.videoHearingsController.get);
   app.get('/your-claim-has-been-saved', app.locals.container.cradle.claimSavedController.get);
   app.get('/your-contact-details-disability', app.locals.container.cradle.disabilityController.get);
-=======
-  app.post('/single-or-multiple-claim', app.locals.container.cradle.singleOrMultipleController.post);
->>>>>>> master
   
 
   app.get(
