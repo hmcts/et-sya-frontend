2d99528e0c0ffe2b22c58314c943877927f0285c
Jagtar Singh <jagtar.singh@hmcts.net>
2022-01-26 15:05:39 +0000

    [RET-325] Bug fix: Use mockResponse object in controller tests

--- src/test/unit/controller/HomeController.test.ts
+++ src/test/unit/controller/HomeController.test.ts
@@ -1,7 +1,7 @@
 import sinon from 'sinon';
-import { Response } from 'express';
 import HomeController from '../../../main/controllers/HomeController';
 import { mockRequest } from '../mocks/mockRequest';
+import { mockResponse } from '../mocks/mockResponse';
 
 const homeController = new HomeController();
 
@@ -11,8 +11,8 @@ describe('Onboarding Controller', () => {
   };
 
   it('should render the onboarding (home) page', () => {
-    const response = { render: () => '' } as unknown as Response;
-    const request = mockRequest({ t });
+    const response = mockResponse();
+    const request = mockRequest(t);
     const responseMock = sinon.mock(response);
     responseMock.expects('render').once().withArgs('home', request.t('home', { returnObjects: true }));
     homeController.get(request, response);
