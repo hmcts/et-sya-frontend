deceb99c4fd83f779508b554ee6bd58ee8d3290c
Jagtar Singh <jagtar.singh@hmcts.net>
2022-01-26 11:32:35 +0000

    [RET-325] Bug fix: include common JSON template

--- src/main/controllers/ChecklistController.ts
+++ src/main/controllers/ChecklistController.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 export default class ChecklistController {
   public get(req: Request, res: Response): void {
     res.render('checklist', {
+      ...req.t('common', { returnObjects: true }),
       ...(req.t('checklist', { returnObjects: true })),
     });
   }
--- src/main/controllers/HomeController.ts
+++ src/main/controllers/HomeController.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 export default class HomeController {
   public get(req: Request, res: Response): void {
     res.render('home', {
+      ...req.t('common', { returnObjects: true }),
       ...(req.t('home', { returnObjects: true })),
     });
   }
