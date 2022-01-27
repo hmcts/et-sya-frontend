4ac5c847754fa9aadffabfa59654841b070f30c8
Tautvydas Cerniauskas <tautvydas.cerniauskas@hmcts.net>
2022-01-27 11:39:14 +0000

    bug: fixed redirects

--- src/main/controllers/acas_single_claim/AcasSingleClaimController.ts
+++ src/main/controllers/acas_single_claim/AcasSingleClaimController.ts
@@ -20,8 +20,8 @@ export default class AcasSingleClaimController {
 
   public post = (req: AppRequest, res: Response): void => {
     // TODO(Tautvydas): Change to the correct redirect url
-    const redirectUrl = conditionalRedirect(req, this.form.getFormFields(), YesOrNo.YES) ? '/' : '/';
-    
+    const redirectUrl = conditionalRedirect(req, this.form.getFormFields(), YesOrNo.YES) ? '/type-of-claim' : '/';
+  
     setUserCase(req, this.form);
     handleSessionErrors(req, res, this.form, redirectUrl);
   }
