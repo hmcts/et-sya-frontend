70eb3f4f6a1b5bea97fd8f5cc5903c4ca2cfd450
Marc Judge <marc.judge@HMCTS.NET>
2022-02-02 16:02:55 +0000

    Adding past employer form

---
+++ src/main/controllers/past_employer/PastEmployerController.ts
@@ -0,0 +1,30 @@
+import { Response } from 'express';
+import { Form } from '../../components/form/form';
+import { FormContent, FormFields } from '../../definitions/form';
+import { AppRequest } from '../../definitions/appRequest';
+import { assignFormData, getPageContent, handleSessionErrors, setUserCase } from '../helpers';
+
+
+export default class pastEmployerController {
+  private readonly form: Form;
+
+  constructor(private readonly pastEmployerFormContent: FormContent) {
+    this.form = new Form(<FormFields>this.pastEmployerFormContent.fields);
+  }
+
+  public post = (req: AppRequest, res: Response): void => {
+    setUserCase(req, this.form);
+    handleSessionErrors(req, res, this.form, '/');
+  };
+
+  public get = (req: AppRequest, res: Response): void => {
+    const content = getPageContent(req, this.pastEmployerFormContent, [
+      'common',
+      'past-employer',
+    ]);
+    assignFormData(req.session.userCase, this.form.getFormFields());
+    res.render('past-employer', {
+      ...content,
+    });
+  };
+}
---
+++ src/main/controllers/past_employer/content.ts
@@ -0,0 +1,38 @@
+import { FormContent } from '../../definitions/form';
+import { AnyRecord } from '../../definitions/util-types';
+import { isFieldFilledIn } from '../../components/form/validator';
+import { YesOrNo } from 'definitions/case';
+
+export const pastEmployerFormContent: FormContent = {
+  fields: {
+    pastEmployer: {
+      classes: 'govuk-radios--inline',
+      id: 'past-employer',
+      type: 'radios',
+      label: (l: AnyRecord): string => l.label,
+      values: [
+        {
+          label: (l: AnyRecord): string => l.yes,
+          name: 'radio1',
+          value: YesOrNo.YES,
+          attributes: { maxLength: 2 },
+        },
+        {
+          label: (l: AnyRecord): string => l.no,
+          name: 'radio2',
+          value: YesOrNo.NO,
+          attributes: { maxLength: 2 },
+        },
+      ],
+      validator: isFieldFilledIn,
+    },
+  },
+  submit: {
+    text: (l: AnyRecord): string => l.submit,
+    classes: 'govuk-!-margin-right-2',
+  },
+  saveForLater: {
+    text: (l: AnyRecord): string => l.saveForLater,
+    classes: 'govuk-button--secondary',
+  },
+}; 
