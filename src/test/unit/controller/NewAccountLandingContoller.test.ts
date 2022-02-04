397f9de8e26c85560fa50e953dd29e3abbae7ffb
marcjudge <98343606+marcjudge@users.noreply.github.com>
2022-02-04 09:14:37 +0000

    Ret 793 (#56)
    * update nodemon
    
    * adding new account landing page
    
    * update package.json dependencies
    
    * add landing page without form
    
    * adding new account landing page with tests
    
    * add new account landing controller
    
    * update interruption panel scss
    
    Co-authored-by: root <root@LAPTOP2358.V1.com>

---
+++ src/test/unit/controller/NewAccountLandingContoller.test.ts
@@ -0,0 +1,21 @@
+import sinon from 'sinon';
+import NewAccountLandingController from '../../../main/controllers/NewAccountLanding/NewAccountLandingController';
+import { mockRequest } from '../mocks/mockRequest';
+import { mockResponse } from '../mocks/mockResponse';
+
+const newAccountLandingController = new NewAccountLandingController();
+
+describe('New Account Landing Controller', () => {
+  const t = {
+    'new-account-landing': {},
+  };
+
+  it('should render the new account landing page', () => {
+    const response = mockResponse();
+    const request = mockRequest(t);
+    const responseMock = sinon.mock(response);
+    responseMock.expects('render').once().withArgs('new-account-landing', request.t('new-account-landing', { returnObjects: true }));
+    newAccountLandingController.get(request, response);
+    responseMock.verify();
+  });
+});
