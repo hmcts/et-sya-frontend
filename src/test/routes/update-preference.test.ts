cb5a0ee23dc5feaee4bdd5451663fa2d924ab557
lvo700 <lynnette.orr@HMCTS.net>
2022-02-08 16:38:45 +0000

    Amended files failing test and lint

--- src/test/routes/update-preference.test.ts
+++ src/test/routes/update-preference.test.ts
@@ -1,4 +1,3 @@
-import { expect } from 'chai';
 import request from 'supertest';
 
 import { app } from '../../main/app';
@@ -6,8 +5,8 @@ import { app } from '../../main/app';
 describe('GET /how-would-you-like-to-be-updated-about-your-claim', () => {
   it('should return the how would you like to be updated about your claim page', async () => {
     const res = await request(app).get('/how-would-you-like-to-be-updated-about-your-claim');
-    expect(res.type).to.equal('text/html');
-    expect(res.status).to.equal(200);
+    expect(res.type).toStrictEqual('text/html');
+    expect(res.status).toStrictEqual(200);
   });
 });
 
@@ -17,20 +16,18 @@ describe('on POST /how-would-you-like-to-be-updated-about-your-claim', () => {
       .post('/how-would-you-like-to-be-updated-about-your-claim')
       .send({ 'update-preference': 'Email' })
       .expect(res => {
-        expect(res.status).to.equal(302);
-        expect(res.header['location']).to.equal('/how-would-you-like-to-be-updated-about-your-claim');
+        expect(res.status).toStrictEqual(302);
+        expect(res.header['location']).toStrictEqual('/how-would-you-like-to-be-updated-about-your-claim');
       });
   });
 });
 
-describe('on POST /how-would-you-like-to-be-updated-about-your-claim', () => {
-  test('should reload the current page when the Post radio button is selected', async () => {
-    await request(app)
-      .post('/how-would-you-like-to-be-updated-about-your-claim')
-      .send({ 'acas-multiple': 'Post' })
-      .expect(res => {
-        expect(res.status).to.equal(302);
-        expect(res.header['location']).to.equal('/how-would-you-like-to-be-updated-about-your-claim');
-      });
-  });
+test('should reload the current page when the Post radio button is selected', async () => {
+  await request(app)
+    .post('/how-would-you-like-to-be-updated-about-your-claim')
+    .send({ 'update-preference': 'Post' })
+    .expect(res => {
+      expect(res.status).toStrictEqual(302);
+      expect(res.header['location']).toStrictEqual('/how-would-you-like-to-be-updated-about-your-claim');
+    });
 });
