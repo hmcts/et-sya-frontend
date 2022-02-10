222bf03768da9ad3f542c907e937fed230e0a769
Marc Judge <marc.judge@HMCTS.NET>
2022-02-10 17:18:09 +0000

    Fix Sonarcloud issues

--- src/main/controllers/past_employer/content.ts
+++ src/main/controllers/past_employer/content.ts
@@ -1,38 +1,14 @@
-import { isFieldFilledIn } from '../../components/form/validator';
-import { YesOrNo } from '../../definitions/case';
+import { DefaultRadioFormFields, RadioFormFields, saveForLaterButton, submitButton } from '../../definitions/radios';
 import { FormContent } from '../../definitions/form';
-import { AnyRecord } from '../../definitions/util-types';
+
+const past_employer: RadioFormFields = {
+  ...DefaultRadioFormFields,
+  id: 'past-employer',
+  classes: 'govuk-radios--inline',
+};
 
 export const pastEmployerFormContent: FormContent = {
-  fields: {
-    pastEmployer: {
-      classes: 'govuk-radios--inline',
-      id: 'past-employer',
-      type: 'radios',
-      label: (l: AnyRecord): string => l.label,
-      values: [
-        {
-          label: (l: AnyRecord): string => l.yes,
-          name: 'radio1',
-          value: YesOrNo.YES,
-          attributes: { maxLength: 2 },
-        },
-        {
-          label: (l: AnyRecord): string => l.no,
-          name: 'radio2',
-          value: YesOrNo.NO,
-          attributes: { maxLength: 2 },
-        },
-      ],
-      validator: isFieldFilledIn,
-    },
-  },
-  submit: {
-    text: (l: AnyRecord): string => l.submit,
-    classes: 'govuk-!-margin-right-2',
-  },
-  saveForLater: {
-    text: (l: AnyRecord): string => l.saveForLater,
-    classes: 'govuk-button--secondary',
-  },
+  fields: { pastEmployer: past_employer  },
+  submit: submitButton,
+  saveForLater: saveForLaterButton,
 };
