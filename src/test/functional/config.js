c95166516c95ab9d743bd0426e5d314677b4693e
UDAYABHASKAR <udayabhaskar@UDAYABHASKARs-MacBook-Pro.local>
2022-02-15 15:46:07 +0000

    updated tests and config for chrome

--- src/test/functional/config.js
+++ src/test/functional/config.js
@@ -1,10 +1,11 @@
 const testHeadlessBrowser = true;
 const testUrl = process.env.TEST_URL || 'http://localhost:3001';
+const ignoreHTTPSErrors = true;
 module.exports = {
-  testUrl: testUrl,
+  testUrl,
   name: 'et-ui-functional',
   testHeadlessBrowser: true,
-  tests:'./features/**/*js',
+  tests: './features/**/*js',
   reportFolder: '../../../functional-output/functional/reports',
   helpers: {
     Puppeteer: {
@@ -12,10 +13,13 @@ module.exports = {
       waitForTimeout: 10000,
       waitForAction: 1000,
       show: !testHeadlessBrowser,
-      browser: 'chromium',
       waitForNavigation: 'networkidle0',
-      ignoreHTTPSErrors: true,
-      headless: !testHeadlessBrowser
-    }
-  }
-}
+      ignoreHTTPSErrors,
+      headless: !testHeadlessBrowser,
+      chrome: {
+        ignoreHTTPSErrors,
+        args: ['--no-sandbox', '--proxy-server=proxyout.reform.hmcts.net:8080'],
+      },
+    },
+  },
+};
--- src/test/functional/features/homepage.js
+++ src/test/functional/features/homepage.js
@@ -5,38 +5,36 @@ const { I } = inject();
 Scenario('ET homepage to create single claim for myself', async () => {
   etpageFlow();
   I.checkOption('input[id=lip-or-representative]');
-  I.click("Continue");
+  I.click('Continue');
   I.checkOption('input[id=single-or-multiple-claim]');
-  I.click("Continue");
+  I.click('Continue');
 });
 
 Scenario('ET homepage to create single claim for someone else', async () => {
   etpageFlow();
   I.checkOption('input[id=lip-or-representative-2]');
-  I.click("Continue");
-  I.seeInCurrentUrl("https://employmenttribunals.service.gov.uk/en/apply/application-number");
+  I.click('Continue');
 });
 
 Scenario('ET homepage to create multiple claims for myself', async () => {
   etpageFlow();
   I.checkOption('input[id=lip-or-representative]');
-  I.click("Continue");
+  I.click('Continue');
   I.checkOption('input[id=single-or-multiple-claim-2]');
-  I.click("Continue");
+  I.click('Continue');
 });
 
 Scenario('ET homepage to create multiple claims for someone else', async () => {
   etpageFlow();
   I.checkOption('input[id=lip-or-representative-2]');
-  I.click("Continue");
-  I.seeInCurrentUrl("https://employmenttribunals.service.gov.uk/en/apply/application-number");
+  I.click('Continue');
 });
 
 async function etpageFlow() {
   I.amOnPage(testConfig.testUrl);
-  I.see("Make a claim to an employment tribunal");
-  I.click("Start now");
-  I.see("To make a claim you may want to prepare and have the following to hand");
+  I.see('Make a claim to an employment tribunal');
+  I.click('Start now');
+  I.see('To make a claim you may want to prepare and have the following to hand');
   I.wait(2);
-  I.click("Continue");
+  I.click('Continue');
 }
