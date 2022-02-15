2311af0235257411229108dcc21fe38aaf22a621
MatthewDalbyV1 <matthew.dalby@version1.com>
2022-02-15 09:43:40 +0000

    prettier linting fixes

--- src/main/controllers/still_working/content.ts
+++ src/main/controllers/still_working/content.ts
@@ -3,39 +3,37 @@ import { FormContent } from '../../definitions/form';
 import { AnyRecord } from '../../definitions/util-types';
 
 export const stillWorkingContent: FormContent = {
-    fields: {
-        isStillWorking: {
-            classes: 'govuk-radios--inline',
-            id: 'still-working',
-            type: 'radios',
-            label: (l: AnyRecord): string => l.label,
-            values: [
-                {
-                    name: 'working',
-                    label: (l: AnyRecord): string => l.optionText1,
-                    value: 1,
-                    selected: false,
-                },
-                {
-                    name: 'working_notice',
-                    label: (l: AnyRecord): string => l.optionText2,
-                    value: 2,
-                    selected: false,
-                },
-                {
-                    name: 'not_working',
-                    label: (l: AnyRecord): string => l.optionText3,
-                    value: 3,
-                    selected: false,
-                },
-            ],
-            validator: isFieldFilledIn,
+  fields: {
+    isStillWorking: {
+      classes: 'govuk-radios--inline',
+      id: 'still-working',
+      type: 'radios',
+      label: (l: AnyRecord): string => l.label,
+      values: [
+        {
+          name: 'working',
+          label: (l: AnyRecord): string => l.optionText1,
+          value: 1,
+          selected: false,
         },
+        {
+          name: 'working_notice',
+          label: (l: AnyRecord): string => l.optionText2,
+          value: 2,
+          selected: false,
+        },
+        {
+          name: 'not_working',
+          label: (l: AnyRecord): string => l.optionText3,
+          value: 3,
+          selected: false,
+        },
+      ],
+      validator: isFieldFilledIn,
     },
-    submit: {
-        text: (l: AnyRecord): string => l.continue,
-        classes: 'govuk-!-margin-right-2',
-      },
-}
-
-
+  },
+  submit: {
+    text: (l: AnyRecord): string => l.continue,
+    classes: 'govuk-!-margin-right-2',
+  },
+};
--- src/main/controllers/still_working/stillWorkingController.ts
+++ src/main/controllers/still_working/stillWorkingController.ts
@@ -5,23 +5,22 @@ import { AppRequest } from '../../definitions/appRequest';
 import { FormContent, FormFields } from '../../definitions/form';
 import { assignFormData, getPageContent, handleSessionErrors, setUserCase } from '../helpers';
 export default class stillWorkingController {
-    private readonly form: Form;
-  
-    constructor(private readonly stillWorkingContent: FormContent) {
-      this.form = new Form(<FormFields>this.stillWorkingContent.fields);
-    }
-  
-    public post = (req: AppRequest, res: Response): void => {
-      setUserCase(req, this.form);
-      handleSessionErrors(req, res, this.form, '/');
-    };
-  
-    public get = (req: AppRequest, res: Response): void => {
-      const content = getPageContent(req, this.stillWorkingContent, ['common', 'still-working']);
-      assignFormData(req.session.userCase, this.form.getFormFields());
-      res.render('still-working', {
-        ...content,
-      });
-    };
+  private readonly form: Form;
+
+  constructor(private readonly stillWorkingContent: FormContent) {
+    this.form = new Form(<FormFields>this.stillWorkingContent.fields);
   }
-  
+
+  public post = (req: AppRequest, res: Response): void => {
+    setUserCase(req, this.form);
+    handleSessionErrors(req, res, this.form, '/');
+  };
+
+  public get = (req: AppRequest, res: Response): void => {
+    const content = getPageContent(req, this.stillWorkingContent, ['common', 'still-working']);
+    assignFormData(req.session.userCase, this.form.getFormFields());
+    res.render('still-working', {
+      ...content,
+    });
+  };
+}
