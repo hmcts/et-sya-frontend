de548f2c0e6159cb6b346f8ba288cf02c48be312
Angela Liu-Wilkin <angela.liu-wilkin@justice.gov.uk>
2022-02-17 17:57:41 +0000

    update chart version

--- src/test/routes/gender-details.test.ts
+++ src/test/routes/gender-details.test.ts
@@ -1,10 +1,11 @@
 import request from 'supertest';
 
 import { app } from '../../main/app';
+import { PageUrls } from '../../main/definitions/constants';
 
-describe('GET /gender-details', () => {
+describe(`GET ${PageUrls.GENDER_DETAILS}`, () => {
   it('should return the gender details page', async () => {
-    const res = await request(app).get('/gender-details');
+    const res = await request(app).get(PageUrls.GENDER_DETAILS);
     expect(res.type).toStrictEqual('text/html');
     expect(res.status).toStrictEqual(200);
   });
