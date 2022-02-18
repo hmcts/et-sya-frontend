f533242fe33d7a4e6f6a15f507b7c0366aa18782
MatthewDalbyV1 <matthew.dalby@version1.com>
2022-02-18 10:29:48 +0000

    added post tests

--- src/test/routes/still-working.test.ts
+++ src/test/routes/still-working.test.ts
@@ -9,3 +9,36 @@ describe('GET /are-you-still-working', () => {
     expect(res.status).toStrictEqual(200);
   });
 });
+
+describe('on POST /are-you-still-working', () => {
+  test('should return the employment details(WORKING) page', async () => {
+    await request(app)
+      .post('/')
+      .send({ isStillWorking: 'WORKING' })
+      .expect(res => {
+        expect(res.status).toStrictEqual(302);
+        // page to be implemented, this test will need updated
+        expect(res.header['location']).toStrictEqual('/');
+      });
+  });
+  test('should return the employment details(NOTICE) page', async () => {
+    await request(app)
+      .post('/')
+      .send({ isStillWorking: 'NOTICE' })
+      .expect(res => {
+        expect(res.status).toStrictEqual(302);
+        // page to be implemented, this test will need updated
+        expect(res.header['location']).toStrictEqual('/');
+      });
+  });
+  test('should return the employment details(NO LONGER WORKING) page', async () => {
+    await request(app)
+      .post('/')
+      .send({ isStillWorking: 'NO LONGER WORKING' })
+      .expect(res => {
+        expect(res.status).toStrictEqual(302);
+        // page to be implemented, this test will need updated
+        expect(res.header['location']).toStrictEqual('/');
+      });
+  });
+});
