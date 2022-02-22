a632b3fa0e9860853ac11be0f3176db71c9e1f6a
Tautvydas Cerniauskas <tautvydas.cerniauskas@hmcts.net>
2022-02-22 10:13:49 +0000

    tidy: cleaned up urls

--- src/test/routes/type-of-claim.test.ts
+++ src/test/routes/type-of-claim.test.ts
@@ -1,23 +1,21 @@
 import request from 'supertest';
 
 import { app } from '../../main/app';
-import { AuthUrls } from '../../main/definitions/constants';
+import { AuthUrls, PageUrls } from '../../main/definitions/constants';
 import { TypesOfClaim } from '../../main/definitions/definition';
 
-const PAGE_URL = '/type-of-claim';
-
-describe(`GET ${PAGE_URL}`, () => {
+describe(`GET ${PageUrls.TYPE_OF_CLAIM}`, () => {
   it('should return the type of claim page', async () => {
-    const res = await request(app).get(PAGE_URL);
+    const res = await request(app).get(PageUrls.TYPE_OF_CLAIM);
     expect(res.type).toStrictEqual('text/html');
     expect(res.status).toStrictEqual(200);
   });
 });
 
-describe(`on POST ${PAGE_URL}`, () => {
+describe(`on POST ${PageUrls.TYPE_OF_CLAIM}`, () => {
   test('should return the type of claim claim page when is not selected "Breach of contract" is selected', async () => {
     await request(app)
-      .post(PAGE_URL)
+      .post(PageUrls.TYPE_OF_CLAIM)
       .send({
         typeOfClaim: [],
       })
@@ -29,7 +27,7 @@ describe(`on POST ${PAGE_URL}`, () => {
 
   test('should navigate to login page when "Breach of contract" is selected', async () => {
     await request(app)
-      .post(PAGE_URL)
+      .post(PageUrls.TYPE_OF_CLAIM)
       .send({
         typeOfClaim: [TypesOfClaim.BREACH_OF_CONTRACT],
       })
