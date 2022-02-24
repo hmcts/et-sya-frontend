2919b9c463c46e630e0ebdbacd28af0103da924b
MatthewDalbyV1 <matthew.dalby@version1.com>
2022-02-24 02:09:31 +0000

    updated title to be consistent with the prototype

--- src/test/unit/views/steps-to-making-your-claim.test.ts
+++ src/test/unit/views/steps-to-making-your-claim.test.ts
@@ -1,10 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+
 import { expect } from 'chai';
 import request from 'supertest';
 
 
 import { app } from '../../../main/app';
 
+const stepsToMakingYourClaimJSONRaw = fs.readFileSync(
+    path.resolve(__dirname, '../../../main/resources/locales/en/translation/steps-to-making-your-claim.json'),
+    'utf-8'
+  );
+const stepsToMakingYourClaimJSON = JSON.parse(stepsToMakingYourClaimJSONRaw);
+
 const PAGE_URL = '/steps-to-making-your-claim-page';
+const tableClass = 'govuk-table';
+
+const titleClass = 'govuk-heading-xl';
+const expectedTitle = stepsToMakingYourClaimJSON.h1 + "TODO";
 
 let htmlRes: Document;
 describe('Steps to making your claim page',()=>{
@@ -15,4 +28,10 @@ describe('Steps to making your claim page',()=>{
             htmlRes = new DOMParser().parseFromString(res.text, 'text/html');
           });
       });
+      it('should display title', () => {
+        const title = htmlRes.getElementsByClassName(titleClass);
+        expect(title[0].innerHTML).contains(expectedTitle, 'Page title does not exist');
+      });
+   
+     
 })
