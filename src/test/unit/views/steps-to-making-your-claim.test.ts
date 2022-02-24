09be9e6599f67b29c7befb01cfb5ef3d3400fd04
MatthewDalbyV1 <matthew.dalby@version1.com>
2022-02-24 14:54:51 +0000

    updated test coverage for the view

--- src/test/unit/views/steps-to-making-your-claim.test.ts
+++ src/test/unit/views/steps-to-making-your-claim.test.ts
@@ -14,10 +14,16 @@ const stepsToMakingYourClaimJSONRaw = fs.readFileSync(
 const stepsToMakingYourClaimJSON = JSON.parse(stepsToMakingYourClaimJSONRaw);
 
 const PAGE_URL = '/steps-to-making-your-claim-page';
+const rowClass = 'govuk-table__row';
+const cellClass = 'govuk-table__cell';
 const tableClass = 'govuk-table';
-
+const headerClass = 'govuk-table__header';
 const titleClass = 'govuk-heading-xl';
 const expectedTitle = stepsToMakingYourClaimJSON.h1 + "TODO";
+const expectedHeader1 = stepsToMakingYourClaimJSON.title1;
+const expectedHeader2 = stepsToMakingYourClaimJSON.title2;
+const expectedHeader3 = stepsToMakingYourClaimJSON.title3;
+const expectedHeader4 = stepsToMakingYourClaimJSON.title4;
 
 let htmlRes: Document;
 describe('Steps to making your claim page',()=>{
@@ -28,10 +34,38 @@ describe('Steps to making your claim page',()=>{
             htmlRes = new DOMParser().parseFromString(res.text, 'text/html');
           });
       });
+
       it('should display title', () => {
         const title = htmlRes.getElementsByClassName(titleClass);
         expect(title[0].innerHTML).contains(expectedTitle, 'Page title does not exist');
       });
-   
+
+      it('should display correct number of rows', () => {
+          const row = htmlRes.getElementsByClassName(rowClass);
+          expect(row.length).equal(14,'number of tables found is not 14');
+      });
+
+      it('should display the correct number of tables', () => {
+          const table = htmlRes.getElementsByClassName(tableClass);
+          expect(table.length).equal(4,'number of tables found is not 4');
+      });
+
+      it('should display the correct number of cells', () => {
+          const cell = htmlRes.getElementsByClassName(cellClass);
+          expect(cell.length).equal(20,'number of cells found is not 20');
+      });
+
+      it('shoul display the correct number of table headers',() =>{
+          const header = htmlRes.getElementsByClassName(headerClass);
+          expect(header.length).equal(4,'number of table headers found is not 4');
+      });
+
+      it('should display the correct table header texts', () => {
+          const header = htmlRes.getElementsByClassName(headerClass);
+          expect(header[0].innerHTML).contains(expectedHeader1,'could not find table 1 header text');
+          expect(header[1].innerHTML).contains(expectedHeader2,'could not find table 2 header text');
+          expect(header[2].innerHTML).contains(expectedHeader3,'could not find table 3 header text');
+          expect(header[3].innerHTML).contains(expectedHeader4,'could not find table 4 header text');
+      });
      
 })
