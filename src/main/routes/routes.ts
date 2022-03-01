f8650af97c3efc0bc809153dd3a1abdc7298906d
Angela Liu-Wilkin <90756556+Angela-LW@users.noreply.github.com>
2022-03-01 16:35:20 +0000

    RET-1007 notice period (#121)
    * add initial notice period layout
    
    * add validation
    
    * add tests
    
    * add error display
    
    * edited test
    
    * fixed errors
    
    * fixed errors
    
    * fixed test
    
    * fixed errors and add number validator
    
    * edited validation
    
    * edited validation

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -48,6 +48,8 @@ export default function (app: Application): void {
   app.post(PageUrls.TYPE_OF_CLAIM, app.locals.container.cradle.typeOfClaimController.post);
   app.get(PageUrls.PAST_EMPLOYER, app.locals.container.cradle.pastEmployerController.get);
   app.post(PageUrls.PAST_EMPLOYER, app.locals.container.cradle.pastEmployerController.post);
+  app.get(PageUrls.NOTICE_PERIOD, app.locals.container.cradle.noticePeriodController.get);
+  app.post(PageUrls.NOTICE_PERIOD, app.locals.container.cradle.noticePeriodController.post);
   app.get(PageUrls.EMPLOYMENT_DETAILS_PENSION, app.locals.container.cradle.employmentDetailsPensionController.get);
   app.post(PageUrls.EMPLOYMENT_DETAILS_PENSION, app.locals.container.cradle.employmentDetailsPensionController.post);
 
---
+++ src/test/unit/controller/NoticePeriodController.test.ts
@@ -0,0 +1,116 @@
+import { isFieldFilledIn } from '../../../main/components/form/validator';
+import NoticePeriodController from '../../../main/controllers/notice_period/NoticePeriodController';
+import { AppRequest } from '../../../main/definitions/appRequest';
+import { TranslationKeys } from '../../../main/definitions/constants';
+import { FormContent } from '../../../main/definitions/form';
+import { mockRequest } from '../mocks/mockRequest';
+import { mockResponse } from '../mocks/mockResponse';
+
+describe('Update Notice Period Controller', () => {
+  const t = {
+    'notice-period': {},
+    common: {},
+  };
+
+  const mockFormContent = {
+    fields: {
+      noticePeriod: {
+        classes: 'govuk-radios',
+        id: 'notice-period',
+        type: 'radios',
+        values: [
+          {
+            name: 'radio1',
+            label: 'radio1',
+            subFields: {
+              noticePeriodUnit: {
+                id: 'notice-period-length',
+                type: 'radios',
+                classes: 'govuk-radios--inline',
+                values: [
+                  {
+                    name: 'innerRadio1',
+                    label: 'innerRadio1',
+                  },
+                  {
+                    name: 'innerRadio2',
+                    label: 'innerRadio2',
+                  },
+                ],
+                validator: isFieldFilledIn,
+              },
+              noticePeriodLength: {
+                id: 'notice-period-unit',
+                type: 'input',
+                classes: 'govuk-input--width-2',
+                label: 'input',
+                labelSize: null,
+                inputMode: 'numeric',
+                pattern: '[0-9]*',
+                spellCheck: false,
+                attributes: {
+                  autocomplete: 'notice-period-length',
+                  maxLength: 2,
+                },
+                validator: isFieldFilledIn,
+              },
+            },
+          },
+          {
+            name: 'radio1',
+            label: 'radio2',
+          },
+        ],
+      },
+    },
+  } as unknown as FormContent;
+
+  it('should render the next page', () => {
+    const controller = new NoticePeriodController(mockFormContent);
+    const response = mockResponse();
+    const request = <AppRequest>mockRequest({ t });
+
+    controller.get(request, response);
+    expect(response.render).toHaveBeenCalledWith(TranslationKeys.NOTICE_PERIOD, expect.anything());
+  });
+
+  it('should redirect to the same screen when errors are present', () => {
+    const errors = [
+      {
+        propertyName: 'noticePeriodUnit',
+        errorType: 'required',
+      },
+      {
+        propertyName: 'noticePeriodLength',
+        errorType: 'required',
+      },
+    ];
+    const body = { noticePeriod: 'Yes', noticePeriodUnit: '', noticePeriodLength: '' };
+    const controller = new NoticePeriodController(mockFormContent);
+
+    const req = mockRequest({ body });
+    const res = mockResponse();
+    controller.post(req, res);
+
+    expect(req.session.errors).toEqual(errors);
+    expect(res.redirect).toBeCalledWith(req.path);
+  });
+
+  it('should assign userCase from notice period form data', () => {
+    const body = { noticePeriod: 'Yes', noticePeriodLength: '2', noticePeriodUnit: 'Weeks' };
+    const controller = new NoticePeriodController(mockFormContent);
+
+    const req = mockRequest({ body });
+    const res = mockResponse();
+    req.session.userCase = undefined;
+
+    controller.post(req, res);
+
+    expect(res.redirect).toBeCalledWith('/');
+    expect(req.session.userCase).toStrictEqual({
+      noticePeriod: 'Yes',
+      noticePeriodLength: '2',
+      noticePeriodUnit: 'Weeks',
+    });
+  });
+});
