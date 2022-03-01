34b034bca45d3d7be90babaa3c626feb80f7db8c
Angela Liu-Wilkin <angela.liu-wilkin@justice.gov.uk>
2022-03-01 12:25:09 +0000

    refactored page to use content array in controller

--- src/main/controllers/StepsToMakingYourClaimController.ts
+++ src/main/controllers/StepsToMakingYourClaimController.ts
@@ -1,10 +1,31 @@
 import { Response } from 'express';
 
 import { AppRequest } from '../definitions/appRequest';
-import { TranslationKeys } from '../definitions/constants';
+import { PageUrls, TranslationKeys } from '../definitions/constants';
 import { FormContent } from '../definitions/form';
+import { AnyRecord } from '../definitions/util-types';
 
 import { getPageContent } from './helpers';
+
+const sections = [
+  {
+    title: (l: AnyRecord): string => l.section1.title,
+    links: [
+      {
+        url: PageUrls.DOB_DETAILS,
+        linkTxt: (l: AnyRecord): string => l.section1.link1Text,
+      },
+      {
+        url: PageUrls.ADDRESS_DETAILS,
+        linkTxt: (l: AnyRecord): string => l.section1.link2Text,
+      },
+      {
+        url: PageUrls.UPDATE_PREFERENCES,
+        linkTxt: (l: AnyRecord): string => l.section1.link3Text,
+      },
+    ],
+  },
+];
 
 export default class StepsToMakingYourClaimController {
   public get(req: AppRequest, res: Response): void {
@@ -12,8 +33,10 @@ export default class StepsToMakingYourClaimController {
       TranslationKeys.COMMON,
       TranslationKeys.STEPS_TO_MAKING_YOUR_CLAIM,
     ]);
+
     res.render(TranslationKeys.STEPS_TO_MAKING_YOUR_CLAIM, {
       ...content,
+      sections,
     });
   }
 }
