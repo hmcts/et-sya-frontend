140edcb415a95fd6d35146662ae848810f28e6c8
MatthewDalbyV1 <matthew.dalby@version1.com>
2022-03-03 12:19:08 +0000

    remaned the files and added translations for the start date content

--- src/main/controllers/start_date/startDateController.ts
+++ src/main/controllers/start_date/startDateController.ts
@@ -6,26 +6,22 @@ import { PageUrls, TranslationKeys } from '../../definitions/constants';
 import { FormContent, FormFields } from '../../definitions/form';
 import { assignFormData, getPageContent, handleSessionErrors, setUserCase } from '../helpers';
 
-export default class employmentDetailsStartDateController {
+export default class startDateController {
   private readonly form: Form;
 
-  constructor(private readonly employmentDetailsStartDateContent: FormContent) {
-    this.form = new Form(<FormFields>this.employmentDetailsStartDateContent.fields);
+  constructor(private readonly startDateContent: FormContent) {
+    this.form = new Form(<FormFields>this.startDateContent.fields);
   }
   public post = (req: AppRequest, res: Response): void => {
     setUserCase(req, this.form);
     // TODO: Change to the correct redirect url
-    // YES, NO or no response - '/employment-details-notice-benefits' RET-1059
     handleSessionErrors(req, res, this.form, PageUrls.HOME);
   };
 
   public get = (req: AppRequest, res: Response): void => {
-    const content = getPageContent(req, this.employmentDetailsStartDateContent, [
-      TranslationKeys.COMMON,
-      TranslationKeys.EMPLOYMENT_DETAILS_START_DATE,
-    ]);
+    const content = getPageContent(req, this.startDateContent, [TranslationKeys.COMMON, TranslationKeys.START_DATE]);
     assignFormData(req.session.userCase, this.form.getFormFields());
-    res.render(TranslationKeys.EMPLOYMENT_DETAILS_START_DATE, {
+    res.render(TranslationKeys.START_DATE, {
       ...content,
     });
   };
