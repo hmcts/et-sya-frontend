2f30bda6f459e31eb27dc3654016e2843cf61e9f
MatthewDalbyV1 <97883126+MatthewDalbyV1@users.noreply.github.com>
2022-03-04 12:54:29 +0000

    RET-1087: Added the task list to the steps to making your claim page (#119)
    * updated the steps to making your claim page with a table and created a
    translation file
    
    * added row headings and link text to translation file
    
    * added text to table
    
    * fixed typo in routes and constants and added the TODO cells to the table
    
    * updated steps to making your claim view urls with only confirmed changes
    
    * updated title to be consistent with the prototype
    
    * updated test coverage for the view
    
    * fixed merge conflicts and updated json structure for readability
    
    * prettifier and eslint fixes
    
    * added test coverage for links on steps to making your claim page
    
    * fixed typo in test
    
    * fixed parge url
    
    * updated title test to not look for TODO
    
    * fixed issue with link test
    
    * fixed link number order issue
    
    * changed view to use a for loop structure
    
    * changed the json to itterate through arrays rather than objects
    
    * updated test to reflect json change
    
    * fixed the row auto width issue
    
    * fixed the bold row issue
    
    * fixed broken links
    
    * changed the json to support using the PageUrls constants to prevent
    issue with url changes
    
    * fixed typo in test
    
    * fixed controller json call structure
    
    * flattened json structure and fixed tests
    
    Co-authored-by: Angela Liu-Wilkin <90756556+Angela-LW@users.noreply.github.com>

--- src/main/controllers/StepsToMakingYourClaimController.ts
+++ src/main/controllers/StepsToMakingYourClaimController.ts
@@ -1,10 +1,76 @@
 import { Response } from 'express';
 
 import { AppRequest } from '../definitions/appRequest';
-import { TranslationKeys } from '../definitions/constants';
+import { PageUrls, TranslationKeys } from '../definitions/constants';
 import { FormContent } from '../definitions/form';
+import { AnyRecord } from '../definitions/util-types';
 
 import { getPageContent } from './helpers';
+
+const sections = [
+  {
+    title: (l: AnyRecord): string => l.section1.title,
+    links: [
+      {
+        url: PageUrls.DOB_DETAILS,
+        linkTxt: (l: AnyRecord): string => l.section1.link1Text,
+      },
+      {
+        url: PageUrls.ADDRESS_DETAILS,
+        linkTxt: (l: AnyRecord): string => l.section1.link2Text,
+      },
+      {
+        url: PageUrls.UPDATE_PREFERENCES,
+        linkTxt: (l: AnyRecord): string => l.section1.link3Text,
+      },
+    ],
+  },
+  {
+    title: (l: AnyRecord): string => l.section2.title,
+    links: [
+      {
+        url: PageUrls.STILL_WORKING,
+        linkTxt: (l: AnyRecord): string => l.section2.link1Text,
+      },
+      {
+        url: '',
+        linkTxt: (l: AnyRecord): string => l.section2.link2Text,
+      },
+      {
+        url: '',
+        linkTxt: (l: AnyRecord): string => l.section2.link3Text,
+      },
+
+      {
+        url: '',
+        linkTxt: (l: AnyRecord): string => l.section2.link4Text,
+      },
+    ],
+  },
+  {
+    title: (l: AnyRecord): string => l.section3.title,
+    links: [
+      {
+        url: '',
+        linkTxt: (l: AnyRecord): string => l.section3.link1Text,
+      },
+
+      {
+        url: '',
+        linkTxt: (l: AnyRecord): string => l.section3.link2Text,
+      },
+    ],
+  },
+  {
+    title: (l: AnyRecord): string => l.section4.title,
+    links: [
+      {
+        url: '',
+        linkTxt: (l: AnyRecord): string => l.section4.link1Text,
+      },
+    ],
+  },
+];
 
 export default class StepsToMakingYourClaimController {
   public get(req: AppRequest, res: Response): void {
@@ -14,6 +80,7 @@ export default class StepsToMakingYourClaimController {
     ]);
     res.render(TranslationKeys.STEPS_TO_MAKING_YOUR_CLAIM, {
       ...content,
+      sections,
     });
   }
 }
