59a346d5ce21e9ec7b32363bae9141ae84a5704b
Angela Liu-Wilkin <angela.liu-wilkin@justice.gov.uk>
2022-03-15 14:29:59 +0000

    add app mock and fixed average working hours

---
+++ src/test/unit/mocks/mockApp.ts
@@ -0,0 +1,35 @@
+import express, { Express } from 'express';
+
+import { app } from '../../../main/app';
+import { AppSession } from '../../../main/definitions/appRequest';
+import { CaseWithId } from '../../../main/definitions/case';
+import { AnyRecord } from '../../../main/definitions/util-types';
+
+export const mockApp = ({
+  body,
+  userCase,
+  session,
+}: {
+  body?: AnyRecord;
+  userCase?: Partial<CaseWithId>;
+  session?: AppSession;
+}): Express => {
+  const mock = express();
+  mock.all('*', function (req, res, next) {
+    req.body = body;
+    req.session = {
+      userCase: {
+        id: '1234',
+        dobDate: { year: '2000', month: '12', day: '24' },
+        ...userCase,
+      } as CaseWithId,
+      ...session,
+      save: jest.fn(done => done()),
+      lang: 'en',
+      errors: undefined,
+    } as unknown as AppSession;
+    next();
+  });
+  mock.use(app);
+  return mock;
+};
