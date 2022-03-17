cc4e3f5c10cc5c0582a1be2fffafa9743fab33ab
Olayode <olayode.olatunji@hmcts.net>
2022-03-17 16:49:47 +0000

    added test for RET-1016

--- src/test/unit/views/employee-benetfit.test.ts
+++ src/test/unit/views/employee-benetfit.test.ts
@@ -8,12 +8,12 @@ import { app } from '../../../main/app';
 import { PageUrls } from '../../../main/definitions/constants';
 
 const employeebBenefitJsonRaw = fs.readFileSync(
-  path.resolve(__dirname, '../../../main/resources/locales/en/translation/notice-period.json'),
+  path.resolve(__dirname, '../../../main/resources/locales/en/translation/employee_benefits.json'),
   'utf-8'
 );
 const employeebBenefitJson = JSON.parse(employeebBenefitJsonRaw);
 
-const titleClass = 'govuk-heading-xl';
+const titleClass = 'govuk-heading-xl govuk-!-margin-bottom-6';
 const expectedTitle = employeebBenefitJson.h1;
 // const radios = 'govuk-radios';
 
@@ -29,6 +29,7 @@ describe('Do or did you receive any employee benefits?', () => {
 
   it('Should display correct heading', () => {
     const title = htmlRes.getElementsByClassName(titleClass);
+    console.log(`Title receieved is dd ${title[0].innerHTML}`);
     expect(title[0].innerHTML).contains(expectedTitle, 'Do or did you receive any employee benefits?');
   });
 });
