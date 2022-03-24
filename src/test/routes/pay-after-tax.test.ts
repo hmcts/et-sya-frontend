337f5a4a09fe40ca7234f7640f036b0ffa1dbd88
lvo700 <lynnette.orr@HMCTS.net>
2022-03-24 11:03:11 +0000

    Fixed failing routes tests

--- src/test/routes/pay-after-tax.test.ts
+++ src/test/routes/pay-after-tax.test.ts
@@ -1,11 +1,19 @@
 import request from 'supertest';
 
 import { app } from '../../main/app';
+import { StillWorking } from '../../main/definitions/case';
 import { PageUrls } from '../../main/definitions/constants';
+import { mockApp } from '../unit/mocks/mockApp';
 
 describe(`GET ${PageUrls.PAY_AFTER_TAX}`, () => {
-  it('should return the pay after tax end page', async () => {
-    const res = await request(app).get(PageUrls.PAY_AFTER_TAX);
+  it('should return the pay after tax page', async () => {
+    const res = await request(
+      mockApp({
+        userCase: {
+          isStillWorking: StillWorking.WORKING || StillWorking.NOTICE || StillWorking.NO_LONGER_WORKING,
+        },
+      })
+    ).get(PageUrls.PAY_AFTER_TAX);
     expect(res.type).toStrictEqual('text/html');
     expect(res.status).toStrictEqual(200);
   });
