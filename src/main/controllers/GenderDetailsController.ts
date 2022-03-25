a1d17931d7df84bd2296b91fe34a23aa9e285bcd
Brendan McHugh <brendan.mchugh@hmcts.net>
2022-03-25 16:46:20 +0000

    add gender details form radios and select

--- src/main/controllers/GenderDetailsController.ts
+++ src/main/controllers/GenderDetailsController.ts
@@ -1,7 +1,9 @@
 import { Response } from 'express';
 
 import { Form } from '../components/form/form';
+import { isFieldFilledIn } from '../components/form/validator';
 import { AppRequest } from '../definitions/appRequest';
+import { YesOrNo } from '../definitions/case';
 import { PageUrls, TranslationKeys } from '../definitions/constants';
 import { FormContent, FormFields } from '../definitions/form';
 import { AnyRecord } from '../definitions/util-types';
@@ -11,7 +13,71 @@ import { assignFormData, getPageContent, handleSessionErrors, setUserCase } from
 export default class GenderDetailsController {
   private readonly form: Form;
   private readonly genderDetailsContent: FormContent = {
-    fields: {},
+    fields: {
+      gender: {
+        classes: 'govuk-radios',
+        id: 'gender',
+        type: 'radios',
+        label: (l: AnyRecord): string => l.sex,
+        values: [
+          {
+            label: (l: AnyRecord): string => l.female,
+            name: 'radio1',
+            value: 'Yes',
+            attributes: { maxLength: 2 },
+          },
+          {
+            label: (l: AnyRecord): string => l.male,
+            name: 'radio2',
+            value: 'No',
+            attributes: { maxLength: 2 },
+          },
+        ],
+        validator: isFieldFilledIn,
+      },
+      genderIdentity: {
+        classes: 'govuk-radios',
+        id: 'gender',
+        type: 'radios',
+        label: (l: AnyRecord): string => l.genderIdentity,
+        hint: (l: AnyRecord): string => l.genderIdentityHint,
+        values: [
+          {
+            label: l => l.yes,
+            value: YesOrNo.YES,
+          },
+          {
+            label: l => l.no,
+            value: YesOrNo.NO,
+          },
+        ],
+        validator: isFieldFilledIn,
+      },
+      preferredTitle: {
+        classes: 'govuk-radios',
+        id: 'preferredTitle',
+        type: 'option',
+        label: (l: AnyRecord): string => l.preferredTitle,
+        values: [
+          {
+            value: 'published',
+            label: (l: AnyRecord): string => l.female,
+          },
+          {
+            value: 'updated',
+            label: (l: AnyRecord): string => l.female,
+          },
+          {
+            value: 'views',
+            label: (l: AnyRecord): string => l.female,
+          },
+          {
+            value: 'comments',
+            label: (l: AnyRecord): string => l.female,
+          },
+        ],
+      },
+    },
     submit: {
       text: (l: AnyRecord): string => l.submit,
       classes: 'govuk-!-margin-right-2',
