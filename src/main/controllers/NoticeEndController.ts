07e4f6218e436f40158990e2634babfe782f6642
olyode <olayode.olatunji@version1.com>
2022-03-25 11:04:05 +0000

    fixes content display

--- src/main/controllers/NoticeEndController.ts
+++ src/main/controllers/NoticeEndController.ts
@@ -11,7 +11,35 @@ import { assignFormData, getPageContent, handleSessionErrors, setUserCase } from
 export default class NoticeEndController {
   private readonly form: Form;
   private readonly noticeEndContent: FormContent = {
-    fields: {},
+    fields: {
+      noticeEnd: {
+        id: 'notice-end',
+        type: 'date',
+        classes: 'govuk-date-input',
+        label: (l: AnyRecord): string => l.label,
+        labelHidden: true,
+        values: [
+          {
+            label: (l: AnyRecord): string => l.dateFormat.day,
+            name: 'day',
+            classes: 'govuk-input--width-2',
+            attributes: { maxLength: 2 },
+          },
+          {
+            label: (l: AnyRecord): string => l.dateFormat.month,
+            name: 'month',
+            classes: 'govuk-input--width-2',
+            attributes: { maxLength: 2 },
+          },
+          {
+            label: (l: AnyRecord): string => l.dateFormat.year,
+            name: 'year',
+            classes: 'govuk-input--width-4',
+            attributes: { maxLength: 4 },
+          },
+        ],
+      },
+    },
     submit: {
       text: (l: AnyRecord): string => l.submit,
       classes: 'govuk-!-margin-right-2',
