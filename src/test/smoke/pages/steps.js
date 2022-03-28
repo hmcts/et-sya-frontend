523e7b7b3b38b44e8523573d759b7d0a4903cb96
UDAYABHASKAR <udayabhaskar@UDAYABHASKARs-MacBook-Pro.local>
2022-03-28 16:08:37 +0100

    removed commented code

--- src/test/smoke/pages/steps.js
+++ src/test/smoke/pages/steps.js
@@ -5,8 +5,6 @@ const steps = requireDirectory(module);
 const actions = {};
 
 function setActorActions(data) {
-  // eslint-disable-next-line no-console
-  // console.log('Setting Action actions for data : '+JSON.stringify(data));
   for (const k in data) {
     if (data.hasOwnProperty(k)) {
       actions[k] = data[k];
@@ -16,9 +14,6 @@ function setActorActions(data) {
 
 function setActorActionsRecutssively(object) {
   const objKeys = Object.keys(object);
-
-  // eslint-disable-next-line no-console
-  // console.log('Set Action for object : '+JSON.stringify(object));
   for (const key in objKeys) {
     if (Object.keys(object[objKeys[key]]).length === 0) {
       setActorActions(object);
@@ -30,7 +25,5 @@ function setActorActionsRecutssively(object) {
 
 module.exports = function () {
   setActorActionsRecutssively(steps);
-  // eslint-disable-next-line no-console
-  // console.log('actions : '+JSON.stringify(actions));
   return actor(actions);
 };
