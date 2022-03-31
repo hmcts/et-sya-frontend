17531242e854d51a633574ca7609f3044b3c650e
olyode <olayode.olatunji@version1.com>
2022-03-31 09:48:56 +0100

    added post test to controller

--- src/test/unit/controller/NoticeEndController.test.ts
+++ src/test/unit/controller/NoticeEndController.test.ts
@@ -1,5 +1,5 @@
 import NoticeEndController from '../../../main/controllers/NoticeEndController';
-import { TranslationKeys } from '../../../main/definitions/constants';
+import { PageUrls, TranslationKeys } from '../../../main/definitions/constants';
 import { mockRequest } from '../mocks/mockRequest';
 import { mockResponse } from '../mocks/mockResponse';
 
@@ -16,11 +16,31 @@ describe('Notice end Controller', () => {
 
     noticeEndController.get(request, response);
     expect(response.render).toHaveBeenCalledWith(TranslationKeys.NOTICE_END, expect.anything());
+
     const body = { noticeEnds: '' };
     const req = mockRequest({ body });
     const res = mockResponse();
     noticeEndController.post(req, res);
     expect(response.render).toHaveBeenCalledWith(TranslationKeys.NOTICE_END, expect.anything());
+  });
+
+  it('should redirect to notice period pay on successful post', () => {
+    const body = {
+      'noticeEnds-day': '21',
+
+      'noticeEnds-month': '01',
+
+      'noticeEnds-year': '2022',
+    };
+
+    const controller = new NoticeEndController();
+    const req = mockRequest({ body });
+
+    const res = mockResponse();
+
+    controller.post(req, res);
+
+    expect(res.redirect).toBeCalledWith(PageUrls.NOTICE_PAY);
   });
 
   it('should redirect to the same screen when errors are present', () => {
