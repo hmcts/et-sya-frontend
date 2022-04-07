31959cc020d052f0026538856e0af835527ee405
UDAYABHASKAR <udayabhaskar@UDAYABHASKARs-MacBook-Pro.local>
2022-04-07 15:24:22 +0100

    account loading page automation

--- src/test/functional/authUser/loginIdam.js
+++ src/test/functional/authUser/loginIdam.js
@@ -4,10 +4,8 @@ const { I } = inject();
 
 function signInWithCredentials(username, password) {
   I.seeElement('#username');
-  /* eslint-disable */
   I.fillField('#username', data.signIn.username);
   I.fillField('#password', data.signIn.password);
-  /* eslint-enable */
   I.click('Sign in');
 }
 module.exports = { signInWithCredentials };
