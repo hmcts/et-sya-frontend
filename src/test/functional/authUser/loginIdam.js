62866e9de440c4c31988e223427b5bdcb72dfbea
UDAYABHASKAR <udayabhaskar@UDAYABHASKARs-MacBook-Pro.local>
2022-04-07 15:47:37 +0100

    new account page loading automation

---
+++ src/test/functional/authUser/loginIdam.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+const data = require('../data.json');
+const { I } = inject();
+
+function signInWithCredentials(username, password) {
+  I.seeElement('#username');
+  I.fillField('#username', data.signIn.username);
+  I.fillField('#password', data.signIn.password);
+  I.click('Sign in');
+}
+module.exports = { signInWithCredentials };
---
+++ src/test/functional/features/newAccountLoading.js
@@ -0,0 +1,16 @@
+Feature('ET check new account loading page');
+const test_url = '/type-of-claim';
+const { I } = inject();
+const loginIdam = require('../authUser/loginIdam.js');
+
+Scenario('Verify new account loading page', () => {
+  I.amOnPage(test_url);
+  I.see('What type of claim do you want to make?');
+  I.checkOption('#typeOfClaim');
+  I.click('#main-form-submit');
+
+  loginIdam.signInWithCredentials();
+
+  I.seeElement('(//a[@href="/steps-to-making-your-claim"])');
+  I.see('You do not have to complete your claim in one go');
+}).tag('@RET-1028');
