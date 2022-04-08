bbb5bac5ff445d7dd2b48baf1f077b824106ac7e
SunnyAyeni <95691644+SunnyAyeni@users.noreply.github.com>
2022-04-08 14:17:19 +0100

    Test Automation For RET-1235 and Cross browser fix (#196)
    * test cross browser
    
    * update url
    
    * resolve review comments

---
+++ src/test/functional/saucelabs.conf.js
@@ -0,0 +1,85 @@
+const supportedBrowsers = require('./supportedBrowsers');
+
+const defaultSauceOptions = {
+  username: process.env.SAUCE_USERNAME,
+  accessKey: process.env.SAUCE_ACCESS_KEY,
+  tunnelIdentifier: process.env.TUNNEL_IDENTIFIER || 'reformtunnel',
+  acceptSslCerts: true,
+  tags: ['ET'],
+};
+
+function merge(intoObject, fromObject) {
+  return Object.assign({}, intoObject, fromObject);
+}
+
+function getBrowserConfig(browserGroup) {
+  const browserConfig = [];
+  for (const candidateBrowser in supportedBrowsers[browserGroup]) {
+    if (candidateBrowser) {
+      const candidateCapabilities = supportedBrowsers[browserGroup][candidateBrowser];
+      candidateCapabilities['sauce:options'] = merge(defaultSauceOptions, candidateCapabilities['sauce:options']);
+      browserConfig.push({
+        browser: candidateCapabilities.browserName,
+        capabilities: candidateCapabilities,
+      });
+    } else {
+      console.error('ERROR: supportedBrowsers.js is empty or incorrectly defined');
+    }
+  }
+  return browserConfig;
+}
+
+const setupConfig = {
+  tests: '../functional/features/*.js',
+  output: 'functional/output',
+  helpers: {
+    WebDriver: {
+      url: process.env.TEST_URL || 'https://et-sya.aat.platform.hmcts.net/',
+      browser: process.env.SAUCE_BROWSER || '',
+      //host: process.env.HOST || 'saucelabs',
+      cssSelectorsEnabled: 'true',
+      host: 'ondemand.eu-central-1.saucelabs.com',
+      port: 80,
+      region: 'eu',
+      capabilities: {},
+    },
+    MyHelper: {
+      require: './saucelabshelper.js',
+      url: process.env.TEST_URL || 'https://et-sya.aat.platform.hmcts.net/',
+    },
+  },
+  include: {
+    I: './pages/steps.js',
+  },
+  bootstrap: null,
+  mocha: {
+    reporterOptions: {
+      'codeceptjs-cli-reporter': {
+        stdout: '-',
+        options: { steps: true },
+      },
+      mochawesome: {
+        stdout: 'functional/output/console.log',
+        options: {
+          reportDir: '',
+          reportName: 'index',
+          inlineAssets: true,
+        },
+      },
+    },
+  },
+  multiple: {
+    chrome: {
+      browsers: getBrowserConfig('chrome'),
+    },
+    firefox: {
+      browsers: getBrowserConfig('firefox'),
+    },
+    safari: {
+      browsers: getBrowserConfig('safari'),
+    },
+  },
+  name: 'Employment Tribunal Crossbrowser Tests',
+};
+
+exports.config = setupConfig;
