db7529efff0acd579049f423b7d466fb11c09b40
Marc Judge <marc.judge@HMCTS.NET>
2022-04-19 15:35:40 +0100

    Add complex types

---
+++ src/main/definitions/complexTypes/claimantIndividual.ts
@@ -0,0 +1,7 @@
+export interface ClaimantIndividual {
+  claimant_first_names: string;
+  claimant_last_name: string;
+  claimant_date_of_birth: string;
+  claimant_title1: string;
+  claimant_title_other: string;
+}
