24f714cd733505ecd2b0cea2c501ab4b840c4118
UDAYABHASKAR <udayabhaskar@UDAYABHASKARs-MacBook-Pro.local>
2022-05-06 09:23:41 +0100

    modified tests and configs

--- src/test/functional/features/etClaimForSomeoneElse.js
+++ src/test/functional/features/etClaimForSomeoneElse.js
@@ -1,11 +1,10 @@
 Feature('ET claim for someone else');
 const commonFlow = require('./commonFlow.js');
 const { I } = inject();
-const waitSeconds = 2;
 
 Scenario('Verify ET claim for someone else', async () => {
   commonFlow.initialPageFlow();
-  I.wait(waitSeconds);
+  I.seeElement('#lip-or-representative-2');
   I.checkOption('input[id=lip-or-representative-2]');
   I.click('Continue');
 });
