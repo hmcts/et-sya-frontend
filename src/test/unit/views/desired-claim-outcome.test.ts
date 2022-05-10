8adab3f86221c2f88d63a0f77a8255427ccf6731
Daryl Boam <daryl.boam@version1.com>
2022-05-10 16:34:56 +0100

    1521: change label for common button to save as draft and update the button labels for still working page

--- src/test/unit/views/desired-claim-outcome.test.ts
+++ src/test/unit/views/desired-claim-outcome.test.ts
@@ -49,8 +49,8 @@ describe('Desired Claim Outcome page', () => {
     expect(button[0].innerHTML).contains('Save and continue', 'Could not find the button');
   });
 
-  it('should display save for later button', () => {
+  it('should display Save as draft button', () => {
     const button = htmlRes.getElementsByClassName(buttonClass);
-    expect(button[1].innerHTML).contains('Save for later', 'Could not find the button');
+    expect(button[1].innerHTML).contains('Save as draft', 'Could not find the button');
   });
 });
