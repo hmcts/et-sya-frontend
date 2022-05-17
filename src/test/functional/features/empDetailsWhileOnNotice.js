9dc14597549f14399688ca81a3670bd665e15d6a
UDAYABHASKAR <udayabhaskar@UDAYABHASKARs-MacBook-Pro.local>
2022-05-17 13:32:21 +0100

    automation changes for failed tests

--- src/test/functional/features/empDetailsWhileOnNotice.js
+++ src/test/functional/features/empDetailsWhileOnNotice.js
@@ -28,12 +28,8 @@ Scenario('Claim while on notice for organisation', () => {
   I.click('#main-form-submit');
 
   I.see('Are you getting paid for working your notice period?');
-  I.checkOption('[name="noticePeriodLength"]');
-  I.fillField('[name="noticePeriodUnit"]', '1');
-  I.checkOption('#notice-period-paid');
+  I.checkOption('#notice-period');
 
-  I.fillField('[name="noticePeriodUnitPaid"]', '3');
-  I.checkOption('#notice-period-paid-2');
   I.click('#main-form-submit');
 
   I.see('What are your average weekly hours?');
@@ -41,25 +37,18 @@ Scenario('Claim while on notice for organisation', () => {
   I.fillField('#avg-weekly-hrs', '20');
   I.click('#main-form-submit');
 
-  I.see('Pay BEFORE tax');
   I.seeElement('#pay-before-tax');
   I.fillField('#pay-before-tax', '40000');
-  I.checkOption('input[id=pay-before-tax-interval-3]');
-  I.click('#main-form-submit');
-
-  I.see('Pay AFTER tax');
-  I.seeElement('#pay-after-tax');
   I.fillField('#pay-after-tax', '35000');
-  I.checkOption('input[id=pay-after-tax-interval-3]');
+  I.checkOption('input[id=pay-interval]');
   I.click('#main-form-submit');
 
-  I.see('Pension scheme');
-  I.seeElement('#pension-2');
-  I.checkOption('input[id=pension-2]');
+  I.seeElement('#pension');
+  I.checkOption('input[id=pension]');
+  //I.fillField('input[id=pension-contributions]','100');
   I.click('#main-form-submit');
 
   I.seeElement('#employee-benefits');
-  I.see('Do or did you receive any employee benefits?');
   I.checkOption('input[id=employee-benefits]');
   I.click('#main-form-submit');
 })
