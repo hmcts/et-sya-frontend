64335d68ec508cc29c41341038f12731be3fef1a
Jagtar Singh <jagtar.singh@hmcts.net>
2022-05-18 19:17:19 +0100

    [RET-1727] Employment details - Notice - Skeleton Pages

--- src/main/controllers/NoticeEndController.ts
+++ src/main/controllers/NoticeEndController.ts
@@ -34,7 +34,7 @@ export default class NoticeEndController {
 
   public post = (req: AppRequest, res: Response): void => {
     setUserCase(req, this.form);
-    handleSessionErrors(req, res, this.form, PageUrls.NOTICE_PAY);
+    handleSessionErrors(req, res, this.form, PageUrls.NOTICE_TYPE);
   };
 
   public get = (req: AppRequest, res: Response): void => {
