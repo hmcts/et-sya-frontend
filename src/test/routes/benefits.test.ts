b00ddfad051351cf3f2f89968403ca5964d65be8
lvo700 <lynnette.orr@HMCTS.net>
2022-05-20 09:17:01 +0100

    Added/updated test files

--- src/test/routes/benefits.test.ts
+++ src/test/routes/benefits.test.ts
@@ -1,23 +1,31 @@
 import request from 'supertest';
 
 import { app } from '../../main/app';
+import { StillWorking } from '../../main/definitions/case';
 import { PageUrls } from '../../main/definitions/constants';
+import { mockApp } from '../unit/mocks/mockApp';
 
 describe(`GET ${PageUrls.BENEFITS}`, () => {
   it('should return the benefits page', async () => {
-    const res = await request(app).get(PageUrls.BENEFITS);
+    const res = await request(
+      mockApp({
+        userCase: {
+          isStillWorking: StillWorking.WORKING || StillWorking.NOTICE || StillWorking.NO_LONGER_WORKING,
+        },
+      })
+    ).get(PageUrls.BENEFITS);
     expect(res.type).toStrictEqual('text/html');
     expect(res.status).toStrictEqual(200);
   });
 });
-// TODO change url from Home to Where do you work?
+
 describe(`on POST ${PageUrls.BENEFITS}`, () => {
-  test('should navigate to the home page when save and continue button is clicked', async () => {
+  test('should navigate to the new job page when save and continue button is clicked', async () => {
     await request(app)
       .post(PageUrls.BENEFITS)
       .expect(res => {
         expect(res.status).toStrictEqual(302);
-        expect(res.header['location']).toStrictEqual(PageUrls.HOME);
+        expect(res.header['location']).toStrictEqual(PageUrls.NEW_JOB);
       });
   });
 });
--- src/test/unit/views/new-job-start-date.test.ts
+++ src/test/unit/views/new-job-start-date.test.ts
@@ -1,18 +1,31 @@
+import fs from 'fs';
+import path from 'path';
+
 import { expect } from 'chai';
 import request from 'supertest';
 
 import { app } from '../../../main/app';
+import { PageUrls } from '../../../main/definitions/constants';
 
-const PAGE_URL = '/new-job-start-date';
+const newJobStartDateJsonRaw = fs.readFileSync(
+  path.resolve(__dirname, '../../../main/resources/locales/en/translation/new-job-start-date.json'),
+  'utf-8'
+);
+const newJobStartDateJson = JSON.parse(newJobStartDateJsonRaw);
+
 const titleClass = 'govuk-heading-xl';
-const expectedTitle = 'New job start date';
+const expectedTitle = newJobStartDateJson.h1;
 const buttonClass = 'govuk-button';
+const inputs = 'govuk-date-input__item';
+const expectedInputLabel1 = 'Day';
+const expectedInputLabel2 = 'Month';
+const expectedInputLabel3 = 'Year';
 
 let htmlRes: Document;
 describe('New Job Start Date page', () => {
   beforeAll(async () => {
     await request(app)
-      .get(PAGE_URL)
+      .get(PageUrls.NEW_JOB_START_DATE)
       .then(res => {
         htmlRes = new DOMParser().parseFromString(res.text, 'text/html');
       });
@@ -21,6 +34,27 @@ describe('New Job Start Date page', () => {
   it('should display title', () => {
     const title = htmlRes.getElementsByClassName(titleClass);
     expect(title[0].innerHTML).contains(expectedTitle, 'Page title does not exist');
+  });
+
+  it('should display 3 input fields', () => {
+    const radioButtons = htmlRes.getElementsByClassName(inputs);
+    expect(radioButtons.length).equal(3, `only ${radioButtons.length} found`);
+  });
+
+  it('should display inputs with valid labels', () => {
+    const radioButtons = htmlRes.getElementsByClassName(inputs);
+    expect(radioButtons[0].innerHTML).contains(
+      expectedInputLabel1,
+      'Could not find the radio button with label ' + expectedInputLabel1
+    );
+    expect(radioButtons[1].innerHTML).contains(
+      expectedInputLabel2,
+      'Could not find the radio button with label ' + expectedInputLabel2
+    );
+    expect(radioButtons[2].innerHTML).contains(
+      expectedInputLabel3,
+      'Could not find the radio button with label ' + expectedInputLabel3
+    );
   });
 
   it('should display Save and continue button', () => {
