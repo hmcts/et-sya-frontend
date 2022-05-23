9b1b2935c834e01ea631493b5b044f3879e2e2c2
jsinghm13 <95350927+jsinghm13@users.noreply.github.com>
2022-05-23 16:34:14 +0100

    Ret 1727v2 (#236)
    * [RET-1727] Fix employment details flow (notice) and update pages
    
    * [RET-1727] Fix: test routes, controllers and views
    
    * [RET-1727] Fix hint date value on Notice End page
    
    * [RET-1727] Fix Notice End page view test

--- src/main/controllers/NoticeEndController.ts
+++ src/main/controllers/NoticeEndController.ts
@@ -8,13 +8,14 @@ import { PageUrls, TranslationKeys } from '../definitions/constants';
 import { DateFormFields, DefaultDateFormFields } from '../definitions/dates';
 import { FormContent, FormFields } from '../definitions/form';
 import { saveForLaterButton, submitButton } from '../definitions/radios';
-import { UnknownRecord } from '../definitions/util-types';
+import { AnyRecord, UnknownRecord } from '../definitions/util-types';
 
 import { assignFormData, getPageContent, handleSessionErrors, setUserCase } from './helpers';
 
 const notice_dates: DateFormFields = {
   ...DefaultDateFormFields,
   id: 'notice-dates',
+  hint: (l: AnyRecord): string => l.hint,
   parser: (body: UnknownRecord): CaseDate => convertToDateObject('noticeEnds', body),
 };
 
