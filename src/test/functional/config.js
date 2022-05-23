5a2101820da65d2c492161f514f4c47ad6bbdc7a
UDAYABHASKAR <udayabhaskar@UDAYABHASKARs-MacBook-Pro.local>
2022-05-23 14:39:42 +0100

    refactored tests

--- src/test/functional/config.js
+++ src/test/functional/config.js
@@ -1,10 +1,10 @@
 const testHeadlessBrowser = true;
-const testUrl = 'https://et-sya-pr-233.service.core-compute-preview.internal';
+const testUrl = process.env.TEST_URL || 'http://localhost:3001';
 module.exports = {
   testUrl,
   name: 'et-ui-functional',
   testHeadlessBrowser: true,
-  tests: './features/**/employeeGotNewJob.js',
+  tests: './features/**/*js',
   reportFolder: './functional-output/reports',
   helpers: {
     Puppeteer: {
