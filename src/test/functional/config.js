b682e0130dfd23c5b166e2c7c197f691c3726413
UDAYABHASKAR <udayabhaskar@UDAYABHASKARs-MacBook-Pro.local>
2022-05-23 11:55:41 +0100

    accessibility changes

--- src/test/functional/config.js
+++ src/test/functional/config.js
@@ -1,10 +1,10 @@
 const testHeadlessBrowser = true;
-const testUrl = process.env.TEST_URL || 'http://localhost:3001';
+const testUrl = 'https://et-sya-pr-234.service.core-compute-preview.internal';
 module.exports = {
   testUrl,
   name: 'et-ui-functional',
   testHeadlessBrowser: true,
-  tests: './features/**/*js',
+  tests: './features/**/empDetailsWhileOnNotice.js',
   reportFolder: './functional-output/reports',
   helpers: {
     Puppeteer: {
