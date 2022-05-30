9eabcf09d417df0e142ef5d59170d7e4a2101e6b
lvo700 <lynnette.orr@HMCTS.net>
2022-05-30 17:06:37 +0100

    Amended test files

--- src/test/unit/controller/BenefitsController.test.ts
+++ src/test/unit/controller/BenefitsController.test.ts
@@ -19,8 +19,8 @@ describe('Benefits Controller', () => {
     expect(response.render).toHaveBeenCalledWith(TranslationKeys.BENEFITS, expect.anything());
   });
 
-  it('should render the new job page when no longer working and yes radio button is selected', () => {
-    const body = { employeeBenefits: YesOrNo.YES };
+  it('should render the new job page when on no longer working route, yes radio button is selected and valid benefits text entered', () => {
+    const body = { employeeBenefits: YesOrNo.YES, benefitsCharCount: 'Test benefits text' };
     const userCase = { isStillWorking: StillWorking.NO_LONGER_WORKING };
     const controller = new BenefitsController();
 
@@ -31,8 +31,8 @@ describe('Benefits Controller', () => {
     expect(res.redirect).toBeCalledWith(PageUrls.NEW_JOB);
   });
 
-  it('should render the home page when working or notice and no radio button is selected', () => {
-    const body = { employeeBenefits: YesOrNo.YES };
+  it('should render the home page when on working or notice route and no radio button is selected', () => {
+    const body = { employeeBenefits: '' };
     const userCase = { isStillWorking: StillWorking.WORKING || StillWorking.NOTICE };
     const controller = new BenefitsController();
 
