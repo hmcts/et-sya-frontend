8a498cb70c579e2a3ec465689a43b898220267c0
Jagtar Singh <jagtar.singh@hmcts.net>
2022-06-01 15:54:33 +0100

    [RET-1795] Fix: SonarCloud issues

--- src/test/unit/controller/NewJobStartDateController.test.ts
+++ src/test/unit/controller/NewJobStartDateController.test.ts
@@ -16,4 +16,41 @@ describe('New Job Start Date Controller', () => {
     controller.get(request, response);
     expect(response.render).toHaveBeenCalledWith(TranslationKeys.NEW_JOB_START_DATE, expect.anything());
   });
+
+  it('should redirect to the same screen when errors are present', () => {
+    const errors = [{ propertyName: 'newJobStartDate', errorType: 'dayRequired', fieldName: 'day' }];
+    const body = {
+      'newJobStartDate-day': '',
+      'newJobStartDate-month': '11',
+      'newJobStartDate-year': '2000',
+    };
+
+    const controller = new NewJobStartDateController();
+
+    const req = mockRequest({ body });
+    const res = mockResponse();
+    controller.post(req, res);
+
+    expect(req.session.userCase).toEqual({
+      dobDate: {
+        year: '2000',
+        month: '12',
+        day: '24',
+      },
+      newJobStartDate: {
+        day: '',
+        month: '11',
+        year: '2000',
+      },
+      id: '1234',
+      startDate: {
+        day: '21',
+        month: '04',
+        year: '2019',
+      },
+    });
+
+    expect(res.redirect).toBeCalledWith(req.path);
+    expect(req.session.errors).toEqual(errors);
+  });
 });
