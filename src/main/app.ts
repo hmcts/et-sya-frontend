e0d1a418a4b8937ecbae12af75c138690e0e958e
pmumbere <mumberepaul@yahoo.com>
2022-06-08 12:14:04 +0100

    RET-1796: resolved content security config issues

--- src/main/app.ts
+++ src/main/app.ts
@@ -44,11 +44,10 @@ new Nunjucks(developmentMode).enableFor(app);
 logger.info('Nunjucks');
 logMemUsage();
 
-new Helmet(
-  config.get('security'),
+new Helmet(config.get('security'), [
   config.get('services.idam.authorizationURL'),
-  config.get('services.pcq.url')
-).enableFor(app);
+  config.get('services.pcq.url'),
+]).enableFor(app);
 logger.info('Helmet');
 logMemUsage();
 
