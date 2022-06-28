97d4567c1ec11f08a43593cc1bcafc8436640cb4
Olayode Olatunji <100837661+olyode@users.noreply.github.com>
2022-06-28 10:55:31 +0100

    Ret 327 (#251)
    * added additional checkboxes for types of claims
    
    * added redirect logic and tests
    
    * refactored implementation from past employer to steps to making your claim
    
    * fixes type mismatch for steps to making claims
    
    * resolved review comments
    
    * fixed unfairdismissal logic and tests, added otherclaim type value test
    
    * fixed typo
    
    * fixed tester identified typo
    
    * fixed async catch
    
    * fixed type unhandled exception
    
    * fixed functional test
    
    Co-authored-by: Peter Moores <82939391+petermoores-version1@users.noreply.github.com>

--- src/test/routes/steps-to-making-your-claim.test.ts
+++ src/test/routes/steps-to-making-your-claim.test.ts
@@ -1,12 +1,21 @@
 import request from 'supertest';
 
 import { PageUrls } from '../../main/definitions/constants';
+import { TypesOfClaim } from '../../main/definitions/definition';
 import { mockApp } from '../unit/mocks/mockApp';
 
 describe(`GET ${PageUrls.CLAIM_STEPS}`, () => {
   it('should return the claim steps page', async () => {
-    const res = await request(mockApp({})).get(PageUrls.CLAIM_STEPS);
+    const userCase = { typeOfClaim: [TypesOfClaim.BREACH_OF_CONTRACT] };
+    const res = await request(mockApp({ userCase })).get(PageUrls.CLAIM_STEPS);
     expect(res.type).toStrictEqual('text/html');
     expect(res.statusCode).toStrictEqual(200);
+  });
+
+  it('should return the still working page when unfair dismissal is type of claim', async () => {
+    const userCase = { typeOfClaim: [TypesOfClaim.UNFAIR_DISMISSAL] };
+    const res = await request(mockApp({ userCase })).get(PageUrls.STILL_WORKING);
+    expect(res.type).toStrictEqual('text/html');
+    expect(res.status).toStrictEqual(200);
   });
 });
