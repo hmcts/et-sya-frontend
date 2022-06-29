c990a2f062a70026b1304a1438b3f0560102bdfd
Sunday Ayeni <sunnyayeni18@gmail.com>
2022-06-29 15:04:49 +0100

    remove redundant steps

--- src/test/functional/config.js
+++ src/test/functional/config.js
@@ -1,11 +1,7 @@
 const testHeadlessBrowser = true;
 const testUrl = process.env.TEST_URL || 'http://localhost:3001';
-const test_case_username = process.env.TEST_CASE_USERNAME || '';
-const test_case_password = process.env.TEST_CASE_PASSWORD || '';
 module.exports = {
   testUrl,
-  test_case_username,
-  test_case_password,
   name: 'et-ui-functional',
   testHeadlessBrowser: true,
   tests: './features/**/*js',
