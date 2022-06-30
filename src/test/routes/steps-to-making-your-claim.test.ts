5dd3046cd39a9831cdb65d1b060ace75308abccb
mehmet.dede <mehmet.dede@justice.gov.uk>
2022-06-30 19:23:52 +0100

    response value for the request was created without redisClient. mockApp method replaced with mockAppWithRedisClient at line 12.

--- src/test/routes/steps-to-making-your-claim.test.ts
+++ src/test/routes/steps-to-making-your-claim.test.ts
@@ -1,15 +1,26 @@
 import request from 'supertest';
 
+import { CaseDataCacheKey, CaseType, YesOrNo } from '../../main/definitions/case';
 import { PageUrls } from '../../main/definitions/constants';
 import { TypesOfClaim } from '../../main/definitions/definition';
-import { mockApp } from '../unit/mocks/mockApp';
+import { mockAppWithRedisClient, mockRedisClient, mockSession } from '../unit/mocks/mockApp';
 
 // There is no routing in this page that is why the only test is being able to get the page itself without any error.
 // Page has links which are tested on the view tests
 describe(`GET ${PageUrls.CLAIM_STEPS}`, () => {
   it('should return httpOK code(200) when page is requested', async () => {
-    const userCase = { typeOfClaim: [TypesOfClaim.BREACH_OF_CONTRACT] };
-    const res = await request(mockApp({ userCase })).get(PageUrls.CLAIM_STEPS);
+    const res = await request(
+      mockAppWithRedisClient({
+        session: mockSession([], [], []),
+        redisClient: mockRedisClient(
+          new Map<CaseDataCacheKey, string>([
+            [CaseDataCacheKey.CLAIMANT_REPRESENTED, YesOrNo.YES],
+            [CaseDataCacheKey.CASE_TYPE, CaseType.SINGLE],
+            [CaseDataCacheKey.TYPES_OF_CLAIM, JSON.stringify([TypesOfClaim.WHISTLE_BLOWING])],
+          ])
+        ),
+      })
+    ).get(PageUrls.CLAIM_STEPS);
     expect(res.type).toStrictEqual('text/html');
     expect(res.statusCode).toStrictEqual(200);
   });
