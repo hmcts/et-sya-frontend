74621ca84f4d1c0f3f1717ab6393d2681b296ff9
Itay-Kirsh <Itayk747@gmail.com>
2022-07-19 14:32:53 +0100

    1973. Add tests to new validator and to validation of fields.

--- src/test/unit/controller/GenderDetailsController.test.ts
+++ src/test/unit/controller/GenderDetailsController.test.ts
@@ -18,6 +18,61 @@ describe('Gender Details Controller', () => {
     expect(response.render).toHaveBeenCalledWith(TranslationKeys.GENDER_DETAILS, expect.anything());
   });
 
+  describe('should throw correct errors and stay on page', () => {
+    it('should require mandatory fields', () => {
+      const body = {
+        gender: '',
+        preferredTitle: '',
+      };
+
+      const req = mockRequest({ body });
+      const res = mockResponse();
+      new GenderDetailsController().post(req, res);
+
+      const expectedErrors = [
+        { propertyName: 'gender', errorType: 'required' },
+        { propertyName: 'preferredTitle', errorType: 'required' },
+      ];
+
+      expect(res.redirect).toBeCalledWith(req.path);
+      expect(req.session.errors).toEqual(expectedErrors);
+    });
+
+    it('should require other title given that preferred title is "other"', () => {
+      const body = {
+        gender: 'Male',
+        preferredTitle: 'Other',
+        otherTitlePreference: '',
+      };
+
+      const req = mockRequest({ body });
+      const res = mockResponse();
+      new GenderDetailsController().post(req, res);
+
+      const expectedErrors = [{ propertyName: 'otherTitlePreference', errorType: 'required' }];
+
+      expect(res.redirect).toBeCalledWith(req.path);
+      expect(req.session.errors).toEqual(expectedErrors);
+    });
+
+    it('should not allow numbers in the other title', () => {
+      const body = {
+        gender: 'Male',
+        preferredTitle: 'Other',
+        otherTitlePreference: '5',
+      };
+
+      const req = mockRequest({ body });
+      const res = mockResponse();
+      new GenderDetailsController().post(req, res);
+
+      const expectedErrors = [{ propertyName: 'otherTitlePreference', errorType: 'numberError' }];
+
+      expect(res.redirect).toBeCalledWith(req.path);
+      expect(req.session.errors).toEqual(expectedErrors);
+    });
+  });
+
   it('should assign userCase from the page form data', () => {
     const body = {
       gender: 'Male',
