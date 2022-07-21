a2d9730a0427ccb2b608ac68cecd44c2cca803a3
Itay-Kirsh <Itayk747@gmail.com>
2022-07-21 09:56:28 +0100

    RET-1929. Validation and errors for RET-1939.

--- src/test/unit/controller/ClaimTypeDiscriminationController.test.ts
+++ src/test/unit/controller/ClaimTypeDiscriminationController.test.ts
@@ -16,4 +16,30 @@ describe('Claim Type Discrimination Controller', () => {
     controller.get(request, response);
     expect(response.render).toHaveBeenCalledWith(TranslationKeys.CLAIM_TYPE_DISCRIMINATION, expect.anything());
   });
+
+  describe('Correct validation', () => {
+    it('should require input', () => {
+      const req = mockRequest({ body: {} });
+      const res = mockResponse();
+      new ClaimTypeDiscriminationController().post(req, res);
+
+      const expectedErrors = [{ propertyName: 'claimTypeDiscrimination', errorType: 'required' }];
+
+      expect(res.redirect).toBeCalledWith(req.path);
+      expect(req.session.errors).toEqual(expectedErrors);
+    });
+
+    it('should assign userCase from the page form data', () => {
+      const req = mockRequest({
+        body: {
+          claimTypeDiscrimination: ['age', 'sex'],
+        },
+      });
+      const res = mockResponse();
+
+      new ClaimTypeDiscriminationController().post(req, res);
+
+      expect(req.session.userCase).toMatchObject({ claimTypeDiscrimination: ['age', 'sex'] });
+    });
+  });
 });
