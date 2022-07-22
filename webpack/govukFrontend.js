612c4ee499434c91016b483b55c3671616557cd2
Marc Judge <marc.judge@HMCTS.NET>
2022-07-22 12:47:58 +0100

    Code cleanup

--- webpack/govukFrontend.js
+++ webpack/govukFrontend.js
@@ -2,7 +2,8 @@ const path = require('path');
 
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const root = path.resolve(require.resolve('govuk-frontend'), '../');
+const packageJson = path.resolve('node_modules', 'govuk-frontend', 'package.json');
+const root = path.resolve(packageJson, '..', 'govuk');
 const sass = path.resolve(root, 'all.scss');
 const javascript = path.resolve(root, 'all.js');
 const components = path.resolve(root, 'components');
