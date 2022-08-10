dea2d638b6e3783da26448901efae02b25bfa045
mehmet.dede <mehmet.dede@justice.gov.uk>
2022-08-10 14:59:10 +0100

    Removed dynamic-styles and added hyphens attribute to all div sections in order to make hyphens available to all wordings. Added set-focus script to index.js to focus errors when any error occurs. This change is implemented for accessibility enhancements.

---
+++ src/main/assets/js/set-focus.ts
@@ -0,0 +1,10 @@
+// Autofocus section
+// Code below automatically focuses to errors, in case any error exists on the page for accessibility
+{
+  const govUKErrors = Array.from(
+    document.getElementsByClassName('govuk-error-summary__body') as HTMLCollectionOf<HTMLElement>
+  );
+  if (govUKErrors !== null && govUKErrors !== undefined && govUKErrors.length > 0) {
+    govUKErrors[0].focus();
+  }
+}
