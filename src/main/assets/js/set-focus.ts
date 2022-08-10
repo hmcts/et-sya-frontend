1f556bebdc5678b4ea6b50421f51df3eb8331818
mehmet.dede <mehmet.dede@justice.gov.uk>
2022-08-10 16:04:40 +0100

    Updated focused area to gov uk error summary title.

--- src/main/assets/js/set-focus.ts
+++ src/main/assets/js/set-focus.ts
@@ -2,7 +2,7 @@
 // Code below automatically focuses to errors, in case any error exists on the page for accessibility
 {
   const govUKErrors = Array.from(
-    document.getElementsByClassName('govuk-error-summary__body') as HTMLCollectionOf<HTMLElement>
+    document.getElementsByClassName('govuk-error-summary__title') as HTMLCollectionOf<HTMLElement>
   );
   if (govUKErrors !== null && govUKErrors !== undefined && govUKErrors.length > 0) {
     govUKErrors[0].focus();
