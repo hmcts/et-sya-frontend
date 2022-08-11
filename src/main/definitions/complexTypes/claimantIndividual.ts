10f9a97d7a63ecca96abadedac02f0a7360895e1
Itay-Kirsh <Itayk747@gmail.com>
2022-08-11 14:52:57 +0100

    RET-1974. Save data and final checks for RET-349 and RET-835. (#298)

--- src/main/definitions/complexTypes/claimantIndividual.ts
+++ src/main/definitions/complexTypes/claimantIndividual.ts
@@ -1,7 +1,12 @@
+import { GenderTitle, Sex, YesOrNoOrPreferNot } from '../case';
+
 export interface ClaimantIndividual {
   claimant_first_names?: string;
   claimant_last_name?: string;
   claimant_date_of_birth?: string;
-  claimant_title1?: string;
-  claimant_title_other?: string;
+  claimant_sex?: Sex;
+  claimant_gender_identity_same?: YesOrNoOrPreferNot;
+  claimant_gender_identity?: string;
+  claimant_preferred_title?: GenderTitle;
+  claimant_title_other?: string; // custom title
 }
