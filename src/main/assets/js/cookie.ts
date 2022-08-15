55832199bf8df1424c44b3361ae57ab269fa94f1
lvo700 <lynnette.orr@HMCTS.net>
2022-08-15 12:53:37 +0100

    Added cookie files. Updated routes, template, constants

---
+++ src/main/assets/js/cookie.ts
@@ -0,0 +1,87 @@
+import cookieManager from '@hmcts/cookie-manager';
+
+cookieManager.on('PreferenceFormSubmitted', () => {
+  const message = document.querySelector('.cookie-preference-success') as HTMLElement;
+  console.log('doing it');
+  message.style.display = 'block';
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
+cookieManager.on('UserPreferencesLoaded', (preferences: { apm: string }) => {
+  const dataLayer = window.dataLayer || [];
+  dataLayer.push({ event: 'Cookie Preferences', cookiePreferences: preferences });
+});
+
+cookieManager.on('UserPreferencesSaved', (preferences: { apm: string }) => {
+  const dataLayer = window.dataLayer || [];
+  const dtrum = window.dtrum;
+
+  dataLayer.push({ event: 'Cookie Preferences', cookiePreferences: preferences });
+
+  if (dtrum !== undefined) {
+    if (preferences.apm === 'on') {
+      dtrum.enable();
+      dtrum.enableSessionReplay();
+    } else {
+      dtrum.disableSessionReplay();
+      dtrum.disable();
+    }
+  }
+});
+const config = {
+  userPreferences: {
+    cookieName: 'et-sya-cookie-preferences',
+    cookieExpiry: 365,
+    cookieSecure: false,
+  },
+  cookieBanner: {
+    class: 'cookie-banner',
+    actions: [
+      {
+        name: 'accept',
+        buttonClass: 'cookie-banner-accept-button',
+        confirmationClass: 'cookie-banner-accept-message',
+        consent: true,
+      },
+      {
+        name: 'reject',
+        buttonClass: 'cookie-banner-reject-button',
+        confirmationClass: 'cookie-banner-reject-message',
+        consent: false,
+      },
+      {
+        name: 'hide',
+        buttonClass: 'cookie-banner-hide-button',
+      },
+    ],
+  },
+  preferencesForm: {
+    class: 'cookie-preferences-form',
+  },
+  cookieManifest: [
+    {
+      categoryName: 'analytics',
+      cookies: ['_ga', '_gid', '_gat_UA-'],
+    },
+    {
+      categoryName: 'apm',
+      cookies: ['dtCookie', 'dtLatC', 'dtPC', 'dtSa', 'rxVisitor', 'rxvt'],
+    },
+  ],
+};
+
+cookieManager.init(config);
+
+declare global {
+  interface Window {
+    dataLayer: Record<string, unknown>[];
+    dtrum: DtrumApi;
+  }
+}
+
+interface DtrumApi {
+  enable(): void;
+  enableSessionReplay(): void;
+  disable(): void;
+  disableSessionReplay(): void;
+}
