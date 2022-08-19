c8136370e3d13f4f29af199c57700bfc5306729f
pmumbere <mumberepaul@yahoo.com>
2022-08-19 12:14:11 +0100

    RET-2260: Moved content to translation file
    Co-authored-by: Itay-Kirsh <Itay-Kirsh@users.noreply.github.com>

--- src/main/controllers/CitizenHubController.ts
+++ src/main/controllers/CitizenHubController.ts
@@ -2,6 +2,29 @@ import { Response } from 'express';
 
 import { AppRequest } from '../definitions/appRequest';
 import { PageUrls, TranslationKeys } from '../definitions/constants';
+import { AnyRecord } from '../definitions/util-types';
+
+// const sections: AnyRecord[] = [
+//   {
+//     sectionTitle: 'Section 1',
+//     sectionLinks: ['<a href="#">Link 1</a>'],
+//   },
+// ];
+
+// todo getSectionStatus(userCase?.personalDetailsCheck, userCase?.dobDate)
+const sections = [
+  {
+    title: (l: AnyRecord): string => l.section1.title,
+    links: [
+      {
+        url: PageUrls.HOME,
+        linkTxt: (l: AnyRecord): string => l.section1.link1Text,
+        status: (l: AnyRecord): string => l.completed.title,
+        statusColor: (l: AnyRecord): string => l.completed.style,
+      },
+    ],
+  },
+];
 
 export default class CitizenHubController {
   public get(req: AppRequest, res: Response): void {
@@ -10,6 +33,7 @@ export default class CitizenHubController {
       ...req.t(TranslationKeys.CITIZEN_HUB, { returnObjects: true }),
       PageUrls,
       userCase: req.session?.userCase,
+      sections,
     });
   }
 }
