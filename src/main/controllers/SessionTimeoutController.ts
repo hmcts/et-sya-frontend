cc90cffb7373e59b9008177638095fceab8844ab
Brendan McHugh <brendan.mchugh@hmcts.net>
2022-08-25 09:48:54 +0100

    controller to use a class instead of functions

--- src/main/controllers/SessionTimeoutController.ts
+++ src/main/controllers/SessionTimeoutController.ts
@@ -1,5 +1,5 @@
 import config from 'config';
-import { NextFunction, Response } from 'express';
+import { Response } from 'express';
 import moment from 'moment';
 import { LoggerInstance } from 'winston';
 
@@ -18,21 +18,24 @@ export default class SessionTimeoutController {
   public getSessionEnded = (req: AppRequest, res: Response): void => {
     this.logger.info('Session ending and being destroyed');
     res.locals.isLoggedIn = false;
-    req.session.destroy(() => res.redirect(PageUrls.HOME));
+    req.session.destroy(() => {
+      this.logger.info('inside session destroy, redirecting to home');
+      res.redirect(PageUrls.HOME);
+    });
   };
 }
 
-export const getSessionEnded = (req: AppRequest, res: Response): void => {
-  console.log('Session ending and being destroyed');
-  res.locals.isLoggedIn = false;
-  req.session.destroy(() => res.redirect(PageUrls.HOME));
-};
+// export const getSessionEnded = (req: AppRequest, res: Response): void => {
+//   console.log('Session ending and being destroyed');
+//   res.locals.isLoggedIn = false;
+//   req.session.destroy(() => res.redirect(PageUrls.HOME));
+// };
 
-export const checkSession = (req: AppRequest, res: Response, next: NextFunction): void | undefined => {
-  if (req.session?.user?.accessToken) {
-    console.log('user has access token');
-    next();
-  }
-  console.log('user does not have access token, returning.... ');
-  return;
-};
+// export const checkSession = (req: AppRequest, res: Response, next: NextFunction): void | undefined => {
+//   if (req.session?.user?.accessToken) {
+//     console.log('user has access token')
+//     next();
+//   }
+//   console.log('user does not have access token, returning.... ')
+//   return
+// }
