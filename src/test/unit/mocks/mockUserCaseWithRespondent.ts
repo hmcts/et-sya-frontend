dd9f9f62df3aab0e888606cd61445a4045b7f2f1
mehmet.dede <mehmet.dede@justice.gov.uk>
2022-08-29 17:38:51 +0100

    Added test to RespondentDetailsCheckController.test.ts to check if it returns PageUrls.RESPONDENT_DETAILS_CHECK and have 6 respondents defined in the session when a new respondent is tried to be defined after 6 respondents.

--- src/test/unit/mocks/mockUserCaseWithRespondent.ts
+++ src/test/unit/mocks/mockUserCaseWithRespondent.ts
@@ -10,3 +10,34 @@ export const userCaseWithRespondent = {
     },
   ],
 };
+
+export const userCaseWith6Respondent = {
+  id: '12354',
+  state: CaseState.AWAITING_SUBMISSION_TO_HMCTS,
+  respondents: [
+    {
+      respondentNumber: 1,
+      respondentName: 'Globo Gym 1',
+    },
+    {
+      respondentNumber: 2,
+      respondentName: 'Globo Gym 2',
+    },
+    {
+      respondentNumber: 3,
+      respondentName: 'Globo Gym 3',
+    },
+    {
+      respondentNumber: 4,
+      respondentName: 'Globo Gym 4',
+    },
+    {
+      respondentNumber: 5,
+      respondentName: 'Globo Gym 5',
+    },
+    {
+      respondentNumber: 6,
+      respondentName: 'Globo Gym 6',
+    },
+  ],
+};
