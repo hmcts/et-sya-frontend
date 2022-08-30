9705587c2c4f02da0752e0b4b09d9c4e9700b575
dharrisMOJ <97886351+dharrisMOJ@users.noreply.github.com>
2022-08-30 12:00:43 +0100

    Feature/refactor e2e (#319)
    * Initial refactor of e2e tests
    
    * Refactor project folders and new feature for personal details
    
    * Remove common flow
    
    * Add new did you work page and refactor common flow
    
    * Personal Details Feature Added
    
    * Ret 2195 employment details final checks (#312)
    
    * testing pipeline failures demo
    
    * rebased to another branch to fix pipeline
    
    * updated new branch
    
    * fixed notice-length punctuation
    
    Co-authored-by: marcjudge <98343606+marcjudge@users.noreply.github.com>
    
    * Ret 1762 - ET1 Form- Google Tag Manager installation (#310)
    
    * Added GA scripts to njk template
    
    * Added cookie banner files
    
    * Added cookie files. Updated routes, template, constants
    
    * Removed console log, added cookies page to nosigninreq
    
    * Moved GA tags further up the page
    
    * Amendments to cookie page and banner
    
    * Added cookie mgr to package.json
    
    * Adding yarn.lock, package.json
    
    * Fixed failing button/text positions after adding cookie banner
    
    * Fixed typo in session name
    
    * Changed cookie banner visibility to hidden
    
    * Added content to cookie preferences page
    
    * Added preferences interface
    
    * Added backlink functionality for notification banner
    
    * Import cookie file to index.js
    
    * Amended cookies file
    
    * Changed apostrophes to fix failing tests
    
    * Added test files for cookie page
    
    * Fixed failing test
    
    * Added data layer
    
    * Added cookie parser to session
    
    * Updated helmet config
    
    * Moved radio button text to json file
    
    Co-authored-by: V1\OrrL <lynnette.orr@version1.com>
    
    * Updated error message and title (#317)
    
    * update config and failing test
    
    * amend username within config
    
    Co-authored-by: Anishkumar Mistry <anishkumarmistry@Anishkumars-MacBook-Pro.local>
    Co-authored-by: Olayode Olatunji <100837661+olyode@users.noreply.github.com>
    Co-authored-by: marcjudge <98343606+marcjudge@users.noreply.github.com>
    Co-authored-by: lvo700 <lynnette.orr@hmcts.net>
    Co-authored-by: V1\OrrL <lynnette.orr@version1.com>

---
+++ src/test/end-to-end/features/EmploymentRespondentDetails/verifyEmploymentAndRespondentDetails.js
@@ -0,0 +1,18 @@
+const { doNotHaveToCompleteCard, didYouWorkForOrganisation } = require('../../helpers/caseHelper');
+const commonFlow = require('../../helpers/commonFlow.js');
+
+Feature('Enter employment and respondent details for claim');
+
+Scenario("Did you work for the organisation or person you're making your claim against? - NO", async ({ I }) => {
+  await commonFlow.createSingleMyselfCase();
+  await I.authenticateWithIdam();
+  await doNotHaveToCompleteCard(I);
+  await didYouWorkForOrganisation(I, 'No');
+}).tag('@RET-WIP');
+
+Scenario("Did you work for the organisation or person you're making your claim against? - YES", async ({ I }) => {
+  await commonFlow.createSingleMyselfCase();
+  await I.authenticateWithIdam();
+  await doNotHaveToCompleteCard(I);
+  await didYouWorkForOrganisation(I, 'Yes');
+}).tag('@RET-WIP');
