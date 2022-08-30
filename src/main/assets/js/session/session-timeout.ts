0f865279f342af2a2d3a6a7cb57fe1b783aa2821
Brendan McHugh <brendan.mchugh@hmcts.net>
2022-08-30 11:15:37 +0100

    redirect to logout instead of session-ended

--- src/main/assets/js/session/session-timeout.ts
+++ src/main/assets/js/session/session-timeout.ts
@@ -152,8 +152,7 @@ export default class SessionTimeout {
 
   signOut(): void {
     console.log('signout begins');
-    window.location.assign('/session-ended');
-    // window.location.assign('/logout');
+    window.location.assign('/logout?redirectUrl=/');
   }
 
   extendSession = (): Promise<void> => {
