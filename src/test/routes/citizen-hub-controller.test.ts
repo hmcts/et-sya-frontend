1622e04467071758f9071db6f680d02dc160e9ee
Itay-Kirsh <Itayk747@gmail.com>
2022-08-31 10:49:28 +0100

    RET-2260. Clean routes test.

--- src/test/routes/citizen-hub-controller.test.ts
+++ src/test/routes/citizen-hub-controller.test.ts
@@ -8,19 +8,17 @@ import { CaseApi } from '../../main/services/CaseService';
 import { mockApp } from '../unit/mocks/mockApp';
 
 jest.mock('axios');
-const mockClient = jest.spyOn(CaseService, 'getCaseApi');
 const caseApi = new CaseApi(axios as jest.Mocked<typeof axios>);
+caseApi.getCase = jest.fn().mockResolvedValue(
+  Promise.resolve({
+    data: {},
+  } as AxiosResponse<CaseApiDataResponse>)
+);
+
+const mockClient = jest.spyOn(CaseService, 'getCaseApi');
+mockClient.mockReturnValue(caseApi);
 
 describe(`GET ${PageUrls.CITIZEN_HUB}`, () => {
-  beforeAll(() => {
-    mockClient.mockReturnValue(caseApi);
-    caseApi.getCase = jest.fn().mockResolvedValue(
-      Promise.resolve({
-        data: {},
-      } as AxiosResponse<CaseApiDataResponse>)
-    );
-  });
-
   it('should return the citizen hub page', async () => {
     const res = await request(mockApp({})).get(PageUrls.CITIZEN_HUB.replace(':caseId', '1111222233334444'));
 
