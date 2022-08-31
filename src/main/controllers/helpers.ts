c1fcbef4a407b245015a74a8ff7bf7ac8cc377a0
Daryl Boam <daryl.boam@version1.com>
2022-08-31 12:21:16 +0100

    2256: remove

--- src/main/controllers/helpers.ts
+++ src/main/controllers/helpers.ts
@@ -384,16 +384,15 @@ export const handleUpdateDraftCase = (req: AppRequest, logger: LoggerInstance): 
 };
 
 export const handleUploadDocument = (req: AppRequest, file: UploadedFile, logger: LoggerInstance): void => {
-  if (!req.session.errors.length) {
-    getCaseApi(req.session.user?.accessToken)
-      .uploadDocument(file, 'ET_EnglandWales')
-      .then((res: AxiosResponse<DocumentUploadResponse>) => {
-        logger.info(`Uploaded document to: ${fromApiFormatDocument(res.data)}`);
-      })
-      .catch(err => {
-        logger.error(err.message);
-      });
-  }
+  getCaseApi(req.session.user?.accessToken)
+    .uploadDocument(file, 'ET_EnglandWales')
+    .then((res: AxiosResponse<DocumentUploadResponse>) => {
+      return fromApiFormatDocument(res.data);
+    })
+    .catch(err => {
+      logger.error(err.message);
+      return null;
+    });
 };
 
 export const resetValuesIfNeeded = (formData: Partial<CaseWithId>): void => {
