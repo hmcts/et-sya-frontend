4a64c4f611fa6035731e439d3927b672d1630500
Brendan McHugh <brendan.mchugh@hmcts.net>
2022-09-13 20:27:33 +0100

    add unit test for caseDocumentController

--- src/main/controllers/CaseDocumentController.ts
+++ src/main/controllers/CaseDocumentController.ts
@@ -10,9 +10,14 @@ const logger = Logger.getLogger('CaseDocumentController');
 export default class CaseDocumentController {
   public async get(req: AppRequest, res: Response): Promise<void> {
     try {
+      if (!req.params || !req.params.docId || req.params.docId === '') {
+        throw new Error('bad request paramater');
+      }
+
       const docId = req.params.docId;
       const sessionDoc = req.session.userCase.acknowledgementOfClaimLetterDetail.find(doc => doc.id === docId);
       const document = await getCaseApi(req.session.user?.accessToken).getCaseDocument(docId);
+
       res.setHeader('Content-Type', sessionDoc.mimeType);
       res.status(200).send(Buffer.from(document.data, 'binary'));
     } catch (error) {
---
+++ src/test/unit/controller/CaseDocumentController.test.ts
@@ -0,0 +1,83 @@
+import { AxiosResponse } from 'axios';
+
+import CaseDocumentController from '../../../main/controllers/CaseDocumentController';
+import * as caseApi from '../../../main/services/CaseService';
+import { mockRequest } from '../mocks/mockRequest';
+import { mockResponse } from '../mocks/mockResponse';
+
+describe('Case Document Controller', () => {
+  const t = {};
+  const getCaseApiMock = jest.spyOn(caseApi, 'getCaseApi');
+
+  it('should retrieve the binary document', async () => {
+    const response = mockResponse();
+    const request = mockRequest({ t });
+    request.params.docId = '1234';
+    request.session.user = {
+      id: '1',
+      givenName: 'Bobby',
+      familyName: 'Ryan',
+      email: 'bobby@gmail.com',
+      accessToken: 'token',
+      isCitizen: true,
+    };
+    request.session.userCase.acknowledgementOfClaimLetterDetail = [
+      {
+        id: '1234',
+        description: 'desc',
+        mimeType: 'test mimeType',
+      },
+    ];
+    const fetchResponse = {
+      status: 200,
+      data: 'someBinaryContent',
+    } as AxiosResponse;
+
+    (getCaseApiMock as jest.Mock).mockReturnValue({
+      getCaseDocument: jest.fn(() => {
+        return fetchResponse;
+      }),
+    });
+
+    const expectedBuffer = Buffer.from(fetchResponse.data, 'binary');
+    await new CaseDocumentController().get(request, response);
+    expect(response.setHeader).toHaveBeenCalledWith('Content-Type', 'test mimeType');
+    expect(response.status).toHaveBeenCalledWith(200);
+    expect(response.send).toHaveBeenCalledWith(expectedBuffer);
+  });
+
+  it('should redirect to not-found on error during request', async () => {
+    const response = mockResponse();
+    const request = mockRequest({ t });
+    request.params.docId = '1234';
+    request.session.user = {
+      id: '1',
+      givenName: 'Bobby',
+      familyName: 'Ryan',
+      email: 'bobby@gmail.com',
+      accessToken: 'token',
+      isCitizen: true,
+    };
+    request.session.userCase.acknowledgementOfClaimLetterDetail = [
+      {
+        id: '1234',
+        description: 'desc',
+        mimeType: 'test mimeType',
+      },
+    ];
+
+    (getCaseApiMock as jest.Mock).mockReturnValue({
+      getCaseDocument: jest.fn().mockRejectedValue(new Error('test error message')),
+    });
+    await new CaseDocumentController().get(request, response);
+    expect(response.redirect).toHaveBeenCalledWith('not-found');
+  });
+
+  it('should redirect to not-found on bad request parameter', async () => {
+    const response = mockResponse();
+    const request = mockRequest({ t });
+    request.params.docId = '';
+    await new CaseDocumentController().get(request, response);
+    expect(response.redirect).toHaveBeenCalledWith('not-found');
+  });
+});
