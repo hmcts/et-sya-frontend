b52b6ecb8c774746b3b9de6aaf09ae6a650b1340
Brendan McHugh <brendan.mchugh@hmcts.net>
2022-09-17 20:23:35 +0100

    changes requested

--- src/main/controllers/CaseDocumentController.ts
+++ src/main/controllers/CaseDocumentController.ts
@@ -10,14 +10,16 @@ const logger = Logger.getLogger('CaseDocumentController');
 export default class CaseDocumentController {
   public async get(req: AppRequest, res: Response): Promise<void> {
     try {
-      if (!req.params || !req.params.docId || req.params.docId === '') {
-        throw new Error('bad request paramater');
+      if (!req.params?.docId) {
+        logger.info('bad request parameter');
+        return res.redirect('/citizen-hub/' + req.session?.userCase?.id);
       }
       const docId = req.params.docId;
       const { mimeType } = req.session.userCase.acknowledgementOfClaimLetterDetail.find(doc => doc.id === docId);
 
       if (!mimeType) {
-        throw new Error('requested document not found in userCase');
+        logger.info('requested document not found in userCase');
+        return res.redirect('/citizen-hub/' + req.session?.userCase?.id);
       }
       const document = await getCaseApi(req.session.user?.accessToken).getCaseDocument(docId);
 
--- src/main/controllers/CitizenHubAcknowledgementController.ts
+++ src/main/controllers/CitizenHubAcknowledgementController.ts
@@ -10,10 +10,10 @@ const logger = Logger.getLogger('CitizenHubAcknowledgementController');
 
 export default class CitizenHubAcknowledgementController {
   public get = async (req: AppRequest, res: Response): Promise<void> => {
+    if (!req.session?.userCase?.acknowledgementOfClaimLetterDetail?.length) {
+      return res.redirect('/citizen-hub/' + req.session?.userCase?.id);
+    }
     try {
-      if (!req.session?.userCase?.acknowledgementOfClaimLetterDetail?.length) {
-        return res.redirect('/citizen-hub/' + req.session?.userCase?.id);
-      }
       for await (const document of req.session.userCase.acknowledgementOfClaimLetterDetail) {
         const docDetails = await getCaseApi(req.session.user?.accessToken).getDocumentDetails(document.id);
         const { createdOn, size, mimeType, originalDocumentName } = docDetails.data;
@@ -35,6 +35,7 @@ export default class CitizenHubAcknowledgementController {
       logger.error(err.response?.status, err.response?.data, err);
       return res.redirect('/not-found');
     }
+
     res.render(TranslationKeys.CITIZEN_HUB_ACKNOWLEDGEMENT, {
       ...req.t(TranslationKeys.COMMON, { returnObjects: true }),
       ...req.t(TranslationKeys.CITIZEN_HUB_ACKNOWLEDGEMENT, { returnObjects: true }),
