422a7aff2a80f86fed90f676fa00c169d562137e
dharrisMOJ <97886351+dharrisMOJ@users.noreply.github.com>
2022-09-21 16:58:21 +0100

    Feature/employment details journey (#347)

--- src/test/end-to-end/helpers/caseHelper.js
+++ src/test/end-to-end/helpers/caseHelper.js
@@ -6,12 +6,37 @@ async function didYouWorkForOrganisation(I, orgOption) {
   await I.didYouWorkForOrganisation(orgOption);
 }
 
-async function personalDetails(I) {
+async function enterPersonalDetails(I) {
   await I.personalDetails();
+}
+
+async function areYouStillWorkingForOrg(I, workingOption) {
+  await I.areYouStillWorkingForOrganisation(workingOption);
+}
+
+async function stillWorkingForRespondentJourney(I, noticePeriodContract, noticePeriod) {
+  await I.stillWorkingJourney(noticePeriodContract, noticePeriod);
+}
+
+async function workingNoticePeriodForRespondentJourney(I, noticePeriodLength) {
+  await I.workingNoticePeriodJourney(noticePeriodLength);
+}
+
+async function noLongerWorkingForRespondentJourney(I, noticePeriod, newJob, noticePeriodLength) {
+  await I.noLongerWorkingJourney(noticePeriod, newJob, noticePeriodLength);
+}
+
+async function enterRespondentDetailsJourney(I, workAddress, doYouHaveAcas) {
+  await I.respondentDetailsJourney(workAddress, doYouHaveAcas);
 }
 
 module.exports = {
   doNotHaveToCompleteCard,
   didYouWorkForOrganisation,
-  personalDetails,
+  enterPersonalDetails,
+  areYouStillWorkingForOrg,
+  stillWorkingForRespondentJourney,
+  workingNoticePeriodForRespondentJourney,
+  noLongerWorkingForRespondentJourney,
+  enterRespondentDetailsJourney,
 };
