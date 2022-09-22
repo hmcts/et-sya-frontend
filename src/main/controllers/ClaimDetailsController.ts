68446d43140e96611279718f1082f808b0bd29a2
Brendan McHugh <brendan.mchugh@hmcts.net>
2022-09-22 14:05:11 +0100

    add acknowledgement and rejection links to details page

--- src/main/controllers/ClaimDetailsController.ts
+++ src/main/controllers/ClaimDetailsController.ts
@@ -3,8 +3,24 @@ import { Response } from 'express';
 import { AppRequest } from '../definitions/appRequest';
 import { PageUrls, TranslationKeys } from '../definitions/constants';
 
+import { combineDocuments, getDocumentDetails } from './helpers/DocumentHelpers';
+
+const { Logger } = require('@hmcts/nodejs-logging');
+
+const logger = Logger.getLogger('ClaimDetailsController');
+
 export default class ClaimDetailsController {
-  public get = (req: AppRequest, res: Response): void => {
+  public get = async (req: AppRequest, res: Response): Promise<void> => {
+    const { userCase } = req.session;
+
+    try {
+      await getDocumentDetails(
+        combineDocuments(userCase?.acknowledgementOfClaimLetterDetail, userCase?.rejectionOfClaimDocumentDetail),
+        req.session.user?.accessToken
+      );
+    } catch (err) {
+      logger.error(err.response?.status, err.response?.data, err);
+    }
     res.render(TranslationKeys.CLAIM_DETAILS, {
       ...req.t(TranslationKeys.COMMON, { returnObjects: true }),
       ...req.t(TranslationKeys.CLAIM_DETAILS, { returnObjects: true }),
