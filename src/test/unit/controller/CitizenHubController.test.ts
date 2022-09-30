b076ebbca71fb722670cc16e1684310522762352
Daryl Boam <daryl.boam@version1.com>
2022-09-30 16:56:04 +0100

    2577: cit hub full coverage

--- src/test/unit/controller/CitizenHubController.test.ts
+++ src/test/unit/controller/CitizenHubController.test.ts
@@ -23,7 +23,17 @@ caseApi.getUserCase = jest.fn().mockResolvedValue(
   } as AxiosResponse<CaseApiDataResponse>)
 );
 
+const OLD_ENV = process.env;
+
 describe('Citizen Hub Controller', () => {
+  beforeEach(() => {
+    jest.resetModules();
+    process.env = { ...OLD_ENV };
+  });
+  afterAll(() => {
+    process.env = OLD_ENV;
+  });
+
   it('should redirect to not found with request.url when case api fails', async () => {
     const controller = new CitizenHubController();
     caseApi.getUserCase = jest.fn().mockRejectedValueOnce('error');
@@ -32,5 +42,24 @@ describe('Citizen Hub Controller', () => {
     controller.get(req, res);
     await new Promise(nextTick);
     expect(res.redirect).toHaveBeenCalledWith('/not-found');
+  });
+
+  it('should assign mock user when in test', async () => {
+    const controller = new CitizenHubController();
+    caseApi.getUserCase = jest.fn().mockResolvedValue(
+      Promise.resolve({
+        data: {
+          created_date: '2022-08-19T09:19:25.79202',
+          last_modified: '2022-08-19T09:19:25.817549',
+        },
+      } as AxiosResponse<CaseApiDataResponse>)
+    );
+    const res = mockResponse();
+    const req = mockRequest({});
+    req.params.caseId = 'a11y';
+    process.env.IN_TEST = 'true';
+    controller.get(req, res);
+    await new Promise(nextTick);
+    expect(req.session.userCase).toBeDefined();
   });
 });
