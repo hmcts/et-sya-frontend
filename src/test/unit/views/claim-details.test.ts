3f5dd2c4470528d8d9a196382c1cb6aeb9cee35d
Itay-Kirsh <Itayk747@gmail.com>
2022-10-05 12:09:24 +0100

    Document tests.

--- src/test/unit/views/claim-details.test.ts
+++ src/test/unit/views/claim-details.test.ts
@@ -1,10 +1,9 @@
 import axios, { AxiosResponse } from 'axios';
-import request from 'supertest';
 
 import * as caseService from '../../../main/services/CaseService';
 import { CaseApi } from '../../../main/services/CaseService';
-import { mockApp } from '../mocks/mockApp';
 import mockUserCaseComplete from '../mocks/mockUserCaseComplete';
+import { getHtmlRes } from '../test-helpers/requester';
 
 const PAGE_URL = '/claim-details';
 const expectedTitle = 'Your claim details';
@@ -12,6 +11,11 @@ const titleClass = 'govuk-heading-xl';
 const summaryListClass = 'govuk-summary-list';
 const summaryListHeadingClass = 'govuk-summary-list__key govuk-heading-m';
 const summaryListKeyExcludeHeadingClass = '.govuk-summary-list__key:not(.govuk-heading-m)';
+
+const tableSelector = 'govuk-table';
+const rowSelector = 'govuk-table__row';
+const cellSelector = 'govuk-table__cell';
+const tableHeaderSelector = 'govuk-table__header';
 
 const axiosResponse: AxiosResponse = {
   data: {
@@ -37,21 +41,19 @@ const axiosResponse: AxiosResponse = {
 
 jest.mock('axios');
 const caseApi = new CaseApi(axios as jest.Mocked<typeof axios>);
-caseApi.getDocumentDetails = jest.fn().mockResolvedValue(axiosResponse); // todo vary the documents' date and size
+caseApi.getDocumentDetails = jest.fn(caseId => {
+  if (caseId === 'a0c113ec-eede-472a-a59c-f2614b48177c') {
+    return Promise.resolve({ ...axiosResponse, data: { createdOn: '2022-09-09T14:39:32.000+00:00' } });
+  }
+  return Promise.resolve(axiosResponse);
+});
 jest.spyOn(caseService, 'getCaseApi').mockReturnValue(caseApi);
 
 let htmlRes: Document;
 
-describe('Claim details page', () => {
-  // todo sections and details
-  it.todo('All et1 details are present');
-
+describe('ET1 details', () => {
   beforeAll(async () => {
-    await request(mockApp({ userCase: mockUserCaseComplete }))
-      .get(PAGE_URL)
-      .then(res => {
-        htmlRes = new DOMParser().parseFromString(res.text, 'text/html');
-      });
+    htmlRes = await getHtmlRes(mockUserCaseComplete, PAGE_URL);
   });
 
   it('should display title', () => {
@@ -98,8 +100,64 @@ describe('Claim details page', () => {
 
     expect(entries).toHaveLength(numberOfEntries);
   });
+});
 
-  // todo dates and links
-  it.todo('Only et1 claim pdf');
-  it.todo('Both et1 claim pdf and supporting document');
+describe('ET1 documents', () => {
+  it('Shows Date uploaded and Document columns', async () => {
+    htmlRes = await getHtmlRes({ ...mockUserCaseComplete, claimSummaryFile: undefined }, PAGE_URL);
+
+    const table = htmlRes.getElementsByClassName(tableSelector)[0];
+    const headerRow = Array.from(table.getElementsByClassName(rowSelector))[0];
+    const columns = Array.from(headerRow.getElementsByClassName(tableHeaderSelector)).map(column => column.textContent);
+
+    expect(columns).toStrictEqual(['Date uploaded', 'Document']);
+  });
+
+  it.each([
+    {
+      caseDocumentsModifier: { claimSummaryFile: undefined },
+      expectedDocuments: [
+        {
+          date: '8 September 2022',
+          link: '/getCaseDocument/3aa7dfc1-378b-4fa8-9a17-89126fae5673',
+          linkText: 'ET1 Form',
+        },
+      ],
+    },
+    {
+      caseDocumentsModifier: {},
+      expectedDocuments: [
+        {
+          date: '8 September 2022',
+          link: '/getCaseDocument/3aa7dfc1-378b-4fa8-9a17-89126fae5673',
+          linkText: 'ET1 Form',
+        },
+        {
+          date: '9 September 2022',
+          link: '/getCaseDocument/a0c113ec-eede-472a-a59c-f2614b48177c',
+          linkText: 'ET1 support document',
+        },
+      ],
+    },
+  ])(
+    'show correct documents with modifier: $caseDocumentsModifier',
+    async ({ caseDocumentsModifier, expectedDocuments }) => {
+      htmlRes = await getHtmlRes({ ...mockUserCaseComplete, ...caseDocumentsModifier }, PAGE_URL);
+
+      const table = htmlRes.getElementsByClassName(tableSelector)[0];
+      const rows = Array.from(table.getElementsByClassName(rowSelector));
+      rows.splice(0, 1);
+
+      const documents = rows.map(row => {
+        const cells = Array.from(row.getElementsByClassName(cellSelector));
+        return {
+          date: cells[0].textContent,
+          link: cells[1].getElementsByTagName('a')[0].href,
+          linkText: cells[1].textContent,
+        };
+      });
+
+      expect(documents).toMatchObject(expectedDocuments);
+    }
+  );
 });
