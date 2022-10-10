cee3294d99b3708de3b7e60b5867053f34307270
Pettedson John <pettedson.john@hmcts.net>
2022-10-10 18:16:11 +0100

    Testing the nightly build with the wait's in place........

--- src/test/end-to-end/pages/employmentDetails/stillWorkingPages.js
+++ src/test/end-to-end/pages/employmentDetails/stillWorkingPages.js
@@ -11,31 +11,31 @@ module.exports = async function (noticePeriodContract, noticePeriod) {
   I.click(commonConfig.saveAndContinue);
 
   //employment start date page
-  I.seeElement('#startDate-day');
+  I.waitForElement('#startDate-day', 30);
   I.fillField('#startDate-day', '20');
   I.fillField('#startDate-month', '04');
   I.fillField('#startDate-year', '2014');
   I.click(commonConfig.saveAndContinue);
 
   //employment if statement to select notice period within contract
-  I.seeElement('#notice-period');
+  I.waitForElement('#notice-period', 30);
   if (noticePeriodContract === 'Yes written contract with notice period') {
     I.checkOption('input[id=notice-period]');
     I.click(commonConfig.saveAndContinue);
     if (noticePeriod === 'Weeks') {
-      I.seeElement('#notice-type');
+      I.waitForElement('#notice-type', 30);
       I.checkOption('input[id=notice-type]');
       I.click(commonConfig.saveAndContinue);
 
-      I.seeElement('#notice-length');
+      I.waitForElement('#notice-length', 30);
       I.fillField('input[id=notice-length]', '4');
       I.click(commonConfig.saveAndContinue);
     } else if (noticePeriod === 'Months') {
-      I.seeElement('#notice-type-2');
+      I.waitForElement('#notice-type-2', 30);
       I.checkOption('input[id=notice-type-2]');
       I.click(commonConfig.saveAndContinue);
 
-      I.seeElement('#notice-length');
+      I.waitForElement('#notice-length', 30);
       I.fillField('input[id=notice-length]', '1');
       I.click(commonConfig.saveAndContinue);
     }
@@ -43,22 +43,22 @@ module.exports = async function (noticePeriodContract, noticePeriod) {
     I.checkOption('input[id=notice-period-2]');
     I.click(commonConfig.saveAndContinue);
   }
-  I.seeElement('#avg-weekly-hrs');
+  I.waitForElement('#avg-weekly-hrs', 30);
   I.fillField('#avg-weekly-hrs', '20');
   I.click(commonConfig.saveAndContinue);
 
-  I.seeElement('#pay-before-tax');
+  I.waitForElement('#pay-before-tax', 30);
   I.fillField('#pay-before-tax', '40000');
   I.fillField('#pay-after-tax', '35000');
   I.checkOption('input[id=pay-interval]');
   I.click(commonConfig.saveAndContinue);
 
-  I.seeElement('#pension');
+  I.waitForElement('#pension', 30);
   I.checkOption('input[id=pension]');
   I.fillField('#pension-contributions', '200');
   I.click(commonConfig.saveAndContinue);
 
-  I.seeElement('#employee-benefits');
+  I.waitForElement('#employee-benefits', 30);
   I.see('Do or did you receive any employee benefits?');
   I.checkOption('input[id=employee-benefits]');
   I.click(commonConfig.saveAndContinue);
