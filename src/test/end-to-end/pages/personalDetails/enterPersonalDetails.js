6f1819648d917894055836286c6e323cdeb192c8
Pettedson John <pettedson.john@hmcts.net>
2022-10-11 07:59:12 +0100

    some more waitForTExt on the Personal Details page.....

--- src/test/end-to-end/pages/personalDetails/enterPersonalDetails.js
+++ src/test/end-to-end/pages/personalDetails/enterPersonalDetails.js
@@ -5,15 +5,17 @@ module.exports = async function () {
   // eslint-disable-next-line @typescript-eslint/no-this-alias
   const I = this;
   I.click('[href="/dob-details"]');
-  I.see('What is your date of birth?');
+  I.waitForText('What is your date of birth?', 30);
   I.fillField('#dobDate-day', '01');
   I.fillField('#dobDate-month', '01');
   I.fillField('#dobDate-year', '2000');
   I.click(commonConfig.saveAndContinue);
-  I.see('Sex and preferred title');
+
+  I.waitForText('Sex and preferred title', 30);
   I.checkOption('#sex-3');
   I.click(commonConfig.saveAndContinue);
-  I.see('What is your contact or home address?');
+
+  I.waitForText('What is your contact or home address?', 30);
   I.refreshPage();
   I.waitToHide('#address1', 10);
   I.dontSeeElement('#address1');
@@ -25,20 +27,26 @@ module.exports = async function () {
     '{"fullAddress":"3, SKELTON AVENUE, LEEDS, LS9 9HE","street1":"3 SKELTON AVENUE","street2":"","town":"LEEDS","county":"LEEDS","postcode":"LS9 9HE","country":"ENGLAND"}'
   );
   I.click(commonConfig.saveAndContinue);
-  I.see('What is your telephone number?');
+
+  I.waitForText('What is your telephone number?', 30);
   I.click(commonConfig.saveAndContinue);
-  I.see('How would you like to be contacted about your claim?');
+
+  I.waitForText('How would you like to be contacted about your claim?', 30);
   I.checkOption('#update-preference');
   I.click(commonConfig.saveAndContinue);
+
   I.checkOption('#hearingPreferences');
   I.click(commonConfig.saveAndContinue);
-  I.see(
-    'Do you have a physical, mental or learning disability or long term health condition that means you need support during your case?'
+
+  I.waitForText(
+    'Do you have a physical, mental or learning disability or long term health condition that means you need support during your case?',
+    30
   );
   I.checkOption('#reasonableAdjustments-2');
   I.click(commonConfig.saveAndContinue);
-  I.see('Have you completed this section?');
+
+  I.waitForText('Have you completed this section?', 30);
   I.checkOption('#tasklist-check');
   I.click(commonConfig.saveAndContinue);
-  I.see('Steps to making your claim');
+  I.waitForText('Steps to making your claim', 30);
 };
