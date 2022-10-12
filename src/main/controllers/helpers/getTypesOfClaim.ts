a0e6188e54c81b3d62660c968604979836bb71c9
KirsanovI <ivan.kirsanov@hmcts.net>
2022-10-12 10:07:28 +0200

    RET-2235: Add translations to json files(welsh), small style changes

---
+++ src/main/controllers/helpers/getTypesOfClaim.ts
@@ -0,0 +1,13 @@
+// import { AnyRecord } from '../../definitions/util-types';
+//
+// export const getTypesOfClaim = (
+//   typesOfClaim: string[],
+//   translations: AnyRecord
+// ): string[] => {
+//   const translatedTypesOfClaim: string[] = [];
+//   typesOfClaim.map(type => {
+//     translations.typeOfClaims.type;
+//   });
+//
+//   return translatedTypesOfClaim;
+// };
