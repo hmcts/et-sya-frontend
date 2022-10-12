1847b5d5b5947c3252789e7525e767e8969d5714
Itay-Kirsh <Itayk747@gmail.com>
2022-10-12 16:43:31 +0100

    Remove memory usage logging.

--- src/main/app.ts
+++ src/main/app.ts
@@ -22,8 +22,6 @@ import { PropertiesVolume } from './modules/properties-volume';
 import { Session } from './modules/session';
 import routes from './routes/routes';
 
-const { Logger } = require('@hmcts/nodejs-logging');
-
 const env = process.env.NODE_ENV || 'development';
 const developmentMode = env === 'development';
 
@@ -31,18 +29,11 @@ export const app = express();
 app.locals.ENV = env;
 app.locals.developmentMode = developmentMode;
 
-const logger = Logger.getLogger('app');
-
 new PropertiesVolume().enableFor(app);
-logger.info('Properties volume enabled');
 
 new AppInsights().enable();
-logger.info('App insights');
-logMemUsage();
 
 new Nunjucks(developmentMode).enableFor(app);
-logger.info('Nunjucks');
-logMemUsage();
 
 new Helmet(config.get('security'), [
   config.get('services.idam.authorizationURL'),
@@ -50,24 +41,13 @@ new Helmet(config.get('security'), [
   config.get('services.et1Legacy.url'),
 ]).enableFor(app);
 
-logger.info('Helmet');
-logMemUsage();
-
 new Container().enableFor(app);
-logger.info('Container read dir');
-logMemUsage();
 
 new I18Next().enableFor(app);
-logger.info('I18Next translations');
-logMemUsage();
 
 new Session().enableFor(app);
-logger.info('Sessions');
-logMemUsage();
 
 new HealthCheck().enableFor(app);
-logger.info('Health Check');
-logMemUsage();
 
 app.use(favicon(path.join(__dirname, '/public/assets/images/favicon.ico')));
 app.use(bodyParser.json());
@@ -80,12 +60,8 @@ app.use((req, res, next) => {
 });
 
 new Oidc().enableFor(app);
-logger.info('Oidc');
-logMemUsage();
 
 routes(app);
-logger.info('routes added');
-logMemUsage();
 
 setupDev(app, developmentMode);
 // returning "not found" page for requests with paths not resolved by the router
@@ -110,20 +86,9 @@ app.use((error: HTTPError, request: Request, response: Response, next: NextFunct
 
 // error handler
 app.use((err: HTTPError, req: AppRequest, res: express.Response) => {
-  logger.error(`${err.stack || err}`);
-
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = env === 'development' ? err : {};
   res.status(err.status || 500);
   res.render('error');
 });
-
-function logMemUsage() {
-  const used = process.memoryUsage();
-  logger.info(`rss ${Math.round((used.rss / 1024 / 1024) * 100) / 100} MB`);
-  logger.info(`heapUsed ${Math.round((used.heapUsed / 1024 / 1024) * 100) / 100} MB`);
-  logger.info(`heapTotal ${Math.round((used.heapTotal / 1024 / 1024) * 100) / 100} MB`);
-  logger.info(`arrayBuffers ${Math.round((used.arrayBuffers / 1024 / 1024) * 100) / 100} MB`);
-  logger.info(`external ${Math.round((used.external / 1024 / 1024) * 100) / 100} MB`);
-}
