8404a8a0c50c7a7e6fb4ffdaca52c39fe047ae3e
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2022-10-13 16:23:28 +0100

    Update submitClaimForSinglePerson.js (#406)

--- src/test/end-to-end/features/e2e_Scenarios/submitClaimForSinglePerson.js
+++ src/test/end-to-end/features/e2e_Scenarios/submitClaimForSinglePerson.js
@@ -30,4 +30,4 @@ Scenario('Submit a single claim for myself', async ({ I }) => {
   await submittingClaim(I);
   await checkYourAnswers(I);
   await claimSubmitted(I);
-}).tag('@RET-BAT');
+}).tag('@RET-BAT-Disabled');
