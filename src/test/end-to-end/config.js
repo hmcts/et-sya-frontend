bd267507f8de8d68dc63567d87cb690615e11826
Pettedson John <pettedson.john@hmcts.net>
2022-10-18 11:35:52 +0100

    Adding the false config flag...

--- src/test/end-to-end/config.js
+++ src/test/end-to-end/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   TestUrl: process.env.TEST_E2E_URL || 'https://et-sya.aat.platform.hmcts.net',
-  TestShowBrowserWindow: process.env.SHOW_BROWSER_WINDOW || true,
+  TestShowBrowserWindow: process.env.SHOW_BROWSER_WINDOW || false,
   TestsPathToRun: process.env.E2E_TEST_PATH || './features/**/*js',
   TestReportFolder: process.env.E2E_OUTPUT_DIR || './functional-output/reports',
   TestEnvETUser: process.env.TEST_CASE_USERNAME || '',
