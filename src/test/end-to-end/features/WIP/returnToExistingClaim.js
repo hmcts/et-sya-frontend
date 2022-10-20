6f3f55894011e3d6be5b0899d54e1aadcabefdc4
Pettedson John <pettedson.john@hmcts.net>
2022-10-20 21:09:44 +0100

    Added another Descoped test to the repository.....

--- src/test/end-to-end/features/WIP/returnToExistingClaim.js
+++ src/test/end-to-end/features/WIP/returnToExistingClaim.js
@@ -34,4 +34,4 @@ Scenario('Verify returing to an existing claim flow when got a new ET account', 
   I.checkOption('input[id=return_number_or_account-2]');
   I.click('Continue');
   authPage.logout();
-}).tag(' @RET-BAT');
+}).tag(' @RET-WIP');
