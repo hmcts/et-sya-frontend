363b440f924acaa392a69346f827c990af90f3f9
Pettedson John <pettedson.john@hmcts.net>
2022-10-21 10:46:05 +0100

    Adding the change scrollPageToBottom....

--- src/test/end-to-end/pages/saveDraft/youCanSaveCard.js
+++ src/test/end-to-end/pages/saveDraft/youCanSaveCard.js
@@ -6,6 +6,7 @@ module.exports = async function () {
   // eslint-disable-next-line @typescript-eslint/no-this-alias
   const I = this;
 
+  await I.scrollPageToBottom();
   await I.waitForVisible("//span[contains(text(),'Contact us')]", testConfig.TestWaitForVisibilityTimeLimit);
   await I.see('You do not have to complete your claim in one go');
   await I.click(commonConfig.continue);
