9db9661633eef58dcb9a44ccc8fec3e26250407e
hsjhita <harpreet.jhita@justice.gov.uk>
2022-10-24 10:33:49 +0100

    Update codecept.smoke.conf.js

--- src/test/smoke/tests/healthCheck.test.js
+++ src/test/smoke/tests/healthCheck.test.js
@@ -1,11 +1,16 @@
 const frontend = process.env.TEST_URL || 'http://localhost:3001';
 const { I } = inject();
 Feature('Health');
+const { Logger } = require('@hmcts/nodejs-logging');
+
+const logger = Logger.getLogger('app');
 
 Scenario('The API is up, healthy and responding to requests to /health', () => {
+  logger.info('Running Smoke Test');
   I.amOnPage(frontend + '/health');
   I.retry({
     minTimeout: 15000,
     maxTimeout: 15000,
   }).see('"status":"UP"');
+  logger.info('After Smoke Test');
 });
