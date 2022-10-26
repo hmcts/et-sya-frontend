9ae924a3dd055230b79d985743ab6a8ada1a5ad8
Itay-Kirsh <Itayk747@gmail.com>
2022-10-26 16:05:11 +0100

    Only add error and path to the axios error details if a response-error

--- src/main/services/AxiosErrorAdapter.ts
+++ src/main/services/AxiosErrorAdapter.ts
@@ -2,7 +2,10 @@ import { AxiosError } from 'axios';
 
 // Get details from an axios error
 export const axiosErrorDetails = (axiosError: AxiosError<{ error: string; path: string }>): string => {
-  const { error, path } = axiosError.response.data;
-
-  return `${axiosError.message}, error: ${error}, path: ${path}`;
+  let errorMessage = axiosError.message;
+  if (axiosError.response) {
+    const { error, path } = axiosError.response.data;
+    errorMessage += `, error: ${error}, path: ${path}`;
+  }
+  return errorMessage;
 };
