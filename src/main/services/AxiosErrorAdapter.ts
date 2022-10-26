2fcdf2958c616e4236b6544262e8c074e190a113
Itay-Kirsh <Itayk747@gmail.com>
2022-10-26 16:54:27 +0100

    Correct the axiosError parameter

--- src/main/services/AxiosErrorAdapter.ts
+++ src/main/services/AxiosErrorAdapter.ts
@@ -1,7 +1,7 @@
 import { AxiosError } from 'axios';
 
 // Get details from an axios error
-export const axiosErrorDetails = (axiosError: AxiosError<{ error: string; path: string }>): string => {
+export const axiosErrorDetails = (axiosError: AxiosError<{ error: string }>): string => {
   let errorMessage = axiosError.message;
   if (axiosError.response) {
     errorMessage += `, ${axiosError.response.data?.error}`;
