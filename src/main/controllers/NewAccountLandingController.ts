4907a2dc76ee7ebf02f08c5cf67a3bf4c6356f66
lvo700 <lynnette.orr@HMCTS.net>
2022-10-27 09:48:15 +0100

    Set cookie and language in new account landing page

--- src/main/controllers/NewAccountLandingController.ts
+++ src/main/controllers/NewAccountLandingController.ts
@@ -1,12 +1,18 @@
 import { Response } from 'express';
 
 import { AppRequest } from '../definitions/appRequest';
+import { PageUrls } from '../definitions/constants';
+
+import { setI18nLanguageCookie, setUrlLanguage } from './helpers/LanguageHelper';
 
 export default class NewAccountLandingController {
   public get = (req: AppRequest, res: Response): void => {
+    const redirectUrl = setUrlLanguage(req, PageUrls.CLAIM_STEPS);
+    req.cookies.i18next = setI18nLanguageCookie(req, req.cookies.i18next);
     res.render('new-account-landing', {
       ...req.t('common', { returnObjects: true }),
       ...req.t('new-account-landing', { returnObjects: true }),
+      redirectUrl,
     });
   };
 }
