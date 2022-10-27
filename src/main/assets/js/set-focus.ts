11e0fd6eb98d3b4c913955ec05d9b9da29ed5313
mehmet.dede <mehmet.dede@justice.gov.uk>
2022-10-27 11:23:39 +0100

    Accessibility Issues Update

--- src/main/assets/js/set-focus.ts
+++ src/main/assets/js/set-focus.ts
@@ -1,78 +1,35 @@
 // Autofocus section
 // Code below automatically focuses to errors, in case any error exists on the page for accessibility
-/*{
-  const govUKErrors = Array.from(
-    document.getElementsByClassName('govuk-error-summary__title') as HTMLCollectionOf<HTMLElement>
-  );
 
-  if (govUKErrors !== null && govUKErrors !== undefined && govUKErrors.length > 0) {
-    if (!isHidden(govUKErrors[0])) {
-      const govUKErrorsSkipLinkArray = Array.from(
-        document.getElementsByClassName('govuk-skip-link') as HTMLCollectionOf<HTMLElement>
-      );
-      if (
-        govUKErrorsSkipLinkArray !== null &&
-        govUKErrorsSkipLinkArray !== undefined &&
-        govUKErrorsSkipLinkArray.length > 0
-      ) {
-        govUKErrorsSkipLinkArray[0].classList.add('hidden');
-      }
-      document.title = 'Error: ' + document.title;
-      govUKErrors[0].focus();
+function focusToErrorSummary() {
+  if (!govUKErrorSummary.classList.contains('hidden')) {
+    const govUKErrorsSkipLink = findFirstElementByClassName('govuk-skip-link');
+    if (govUKErrorsSkipLink !== null) {
+      govUKErrorsSkipLink.classList.add('hidden');
     }
+    if (!document.title.includes('Error')) {
+      document.title = 'Error: ' + document.title;
+    }
+    govUKErrorSummary.focus();
+    govUKErrorSummary.setAttribute('tabindex', '-1');
   }
-
-}*/
+}
 
 if (document.addEventListener) {
-  document.addEventListener('load', onElementFocused);
-  document.addEventListener('pageshow', onElementFocused);
-  document.addEventListener('DOMContentLoaded', onElementFocused, true);
+  document.addEventListener('load', focusToErrorSummary);
+  document.addEventListener('pageshow', focusToErrorSummary);
+  document.addEventListener('DOMContentLoaded', focusToErrorSummary, true);
 }
 
-function onElementFocused() {
-  if (document.hasFocus()) {
-    const govUKErrors = Array.from(
-      document.getElementsByClassName('govuk-error-summary__title') as HTMLCollectionOf<HTMLElement>
-    );
-
-    if (govUKErrors !== null && govUKErrors !== undefined && govUKErrors.length > 0) {
-      if (!isHidden(govUKErrors[0])) {
-        const govUKErrorsSkipLinkArray = Array.from(
-          document.getElementsByClassName('govuk-skip-link') as HTMLCollectionOf<HTMLElement>
-        );
-        if (
-          govUKErrorsSkipLinkArray !== null &&
-          govUKErrorsSkipLinkArray !== undefined &&
-          govUKErrorsSkipLinkArray.length > 0
-        ) {
-          govUKErrorsSkipLinkArray[0].classList.add('hidden');
-        }
-        if (!document.title.includes('Error')) {
-          document.title = 'Error: ' + document.title;
-        }
-        const govUKBody = Array.from(
-          document.getElementsByClassName('govuk-template__body') as HTMLCollectionOf<HTMLElement>
-        );
-        const errorSummary = govUKBody[0];
-        errorSummary.focus();
-        errorSummary.setAttribute('tabindex', '-1');
-        errorSummary.addEventListener('blur', function () {
-          errorSummary.removeAttribute('tabindex');
-        });
-      }
-    }
+export const findFirstElementByClassName = (className: string): HTMLElement => {
+  const elementsByClassName = Array.from(document.getElementsByClassName(className) as HTMLCollectionOf<HTMLElement>);
+  if (elementsByClassName !== null && elementsByClassName !== undefined && elementsByClassName.length > 0) {
+    return elementsByClassName[0];
   }
-}
+  return null;
+};
+const govUKErrorSummary = findFirstElementByClassName('govuk-error-summary');
 
-function isHidden(el: HTMLElement): boolean {
-  const govUKErrorsDivHidden = Array.from(
-    document.getElementsByClassName('govuk-error-summary hidden') as HTMLCollectionOf<HTMLElement>
-  );
-  if (govUKErrorsDivHidden !== null && govUKErrorsDivHidden !== undefined && govUKErrorsDivHidden.length > 0) {
-    return true;
-  } else {
-    const style = window.getComputedStyle(el);
-    return style.display === 'none';
-  }
+{
+  focusToErrorSummary();
 }
