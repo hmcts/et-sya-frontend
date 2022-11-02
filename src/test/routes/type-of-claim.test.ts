aa9f8387e83282bdc2c584987e54a2988493d58e
lvo700 <lynnette.orr@HMCTS.net>
2022-11-02 10:55:26 +0000

    Fixed failing test

--- src/test/routes/type-of-claim.test.ts
+++ src/test/routes/type-of-claim.test.ts
@@ -1,6 +1,6 @@
 import request from 'supertest';
 
-import { PageUrls, languages } from '../../main/definitions/constants';
+import { PageUrls } from '../../main/definitions/constants';
 import { mockApp } from '../unit/mocks/mockApp';
 
 describe(`GET ${PageUrls.TYPE_OF_CLAIM}`, () => {
@@ -21,18 +21,6 @@ describe(`on POST ${PageUrls.TYPE_OF_CLAIM}`, () => {
       .expect(res => {
         expect(res.status).toStrictEqual(302);
         expect(res.header['location']).toStrictEqual(PageUrls.TYPE_OF_CLAIM);
-      });
-  });
-
-  test('should return the type of claim (Welsh language) page when the current language is Welsh and case types discrimination or whistle blowing is not selected', async () => {
-    await request(mockApp({}))
-      .post(PageUrls.TYPE_OF_CLAIM + languages.WELSH_URL_PARAMETER)
-      .send({
-        typeOfClaim: [],
-      })
-      .expect(res => {
-        expect(res.status).toStrictEqual(302);
-        expect(res.header['location']).toStrictEqual(PageUrls.TYPE_OF_CLAIM + languages.WELSH_URL_PARAMETER);
       });
   });
 });
