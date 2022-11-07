a97d5a30a89db22ed3306fcb26873ade56f8086a
lvo700 <lynnette.orr@HMCTS.net>
2022-11-07 11:42:29 +0000

    Reverting feature flag config name

--- src/main/modules/nunjucks/index.ts
+++ src/main/modules/nunjucks/index.ts
@@ -26,10 +26,7 @@ export class Nunjucks {
       }
     );
 
-    nunEnv.addGlobal(
-      'welshEnabled',
-      process.env.FT_WELSH === 'true' || config.get('featureFlags.welshTranslations') === 'true'
-    );
+    nunEnv.addGlobal('welshEnabled', process.env.FT_WELSH === 'true' || config.get('featureFlags.welsh') === 'true');
 
     nunEnv.addGlobal('getContent', function (prop: ((param: string) => string) | string): string {
       return typeof prop === 'function' ? prop(this.ctx) : prop;
