2d48afd643b1aa7a182b4162f5b8a750a1c49678
Mehmet Tahir Dede <105645217+JustMehmet@users.noreply.github.com>
2022-11-25 15:12:37 +0000

    Ret-2738 Improve et-sya-frontend tests (#533)
    * Note for Fix tests with issues logged in pipeline for router test
    
    * Note for Fix tests with issues logged in pipeline for router test
    
    * Resolved issue "after(functionalTest:preview) is deprecated, consider using ‘afterSuccess’, ‘afterFailure’, ‘afterAlways’ instead This configuration will stop working by 30/01/2023 ( in 66 days )" by replacing after with afterAlways
    
    * Resolved issue "after(functionalTest:preview) is deprecated, consider using ‘afterSuccess’, ‘afterFailure’, ‘afterAlways’ instead This configuration will stop working by 30/01/2023 ( in 66 days )" by replacing after with afterAlways
    
    * Resolved issue "after(functionalTest:preview) is deprecated, consider using ‘afterSuccess’, ‘afterFailure’, ‘afterAlways’ instead This configuration will stop working by 30/01/2023 ( in 66 days )" by replacing after with afterAlways
    
    * Fix for functional test for respondentDetailsPages.js at line 32 by adding comma after 7 in the question Did you work at 7, VALLEY GARDENS?

--- src/test/end-to-end/pages/employmentDetails/respondentDetailsPages.js
+++ src/test/end-to-end/pages/employmentDetails/respondentDetailsPages.js
@@ -29,7 +29,7 @@ module.exports = async function (workAddress, doYouHaveAcas) {
 
   //enter address for another location
   await I.waitForVisible("//span[contains(text(),'Contact us')]", testConfig.TestWaitForVisibilityTimeLimit);
-  I.see('Did you work at 7 VALLEY GARDENS?');
+  I.see('Did you work at 7, VALLEY GARDENS?');
   //Did you work at address or another
   if (workAddress === 'Yes') {
     await I.waitForVisible("//span[contains(text(),'Contact us')]", testConfig.TestWaitForVisibilityTimeLimit);
