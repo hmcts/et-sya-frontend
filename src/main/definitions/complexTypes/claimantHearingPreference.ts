6914f1360404943473622ceabee89021486deeaa
Marc Judge <marcjudge@LAPTOP4236.V1.com>
2022-12-12 10:00:42 +0000

    Add Welsh language preferences

--- src/main/definitions/complexTypes/claimantHearingPreference.ts
+++ src/main/definitions/complexTypes/claimantHearingPreference.ts
@@ -1,8 +1,10 @@
-import { HearingPreference, YesOrNo } from '../case';
+import { EnglishOrWelsh, HearingPreference, YesOrNo } from '../case';
 
 export interface ClaimantHearingPreference {
   reasonable_adjustments?: YesOrNo;
   reasonable_adjustments_detail?: string;
   hearing_preferences?: HearingPreference[];
   hearing_assistance?: string;
+  contact_language?: EnglishOrWelsh;
+  hearing_language?: EnglishOrWelsh;
 }
