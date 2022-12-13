7705c9742e82a51d3c36ea0b575f6b8819a0775f
Itay-Kirsh <Itayk747@gmail.com>
2022-12-13 10:39:28 +0000

    RET-2615. Clean file

--- src/main/controllers/helpers/ContactApplicationHelper.ts
+++ src/main/controllers/helpers/ContactApplicationHelper.ts
@@ -1,45 +1,46 @@
-import {CaseWithId} from "../../definitions/case";
-import {AnyRecord} from "../../definitions/util-types";
-import {InterceptPaths} from "../../definitions/constants";
+import { CaseWithId } from '../../definitions/case';
+import { InterceptPaths } from '../../definitions/constants';
+import { AnyRecord } from '../../definitions/util-types';
 
 export const getFiles = (
   userCase: CaseWithId | undefined,
   translations: AnyRecord
 ): { key: unknown; value?: unknown; actions?: unknown }[] => {
-    if (userCase === undefined || userCase.contactApplicationFile === undefined) {
-      return [
-        {
-          key: {
-            html: translations.noFilesUpload,
-            classes: 'govuk-!-font-weight-regular-m',
-          },
-          value: {
-            text: "",
-          },
-          actions: {
-            items: [
-            ],
-          },
-        }];
-    } else {
-      return [
-        {
-          key: {
-            text: userCase.contactApplicationFile,
-            classes: 'govuk-!-font-weight-regular-m',
-          },
-          value: {
-            text: "",
-          },
-          actions: {
-            items: [
-              {
-                href: InterceptPaths.REMOVE_FILE,
-                text: translations.remove,
-                visuallyHiddenText: translations.remove,
-              },
-            ],
-          },
-        }];
-    }
+  if (userCase === undefined || userCase.contactApplicationFile === undefined) {
+    return [
+      {
+        key: {
+          html: translations.noFilesUpload,
+          classes: 'govuk-!-font-weight-regular-m',
+        },
+        value: {
+          text: '',
+        },
+        actions: {
+          items: [],
+        },
+      },
+    ];
+  } else {
+    return [
+      {
+        key: {
+          text: userCase.contactApplicationFile,
+          classes: 'govuk-!-font-weight-regular-m',
+        },
+        value: {
+          text: '',
+        },
+        actions: {
+          items: [
+            {
+              href: InterceptPaths.REMOVE_FILE,
+              text: translations.remove,
+              visuallyHiddenText: translations.remove,
+            },
+          ],
+        },
+      },
+    ];
+  }
 };
