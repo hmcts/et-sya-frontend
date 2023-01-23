b37e2069a7ba7061e29f0f5e8165e01a537d692f
gauravchawla <gaurav-chawla@hotmail.com>
2023-01-23 17:19:31 +0000

    first commit

--- src/main/controllers/ClaimantApplicationsController.ts
+++ src/main/controllers/ClaimantApplicationsController.ts
@@ -6,6 +6,8 @@ import { FormContent } from '../definitions/form';
 import { getUserApplications, getUserCasesByLastModified } from '../services/CaseSelectionService';
 
 import { getPageContent } from './helpers/FormHelpers';
+import {AnyRecord} from "../definitions/util-types";
+import {getLanguageParam} from "./helpers/RouterHelpers";
 
 export default class ClaimantApplicationsController {
   public get = async (req: AppRequest, res: Response): Promise<void> => {
@@ -13,14 +15,19 @@ export default class ClaimantApplicationsController {
       TranslationKeys.COMMON,
       TranslationKeys.CLAIMANT_APPLICATIONS,
     ]);
+    const translations: AnyRecord = {
+      ...req.t(TranslationKeys.COMMON, { returnObjects: true }),
+    };
     const userCases = await getUserCasesByLastModified(req);
     if (userCases.length === 0) {
       return res.redirect(PageUrls.HOME);
     } else {
-      const usersApplications = getUserApplications(userCases);
+      const languageParam = getLanguageParam(req.url);
+      const usersApplications = getUserApplications(userCases, translations, languageParam);
       res.render(TranslationKeys.CLAIMANT_APPLICATIONS, {
         ...content,
         usersApplications,
+        currentUrl: PageUrls.CLAIMANT_APPLICATIONS,
       });
     }
   };
