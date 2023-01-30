05ef266c270d7a221e199ed3310d7b9b9c1f6504
GauravChawlaMOJ <58194626+GauravChawlaMOJ@users.noreply.github.com>
2023-01-30 08:58:49 +0000

    Ret 3114 and ret 3118 (#630)

--- src/main/controllers/helpers/ApplicationTableRecordTranslationHelper.ts
+++ src/main/controllers/helpers/ApplicationTableRecordTranslationHelper.ts
@@ -3,6 +3,7 @@ import { AnyRecord } from '../../definitions/util-types';
 
 export const translateTypesOfClaims = (rec: ApplicationTableRecord, translations: AnyRecord): void => {
   rec.userCase.typeOfClaim = rec?.userCase?.typeOfClaim?.map(toc => translateTypeOfClaim(toc, translations));
+  rec.userCase.typeOfClaimString = rec?.userCase?.typeOfClaim?.toString()?.replace(",", ", ");
 };
 
 export const translateTypeOfClaim = (typeOfClaims: string, translations: AnyRecord): string => {
