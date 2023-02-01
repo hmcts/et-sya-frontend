208bf5207d869d0b189b9954e2f16ce5eb6699ad
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2023-02-01 13:11:24 +0000

    Pet 1.1.0.6 new (#636)
    * RET-3046 Average weekly hours Decimal Check Fails (#612)
    
    * Decimal value validation is added to average weekly hours field
    
    * Decimal value validation is added to average weekly hours field
    
    * Decimal value validation is added to average weekly hours field
    
    * Decimal value validation is added to average weekly hours field
    
    * Decimal value validation is added to average weekly hours field
    
    * Ret 3046 (#618)
    
    * Decimal value validation is added to average weekly hours field
    
    * Decimal value validation is added to average weekly hours field
    
    * Decimal value validation is added to average weekly hours field
    
    * Decimal value validation is added to average weekly hours field
    
    * Decimal value validation is added to average weekly hours field
    
    * Decimal value validation is added to average weekly hours field
    
    * Decimal value validation is added to average weekly hours field
    
    * Ret 3046 (#622)
    
    * Decimal value validation is added to average weekly hours field
    
    * Decimal value validation is added to average weekly hours field
    
    * Decimal value validation is added to average weekly hours field
    
    * Decimal value validation is added to average weekly hours field
    
    * Decimal value validation is added to average weekly hours field
    
    * Decimal value validation is added to average weekly hours field
    
    * Decimal value validation is added to average weekly hours field
    
    * Decimal value validation is added to average weekly hours field
    
    * Ret 2942 (#613)
    
    * Ret 2942 (#625)
    
    * RET-3021 (#624)
    
    * Ret 2942 (#626)
    
    * Ret 3114 and ret 3118 (#630)
    
    * Ret 3118 (#632)
    
    * Ret 3080 (#633)
    
    * Added employee end date to checklist.
    
    * Test updates for adding employee end date to checklist.
    
    * Upgrade to Yarn v3
    
    * Revert "Upgrade to Yarn v3"
    
    This reverts commit 734e4946a5fbf6cb31f436bd9013ac05fdcaebe3.
    
    * Jenkinsfile_nightly fix (#637)
    
    ---------
    
    Co-authored-by: Mehmet Tahir Dede <105645217+JustMehmet@users.noreply.github.com>
    Co-authored-by: GauravChawlaMOJ <58194626+GauravChawlaMOJ@users.noreply.github.com>
    Co-authored-by: mehmet.dede <mehmet.dede@justice.gov.uk>

---
+++ src/test/unit/mocks/mockTranslations.ts
@@ -0,0 +1,22 @@
+import { AnyRecord } from '../../../main/definitions/util-types';
+
+export const mockWelshClaimTypesTranslations: AnyRecord = {
+  claimTypes: {
+    breachOfContract: 'Torri contract',
+    discrimination: 'Gwahaniaethu',
+    payRelated: 'Ymwneud â thâl',
+    unfairDismissal: 'Diswyddo annheg',
+    whistleBlowing: 'Chwythu’r chwiban',
+    otherTypesOfClaims: 'Math arall o hawliad',
+  },
+  of: 'o',
+  tasksCompleted: "tasg wedi'u cwblhau",
+};
+
+export const mockEnglishClaimTypesTranslations: AnyRecord = {
+  claimTypes: {
+    discrimination: 'discrimination',
+  },
+  of: 'of',
+  tasksCompleted: 'tasks completed',
+};
