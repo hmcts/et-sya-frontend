ccdab482fdca67f94d23625fd6a7157483af4178
Sunday Ayeni <sunnyayeni18@gmail.com>
2023-03-01 18:09:40 +0000

    create a test to run on demo

--- src/test/end-to-end/features/e2e_Scenarios/submitClaimForSinglePerson.js
+++ src/test/end-to-end/features/e2e_Scenarios/submitClaimForSinglePerson.js
@@ -53,6 +53,7 @@ Scenario('Submit a single claim for myself without the Complete PCQ/Equality', a
   await claimSubmitted(I);
 })
   .tag('@RET-BAT')
+  .tag('@demo')
   .retry(2);
 
 Scenario('Submit a single claim for myself without the Complete PCQ/Equality CrossBrowser', async ({ I }) => {
