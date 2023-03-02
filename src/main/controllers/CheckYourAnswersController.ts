99abcc3f22e3e4a1cc7eee200f223655770a6a62
mehmet.dede <mehmet.dede@justice.gov.uk>
2023-03-02 12:08:36 +0000

    Update to remove claim data from session after case submission.

--- src/main/controllers/CheckYourAnswersController.ts
+++ src/main/controllers/CheckYourAnswersController.ts
@@ -12,6 +12,9 @@ import { getYourDetails } from './helpers/YourDetailsAnswersHelper';
 
 export default class CheckYourAnswersController {
   public get(req: AppRequest, res: Response): void {
+    if (!req.session || !req.session.userCase) {
+      return res.redirect(PageUrls.CLAIMANT_APPLICATIONS);
+    }
     const userCase = req.session?.userCase;
     const translations: AnyRecord = {
       ...req.t(TranslationKeys.CHECK_ANSWERS, { returnObjects: true }),
