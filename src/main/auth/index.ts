b3145528a382fa4f7b60bb588729fc4e63306588
ikirsanov <ivan.kirsanov@version1.com>
2023-03-30 13:24:36 +0200

    RET-2927: Rollback index files changes

--- src/main/auth/index.ts
+++ src/main/auth/index.ts
@@ -28,7 +28,6 @@ export const getUserDetails = async (
   const callbackUrl = encodeURI(serviceUrl + callbackUrlPageLink);
   const code = encodeURIComponent(rawCode);
   const data = `client_id=${id}&client_secret=${secret}&grant_type=authorization_code&redirect_uri=${callbackUrl}&code=${code}`;
-
   const headers = {
     Accept: 'application/json',
     'Content-Type': 'application/x-www-form-urlencoded',
