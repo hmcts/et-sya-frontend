47397c01885b6ba7ec7861842eaf24db58bfb676
mehmet.dede <mehmet.dede@justice.gov.uk>
2023-04-18 16:03:04 +0100

    Updated document helpers' combineUserCaseDocuments to combine all documents of the cases of user's.

--- src/test/unit/controller/helpers/DocumentHelpers.test.ts
+++ src/test/unit/controller/helpers/DocumentHelpers.test.ts
@@ -1,4 +1,5 @@
-import { combineDocuments } from '../../../../main/controllers/helpers/DocumentHelpers';
+import { combineDocuments, combineUserCaseDocuments } from '../../../../main/controllers/helpers/DocumentHelpers';
+import mockUserCaseWithDocumentsComplete from '../../mocks/mockUserCaseWithDocumentsComplete';
 
 it('should combine documents correctly', () => {
   expect(
@@ -15,5 +16,19 @@ it('should combine documents correctly', () => {
     { id: '1', description: 'desc1' },
     { id: '2', description: 'desc2' },
     { id: '3', description: 'desc3' },
+  ]);
+});
+
+it('should combine user case documents correctly', () => {
+  expect(combineUserCaseDocuments([mockUserCaseWithDocumentsComplete])).toStrictEqual([
+    { description: 'Case Details - Sunday Ayeni', id: '3aa7dfc1-378b-4fa8-9a17-89126fae5673', type: 'ET1' },
+    { id: '1', description: 'desc1' },
+    { id: '2', description: 'desc2' },
+    { id: '3', description: 'desc3' },
+    { id: '4', description: 'desc4' },
+    { id: '5', description: 'desc5' },
+    { id: '6', description: 'desc6' },
+    { id: '7', description: 'desc7' },
+    { id: '8', description: 'desc8' },
   ]);
 });
