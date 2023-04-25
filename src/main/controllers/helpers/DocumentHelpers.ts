bdee9ba80e5dd02dd92b593a959f99bd85f3ee60
lvo700 <lynnette.orr@hmcts.net>
2023-04-25 10:53:39 +0100

    Ret 3234 fix (#768)
    * Document validation fix
    
    * Application details link fix
    
    * Application details link fix
    
    * Refactor attachment controller
    
    * Refactor

--- src/main/controllers/helpers/DocumentHelpers.ts
+++ src/main/controllers/helpers/DocumentHelpers.ts
@@ -1,8 +1,11 @@
+import { AppRequest } from '../../definitions/appRequest';
 import { Document } from '../../definitions/case';
 import { GenericTseApplicationTypeItem } from '../../definitions/complexTypes/genericTseApplicationTypeItem';
 import { DocumentDetail } from '../../definitions/definition';
 import { getDocId, getFileExtension } from '../../helper/ApiFormatter';
 import { getCaseApi } from '../../services/CaseService';
+
+import { getClaimantResponseDocDownload } from './TseRespondentApplicationHelpers';
 
 export const getDocumentDetails = async (documents: DocumentDetail[], accessToken: string): Promise<void> => {
   for await (const document of documents) {
@@ -74,4 +77,40 @@ export function formatBytes(bytes: number, decimals = 2): string {
   const i = Math.floor(Math.log(bytes) / Math.log(k));
 
   return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))}${sizes[i]}`;
+}
+
+export function getClaimantResponseDocId(selectedApplication: GenericTseApplicationTypeItem): string {
+  let claimantResponseDocId = undefined;
+  const claimantResponseDoc = getClaimantResponseDocDownload(selectedApplication);
+  if (claimantResponseDoc !== undefined) {
+    claimantResponseDocId = getDocId(claimantResponseDoc.document_url);
+  }
+  return claimantResponseDocId;
+}
+
+export function getDecisionDocId(req: AppRequest, selectedApplication: GenericTseApplicationTypeItem): string {
+  let decisionDocId = undefined;
+  const docId = req.params.docId;
+  const decisionDocIds = [];
+  if (selectedApplication?.value.adminDecision?.length) {
+    const adminDecisions = selectedApplication.value.adminDecision;
+    for (let i = adminDecisions.length - 1; i >= 0; i--) {
+      if (adminDecisions[i].value.responseRequiredDoc !== undefined) {
+        decisionDocIds[i] = adminDecisions[i].value.responseRequiredDoc.document_url;
+      }
+    }
+  }
+  decisionDocId = decisionDocIds.map(it => getDocId(it)).find(id => id === docId);
+  return decisionDocId;
+}
+
+export function getRequestDocId(req: AppRequest): string {
+  const docId = req.params.docId;
+  const userCase = req.session?.userCase;
+  const requestDoc = userCase.selectedRequestOrOrder?.value.sendNotificationUploadDocument;
+  let requestDocId = undefined;
+  if (requestDoc?.length) {
+    requestDocId = requestDoc.map(it => getDocId(it.value.uploadedDocument.document_url)).find(id => id === docId);
+  }
+  return requestDocId;
 }
