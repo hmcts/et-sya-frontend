af6d58b50657554b516b862a1fb2aa0b148622dc
gauravchawla <gaurav-chawla@hotmail.com>
2023-04-30 23:59:35 +0100

    RET-3471- commit

--- src/test/end-to-end/pages/welshTranslations/personal.details.welsh.page.js
+++ src/test/end-to-end/pages/welshTranslations/personal.details.welsh.page.js
@@ -37,7 +37,7 @@ module.exports = {
     I.refreshPage();
     I.fillField('#enterPostcode', 'LS9 9HE');
     I.click('#saveAndContinue');
-    I.waitForVisible('#selectAddressInput', 30);
+    I.waitForVisible('#selectAddress', 30);
     I.selectOption(
       '#addressTypes',
       '{"text":"3, Skelton Avenue, Leeds, LS9 9HE","street1":"3, Skelton Avenue","street2":"","town":"Leeds","county":"Leeds","postcode":"LS9 9HE","country":"England"}'
