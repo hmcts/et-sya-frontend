a7f9785cb7215c05881e060a74370b9e57b08b56
gauravchawla <gaurav-chawla@hotmail.com>
2023-04-30 20:08:31 +0100

    RET-3471- commit

---
+++ src/main/assets/js/index.js
@@ -0,0 +1,14 @@
+const govUkFrontend = require('govuk-frontend');
+
+const sessionTimeout = require('./session/utils');
+
+import '../scss/main.scss';
+import './go-back';
+
+govUkFrontend.initAll();
+import './print';
+import './cookie';
+import './set-focus';
+import './submit-claim';
+sessionTimeout.ready(sessionTimeout.initialize);
+import './data-layer';
---
+++ src/main/assets/js/print.ts
@@ -0,0 +1,7 @@
+// This is needed as the inline print script violates the Content Security Policy
+const printLink = document.getElementById('print') as HTMLElement | null;
+if (printLink) {
+  printLink.onclick = function () {
+    window.print();
+  };
+}
