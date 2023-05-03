5c582323d2659554da64fac6576027a3aa1c95f5
Daniel Furnivall <danielfurnivall@gmail.com>
2023-05-03 14:27:47 +0100

    wip

--- jest.unit.config.js
+++ jest.unit.config.js
@@ -1,9 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
   displayName: 'unit',
   testEnvironment: 'node',
   cache: true,
+  transform: {
+  '\\.[jt]sx?$': 'babel-jest'
+  },
   coveragePathIgnorePatterns: ['<rootDir>/src/main/assets'],
   roots: ['<rootDir>/src/test/unit', '<rootDir>/src/test/routes'],
   testRegex: '(/src/test/.*|\\.(test|spec))\\.(ts|js)$',
--- jest.views.config.js
+++ jest.views.config.js
@@ -2,9 +2,11 @@ module.exports = {
   displayName: 'views',
   cache: true,
   roots: ['<rootDir>/src/test/unit/views'],
+  transform: {
+  '\\.[jt]sx?$': 'babel-jest'
+  },
   testRegex: '(/src/test/.*|\\.(test|spec))\\.(ts|js)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  preset: 'ts-jest',
   modulePathIgnorePatterns: ['<rootDir>/src/test/unit/mocks', '<rootDir>/src/test/unit/test-helpers'],
   testEnvironment: 'jest-environment-jsdom',
 };
