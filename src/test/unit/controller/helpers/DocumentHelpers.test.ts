07c0819e533f90afb87ead89f19a0ad0c000d2e3
gauravchawla <gaurav-chawla@hotmail.com>
2023-05-10 16:10:03 +0100

    conflicts

--- src/test/unit/controller/helpers/DocumentHelpers.test.ts
+++ src/test/unit/controller/helpers/DocumentHelpers.test.ts
@@ -1,18 +1,18 @@
 import axios, { AxiosResponse } from 'axios';
 
-import { Document } from '../../../../main/definitions/case';
-import * as caseService from '../../../../main/services/CaseService';
-import { CaseApi } from '../../../../main/services/CaseService';
 import {
   combineDocuments,
-  createDownloadLink,
-  getDocumentAdditionalInformation,
   combineUserCaseDocuments,
+  createDownloadLink,
   findContentTypeByDocument,
   findContentTypeByDocumentDetail,
   findDocumentMimeTypeByExtension,
+  getDocumentAdditionalInformation,
 } from '../../../../main/controllers/helpers/DocumentHelpers';
+import { Document } from '../../../../main/definitions/case';
 import { DocumentDetail } from '../../../../main/definitions/definition';
+import * as caseService from '../../../../main/services/CaseService';
+import { CaseApi } from '../../../../main/services/CaseService';
 import mockUserCaseWithDocumentsComplete from '../../mocks/mockUserCaseWithDocumentsComplete';
 
 const TEST_DOCUMENT_CONTENT_TYPES = {
@@ -277,15 +277,15 @@ it('should update document size and mime type values', async () => {
   expect(modifiedDoc.document_mime_type).toEqual('pdf');
   it('should combine user case documents correctly', () => {
     expect(combineUserCaseDocuments([mockUserCaseWithDocumentsComplete])).toStrictEqual([
-      {description: 'Case Details - Sunday Ayeni', id: '3aa7dfc1-378b-4fa8-9a17-89126fae5673', type: 'ET1'},
-      {id: '1', description: 'desc1'},
-      {id: '2', description: 'desc2'},
-      {id: '3', description: 'desc3'},
-      {id: '4', description: 'desc4'},
-      {id: '5', description: 'desc5'},
-      {id: '6', description: 'desc6'},
-      {id: '7', description: 'desc7'},
-      {id: '8', description: 'desc8'},
+      { description: 'Case Details - Sunday Ayeni', id: '3aa7dfc1-378b-4fa8-9a17-89126fae5673', type: 'ET1' },
+      { id: '1', description: 'desc1' },
+      { id: '2', description: 'desc2' },
+      { id: '3', description: 'desc3' },
+      { id: '4', description: 'desc4' },
+      { id: '5', description: 'desc5' },
+      { id: '6', description: 'desc6' },
+      { id: '7', description: 'desc7' },
+      { id: '8', description: 'desc8' },
       {
         id: 'a0c113ec-eede-472a-a59c-f2614b48177c',
         description: 'Claim Summary File Detail',
@@ -325,4 +325,4 @@ it('should update document size and mime type values', async () => {
       }
     );
   });
-}
+});
