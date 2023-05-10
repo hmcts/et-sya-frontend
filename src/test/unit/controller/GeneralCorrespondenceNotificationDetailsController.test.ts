fd24a15994cead114dfdc862b84dd1518677a1ef
ikirsanov <ivan.kirsanov@version1.com>
2023-05-10 14:20:20 +0200

    RET-3345: GeneralCorrespondenceNotificationDetailsController.test fix

--- src/test/unit/controller/GeneralCorrespondenceNotificationDetailsController.test.ts
+++ src/test/unit/controller/GeneralCorrespondenceNotificationDetailsController.test.ts
@@ -1,5 +1,9 @@
 import GeneralCorrespondenceNotificationDetailsController from '../../../main/controllers/GeneralCorrespondenceNotificationDetailsController';
 import { CaseWithId } from '../../../main/definitions/case';
+import {
+  SendNotificationType,
+  SendNotificationTypeItem,
+} from '../../../main/definitions/complexTypes/sendNotificationTypeItem';
 import { PageUrls } from '../../../main/definitions/constants';
 import generalRespondenceRaw from '../../../main/resources/locales/en/translation/general-correspondence-notification-details.json';
 import { mockNotificationItem } from '../mocks/mockNotificationItem';
@@ -17,7 +21,16 @@ describe('General correspondence notification details controller', () => {
     const response = mockResponse();
     const request = mockRequestWithTranslation({ userCase }, translationJsons);
     request.url = PageUrls.GENERAL_CORRESPONDENCE_NOTIFICATION_DETAILS;
-    request.params.orderId = '1';
+    request.params.itemId = '1';
+
+    const generalCorrespondence = {
+      id: '1',
+      value: {
+        sendNotificationCaseManagement: undefined,
+        notificationState: 'viewed',
+      } as SendNotificationType,
+    } as SendNotificationTypeItem;
+    request.session.userCase.sendNotificationCollection = [generalCorrespondence];
 
     const controller = new GeneralCorrespondenceNotificationDetailsController();
     controller.get(request, response);
