ceb651e2fe481fb2d271f3d8346df7db60111ac0
lvo700 <lorr05@qub.ac.uk>
2023-05-23 14:44:31 +0100

    Update controllers

--- src/main/controllers/PrepareDocumentsController.ts
+++ src/main/controllers/PrepareDocumentsController.ts
@@ -9,12 +9,14 @@ export default class PrepareDocumentsController {
   public get(req: AppRequest, res: Response): void {
     const languageParam = getLanguageParam(req.url);
     const redirectUrl = PageUrls.AGREEING_DOCUMENTS_FOR_HEARING + languageParam;
+    const cancelPage = `/citizen-hub/${req.session.userCase?.id}${languageParam}`;
 
     res.render(TranslationKeys.PREPARE_DOCUMENTS, {
       ...req.t(TranslationKeys.SIDEBAR_CONTACT_US, { returnObjects: true }),
       ...req.t(TranslationKeys.PREPARE_DOCUMENTS, { returnObjects: true }),
       redirectUrl,
       hideContactUs: true,
+      cancelPage,
     });
   }
 }
