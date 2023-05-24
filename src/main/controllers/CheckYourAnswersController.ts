35a54b99a4777e3d7a4f5e2874e0fbb422eac0ef
gauravchawla <gaurav-chawla@hotmail.com>
2023-05-24 21:24:37 +0100

    RET-3452

--- src/main/controllers/CheckYourAnswersController.ts
+++ src/main/controllers/CheckYourAnswersController.ts
@@ -17,6 +17,9 @@ export default class CheckYourAnswersController {
     }
     const userCase = req.session?.userCase;
     if (userCase?.typeOfClaim === undefined || userCase?.typeOfClaim.length === 0) {
+      if (req.session.errors === undefined) {
+        req.session.errors = [];
+      }
       req.session.errors.push({ propertyName: 'typeOfClaim', errorType: 'required' });
     }
 
