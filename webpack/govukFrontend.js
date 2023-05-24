676ab322a7b65c594dc7bdc3ab18edfb646fe462
Daniel Furnivall <danielfurnivall@gmail.com>
2023-05-24 13:48:05 +0100

    test for pipeline

--- webpack/govukFrontend.js
+++ webpack/govukFrontend.js
@@ -3,9 +3,9 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const root = require.resolve('govuk-frontend');
-const sass = path.join(root, '../govuk/all.scss');
+const sass = path.join(root, '../all.scss');
 const javascript = path.join(root, 'all.js');
-const components = path.join(root, '../govuk/components');
+const components = path.join(root, '../components');
 const assets = path.join(root, '../assets');
 const images = path.join(assets, 'images');
 const fonts = path.join(assets, 'fonts');
