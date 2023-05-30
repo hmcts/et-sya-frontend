c0cf0a5078dc50c197a6ecceff71d189f1824940
GauravChawlaMOJ <58194626+GauravChawlaMOJ@users.noreply.github.com>
2023-05-30 18:16:48 +0100

    Ret 3732 (#824)
    * RET-3732
    
    * tests added
    
    * tests added
    
    * tests added
    
    * tests added
    
    * tests added
    
    * tests added
    
    * tests added
    
    * tests modified
    
    ---------
    
    Co-authored-by: gauravchawla <gaurav-chawla@hotmail.com>

---
+++ src/test/unit/views/address-postcode-enter.test.ts
@@ -0,0 +1,57 @@
+import fs from 'fs';
+import path from 'path';
+
+import { expect } from 'chai';
+import request from 'supertest';
+
+import { PageUrls } from '../../../main/definitions/constants';
+import { mockApp } from '../mocks/mockApp';
+
+const addressPostCodeEnterJsonRaw = fs.readFileSync(
+  path.resolve(__dirname, '../../../main/resources/locales/en/translation/common.json'),
+  'utf-8'
+);
+const addressPostCodeEnterJson = JSON.parse(addressPostCodeEnterJsonRaw);
+
+const titleClass = 'govuk-heading-xl';
+const labelClass = 'govuk-label govuk-label--m';
+const expectedAddressPageHeader = addressPostCodeEnterJson.addressPageHeader;
+const expectedEnterPostcode = addressPostCodeEnterJson.enterPostcode;
+const buttonClass = 'govuk-button';
+let htmlRes: Document;
+describe('Address Postcode enter page', () => {
+  beforeAll(async () => {
+    await request(
+      mockApp({
+        userCase: {
+          addressEnterPostcode: 'LS12DE',
+        },
+      })
+    )
+      .get(PageUrls.ADDRESS_POSTCODE_ENTER)
+      .then(res => {
+        htmlRes = new DOMParser().parseFromString(res.text, 'text/html');
+        console.log(res.text);
+      });
+  });
+
+  it('should display correct label', () => {
+    console.log(htmlRes.body);
+    const label = htmlRes.getElementsByClassName(labelClass);
+    expect(label[0].innerHTML).contains(expectedEnterPostcode, 'label does not exist');
+  });
+
+  it('should display save and continue button', () => {
+    const button = htmlRes.getElementsByClassName(buttonClass);
+    expect(button[5].innerHTML).contains('Save and continue', 'Could not find the button');
+  });
+
+  it('should display Save as draft button', () => {
+    const button = htmlRes.getElementsByClassName(buttonClass);
+    expect(button[6].innerHTML).contains('Save as draft', 'Could not find the button');
+  });
+  it('should display correct heading', () => {
+    const title = htmlRes.getElementsByClassName(titleClass);
+    expect(title[0].innerHTML).contains(expectedAddressPageHeader, 'Page title does not exist');
+  });
+});
---
+++ src/test/unit/views/respondent-postcode-enter.test.ts
@@ -0,0 +1,54 @@
+import fs from 'fs';
+import path from 'path';
+
+import { expect } from 'chai';
+import request from 'supertest';
+
+import { mockApp } from '../mocks/mockApp';
+
+const respondentPostCodeEnterJsonRaw = fs.readFileSync(
+  path.resolve(__dirname, '../../../main/resources/locales/en/translation/common.json'),
+  'utf-8'
+);
+const respondentPostCodeEnterJson = JSON.parse(respondentPostCodeEnterJsonRaw);
+
+const titleClass = 'govuk-heading-xl';
+const labelClass = 'govuk-label govuk-label--m';
+const expectedRespondentPageHeader = respondentPostCodeEnterJson.respondentPageHeader;
+const expectedEnterPostcode = respondentPostCodeEnterJson.enterPostcode;
+const buttonClass = 'govuk-button';
+let htmlRes: Document;
+describe('Respondent Postcode enter page', () => {
+  beforeAll(async () => {
+    await request(
+      mockApp({
+        userCase: {
+          respondentEnterPostcode: 'LS12DE',
+        },
+      })
+    )
+      .get('/respondent/1/respondent-postcode-enter')
+      .then(res => {
+        htmlRes = new DOMParser().parseFromString(res.text, 'text/html');
+      });
+  });
+
+  it('should display correct label', () => {
+    const label = htmlRes.getElementsByClassName(labelClass);
+    expect(label[0].innerHTML).contains(expectedEnterPostcode, 'label does not exist');
+  });
+
+  it('should display save and continue button', () => {
+    const button = htmlRes.getElementsByClassName(buttonClass);
+    expect(button[5].innerHTML).contains('Save and continue', 'Could not find the button');
+  });
+
+  it('should display Save as draft button', () => {
+    const button = htmlRes.getElementsByClassName(buttonClass);
+    expect(button[6].innerHTML).contains('Save as draft', 'Could not find the button');
+  });
+  it('should display correct heading', () => {
+    const title = htmlRes.getElementsByClassName(titleClass);
+    expect(title[0].innerHTML).contains(expectedRespondentPageHeader, 'Page title does not exist');
+  });
+});
