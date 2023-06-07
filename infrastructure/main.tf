41c4c3b041c70ad75fe8af366845c2500000fe59
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2023-06-07 14:33:24 +0100

    Redis test

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,19 +6,16 @@ locals {
   vaultName = "${var.product}-${var.env}"
 }
 
-data "azurerm_subnet" "core_infra_redis_subnet" {
-  name                 = "core-infra-subnet-1-${var.env}"
-  virtual_network_name = "core-infra-vnet-${var.env}"
-  resource_group_name = "core-infra-${var.env}"
-}
-
 module "et-frontend-session-storage" {
-  source   = "git@github.com:hmcts/cnp-module-redis?ref=master"
+  source   = "git@github.com:hmcts/cnp-module-redis?ref=add-redis-version-flag"
   product  = "${var.product}-${var.component}-session-storage"
   location = var.location
   env      = var.env
-  subnetid = data.azurerm_subnet.core_infra_redis_subnet.id
   common_tags  = var.common_tags
+  private_endpoint_enabled = true
+  redis_version = "6"
+  business_area = "cft"
+  public_network_access_enabled = false
 }
 
 data "azurerm_key_vault" "key_vault" {
