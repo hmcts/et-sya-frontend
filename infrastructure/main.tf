bfafead47f2c8b11dfffbb0709cfed3c2f5ae50b
mehmet.dede <mehmet.dede@justice.gov.uk>
2023-06-26 11:03:06 +0100

    Redis V6 Upgrade

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,18 +6,15 @@ locals {
   vaultName = "${var.product}-${var.env}"
 }
 
-data "azurerm_subnet" "core_infra_redis_subnet" {
-  name                 = "core-infra-subnet-1-${var.env}"
-  virtual_network_name = "core-infra-vnet-${var.env}"
-  resource_group_name = "core-infra-${var.env}"
-}
-
 module "et-frontend-session-storage" {
   source   = "git@github.com:hmcts/cnp-module-redis?ref=master"
   product  = "${var.product}-${var.component}-session-storage"
   location = var.location
   env      = var.env
-  subnetid = data.azurerm_subnet.core_infra_redis_subnet.id
+  private_endpoint_enabled = true
+  redis_version = "6"
+  business_area = "cft"
+  public_network_access_enabled = false
   common_tags  = var.common_tags
 }
 
