1ee7a27e2c224199ab20a2a8983394030fb8c657
Itay-Kirsh <Itayk747@gmail.com>
2023-07-03 17:06:12 +0100

    stop using chai

--- src/test/unit/views/respondent-application-details.test.ts
+++ src/test/unit/views/respondent-application-details.test.ts
@@ -1,7 +1,6 @@
 import fs from 'fs';
 import path from 'path';
 
-import { expect } from 'chai';
 import request from 'supertest';
 
 import { YesOrNo } from '../../../main/definitions/case';
@@ -120,106 +119,97 @@ describe('Respondent Application details page', () => {
 
   it('should display title', () => {
     const title = htmlRes.getElementsByClassName(titleClass);
-    expect(title[0].innerHTML).contains(respondentApplicationDetailsJSON.applicationTo + 'amend response');
+    expect(title[0].innerHTML).toContain(respondentApplicationDetailsJSON.applicationTo + 'amend response');
   });
 
   it('should display respondent application row headers', () => {
     const rowHeader = htmlRes.getElementsByClassName(summaryListKeyClass);
-    expect(rowHeader[0].innerHTML).contains(respondentAppRowHeader1, 'Header does not exist');
-    expect(rowHeader[1].innerHTML).contains(respondentAppRowHeader2, 'Header does not exist');
-    expect(rowHeader[2].innerHTML).contains(respondentAppRowHeader3, 'Header does not exist');
-    expect(rowHeader[3].innerHTML).contains(respondentAppRowHeader4, 'Header does not exist');
-    expect(rowHeader[4].innerHTML).contains(respondentAppRowHeader5, 'Header does not exist');
-    expect(rowHeader[5].innerHTML).contains(respondentAppRowHeader6, 'Header does not exist');
+    expect(rowHeader[0].innerHTML).toContain(respondentAppRowHeader1);
+    expect(rowHeader[1].innerHTML).toContain(respondentAppRowHeader2);
+    expect(rowHeader[2].innerHTML).toContain(respondentAppRowHeader3);
+    expect(rowHeader[3].innerHTML).toContain(respondentAppRowHeader4);
+    expect(rowHeader[4].innerHTML).toContain(respondentAppRowHeader5);
+    expect(rowHeader[5].innerHTML).toContain(respondentAppRowHeader6);
   });
 
   it('should display respondent application details', () => {
     const summaryListData = htmlRes.getElementsByClassName(summaryListValueClass);
-    expect(summaryListData[0].innerHTML).contains('Respondent', 'Applicant does not exist');
-    expect(summaryListData[1].innerHTML).contains('7 March 2023', 'Application type does not exist');
-    expect(summaryListData[2].innerHTML).contains('amend response', 'Application date does not exist');
-    expect(summaryListData[3].innerHTML).contains('Test text', 'Application detail does not exist');
-    expect(summaryListData[4].innerHTML).contains('', 'Supporting material does not exist');
-    expect(summaryListData[5].innerHTML).contains(YesOrNo.YES, 'Rule 92 answer does not exist');
+    expect(summaryListData[0].innerHTML).toContain('Respondent');
+    expect(summaryListData[1].innerHTML).toContain('7 March 2023');
+    expect(summaryListData[2].innerHTML).toContain('amend response');
+    expect(summaryListData[3].innerHTML).toContain('Test text');
+    expect(summaryListData[4].innerHTML).toContain('');
+    expect(summaryListData[5].innerHTML).toContain(YesOrNo.YES);
   });
 
   it('should display claimant response header', () => {
     const title = htmlRes.getElementsByClassName(summaryListTitleClass);
-    expect(title[0].innerHTML).contains(expectedResponseSummaryListHeader);
+    expect(title[0].innerHTML).toContain(expectedResponseSummaryListHeader);
   });
 
   it('should display claimant response row headers', () => {
     const rowHeader = htmlRes.getElementsByClassName(summaryListKeyClass);
-    expect(rowHeader[7].innerHTML).contains(claimantResponseRowHeader1, 'Header does not exist');
-    expect(rowHeader[8].innerHTML).contains(claimantResponseRowHeader2, 'Header does not exist');
-    expect(rowHeader[9].innerHTML).contains(claimantResponseRowHeader3, 'Header does not exist');
-    expect(rowHeader[10].innerHTML).contains(claimantResponseRowHeader4, 'Header does not exist');
-    expect(rowHeader[11].innerHTML).contains(claimantResponseRowHeader5, 'Header does not exist');
+    expect(rowHeader[7].innerHTML).toContain(claimantResponseRowHeader1);
+    expect(rowHeader[8].innerHTML).toContain(claimantResponseRowHeader2);
+    expect(rowHeader[9].innerHTML).toContain(claimantResponseRowHeader3);
+    expect(rowHeader[10].innerHTML).toContain(claimantResponseRowHeader4);
+    expect(rowHeader[11].innerHTML).toContain(claimantResponseRowHeader5);
   });
 
   it('should display claimant response details', () => {
     const summaryListData = htmlRes.getElementsByClassName(summaryListValueClass);
-    expect(summaryListData[6].innerHTML).contains('Claimant', 'Applicant does not exist');
-    expect(summaryListData[7].innerHTML).contains('20 March 2023', 'Application date does not exist');
-    expect(summaryListData[8].innerHTML).contains('Response text', 'Application detail does not exist');
-    expect(summaryListData[9].innerHTML).contains('', 'Supporting material field should be blank');
-    expect(summaryListData[10].innerHTML).contains(YesOrNo.YES, 'Rule 92 answer does not exist');
+    expect(summaryListData[6].innerHTML).toContain('Claimant');
+    expect(summaryListData[7].innerHTML).toContain('20 March 2023');
+    expect(summaryListData[8].innerHTML).toContain('Response text');
+    expect(summaryListData[9].innerHTML).toContain('');
+    expect(summaryListData[10].innerHTML).toContain(YesOrNo.YES);
   });
 
   it('should display admin decision row headers for latest admin decision, with no space at the top of the table', () => {
     const rowHeader = htmlRes.getElementsByClassName(summaryListKeyClass);
-    expect(rowHeader[12].innerHTML).contains(adminDecisionRowHeader1, 'Header does not exist');
-    expect(rowHeader[13].innerHTML).contains(adminDecisionRowHeader2, 'Header does not exist');
-    expect(rowHeader[14].innerHTML).contains(adminDecisionRowHeader3, 'Header does not exist');
-    expect(rowHeader[15].innerHTML).contains(adminDecisionRowHeader4, 'Header does not exist');
-    expect(rowHeader[16].innerHTML).contains(adminDecisionRowHeader5, 'Header does not exist');
-    expect(rowHeader[17].innerHTML).contains(adminDecisionRowHeader6, 'Header does not exist');
-    expect(rowHeader[18].innerHTML).contains(adminDecisionRowHeader7, 'Header does not exist');
-    expect(rowHeader[19].innerHTML).contains(adminDecisionRowHeader8, 'Header does not exist');
-    expect(rowHeader[20].innerHTML).contains(adminDecisionRowHeader9, 'Header does not exist');
-    expect(rowHeader[21].innerHTML).contains(adminDecisionRowHeader10, 'Header does not exist');
+    expect(rowHeader[12].innerHTML).toContain(adminDecisionRowHeader1);
+    expect(rowHeader[13].innerHTML).toContain(adminDecisionRowHeader2);
+    expect(rowHeader[14].innerHTML).toContain(adminDecisionRowHeader3);
+    expect(rowHeader[15].innerHTML).toContain(adminDecisionRowHeader4);
+    expect(rowHeader[16].innerHTML).toContain(adminDecisionRowHeader5);
+    expect(rowHeader[17].innerHTML).toContain(adminDecisionRowHeader6);
+    expect(rowHeader[18].innerHTML).toContain(adminDecisionRowHeader7);
+    expect(rowHeader[19].innerHTML).toContain(adminDecisionRowHeader8);
+    expect(rowHeader[20].innerHTML).toContain(adminDecisionRowHeader9);
+    expect(rowHeader[21].innerHTML).toContain(adminDecisionRowHeader10);
   });
 
   it('if more than one admin decision, should display in order starting with the most recent', () => {
     const summaryListData = htmlRes.getElementsByClassName(summaryListValueClass);
-    expect(summaryListData[11].innerHTML).contains('Decision title 2 test text', 'Decision title does not exist');
-    expect(summaryListData[12].innerHTML).contains('Granted', 'Decision does not exist');
-    expect(summaryListData[13].innerHTML).contains('4 March 2023', 'Decision date does not exist');
-    expect(summaryListData[14].innerHTML).contains('Tribunal', 'Decision maker does not exist');
-    expect(summaryListData[15].innerHTML).contains('Judgment', 'Decision type does not exist');
-    expect(summaryListData[16].innerHTML).contains(
-      'Additional info 2 test text',
-      'Decision additional info does not exist'
-    );
-    expect(summaryListData[17].innerHTML).contains('', 'Supporting material should be blank');
-    expect(summaryListData[18].innerHTML).contains('Judge', 'Decision made by does not exist');
-    expect(summaryListData[19].innerHTML).contains('Mr Decider', 'Decision maker name does not exist');
-    expect(summaryListData[20].innerHTML).contains('Both parties', 'Decision party notification does not exist');
+    expect(summaryListData[11].innerHTML).toContain('Decision title 2 test text');
+    expect(summaryListData[12].innerHTML).toContain('Granted');
+    expect(summaryListData[13].innerHTML).toContain('4 March 2023');
+    expect(summaryListData[14].innerHTML).toContain('Tribunal');
+    expect(summaryListData[15].innerHTML).toContain('Judgment');
+    expect(summaryListData[16].innerHTML).toContain('Additional info 2 test text');
+    expect(summaryListData[17].innerHTML).toContain('');
+    expect(summaryListData[18].innerHTML).toContain('Judge');
+    expect(summaryListData[19].innerHTML).toContain('Mr Decider');
+    expect(summaryListData[20].innerHTML).toContain('Both parties');
   });
 
   it('should display the first row of older admin decisions with a space at the top', () => {
     const rowHeader = htmlRes.getElementsByClassName(summaryListKeyClass);
-    expect(rowHeader[23].innerHTML).contains(adminDecisionRowHeader11, 'Header does not exist');
+    expect(rowHeader[23].innerHTML).toContain(adminDecisionRowHeader11);
   });
 
   it('should display remaining admin decisions in descending order by application number', () => {
     const summaryListData = htmlRes.getElementsByClassName(summaryListValueClass);
-    expect(summaryListData[21].innerHTML).contains(
-      '<br><br>Decision title 1 test text',
-      'Decision title does not exist'
-    );
-    expect(summaryListData[22].innerHTML).contains('Granted', 'Decision does not exist');
-    expect(summaryListData[23].innerHTML).contains('3 March 2023', 'Decision date does not exist');
-    expect(summaryListData[24].innerHTML).contains('Tribunal', 'Decision maker does not exist');
-    expect(summaryListData[25].innerHTML).contains('Judgment', 'Decision type does not exist');
-    expect(summaryListData[26].innerHTML).contains(
-      'Additional info 1 test text',
-      'Decision additional info does not exist'
-    );
-    expect(summaryListData[27].innerHTML).contains('', 'Supporting material should be blank');
-    expect(summaryListData[28].innerHTML).contains('Judge', 'Decision made by does not exist');
-    expect(summaryListData[29].innerHTML).contains('Mr Judgey', 'Decision maker name does not exist');
-    expect(summaryListData[30].innerHTML).contains('Both parties', 'Decision party notification does not exist');
+    expect(summaryListData[21].innerHTML).toContain('<br><br>Decision title 1 test text');
+    expect(summaryListData[22].innerHTML).toContain('Granted');
+    expect(summaryListData[23].innerHTML).toContain('3 March 2023');
+    expect(summaryListData[24].innerHTML).toContain('Tribunal');
+    expect(summaryListData[25].innerHTML).toContain('Judgment');
+    expect(summaryListData[26].innerHTML).toContain('Additional info 1 test text');
+    expect(summaryListData[27].innerHTML).toContain('');
+    expect(summaryListData[28].innerHTML).toContain('Judge');
+    expect(summaryListData[29].innerHTML).toContain('Mr Judgey');
+    expect(summaryListData[30].innerHTML).toContain('Both parties');
   });
 });
 
@@ -285,8 +275,8 @@ describe('reply button link', () => {
     const buttons = htmlRes.getElementsByClassName('govuk-button');
     console.log(JSON.stringify(buttons));
 
-    expect(buttons.length).to.equal(1);
-    expect((buttons[0] as HTMLAnchorElement).href).equal('link');
+    expect(buttons).toHaveLength(1);
+    expect((buttons[0] as HTMLAnchorElement).href).toBe('link');
   });
 
   it('should link to respond-to-respondent', async () => {
@@ -359,7 +349,7 @@ describe('reply button link', () => {
 
     const buttons = htmlRes.getElementsByClassName('govuk-button');
 
-    expect(buttons.length).to.equal(1);
-    expect((buttons[0] as HTMLAnchorElement).href).equal('link');
+    expect(buttons).toHaveLength(1);
+    expect((buttons[0] as HTMLAnchorElement).href).toBe('link');
   });
 });
