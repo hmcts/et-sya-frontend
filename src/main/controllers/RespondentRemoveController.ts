c94de5279195b8f9df46410bbfb0369aa3558565
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2023-07-04 09:57:21 +0100

    Update RespondentRemoveController.ts

--- src/main/controllers/RespondentRemoveController.ts
+++ src/main/controllers/RespondentRemoveController.ts
@@ -2,12 +2,18 @@ import { Response } from 'express';
 
 import { AppRequest } from '../definitions/appRequest';
 import { PageUrls } from '../definitions/constants';
+import { getLogger } from '../logger';
 
+import { handleUpdateDraftCase } from './helpers/CaseHelpers';
 import { deleteRespondent, getRespondentIndex } from './helpers/RespondentHelpers';
 
+const logger = getLogger('RespondentRemoveController');
+
 export default class RespondentRemoveController {
-  public get = (req: AppRequest, res: Response): void => {
+  public get = async (req: AppRequest, res: Response): Promise<void> => {
     req.session.userCase.respondents = deleteRespondent(req.session.userCase.respondents, getRespondentIndex(req));
+    req.session.save();
+    await handleUpdateDraftCase(req, logger);
 
     if (req.query !== undefined && req.query.redirect === 'answers') {
       return res.redirect(PageUrls.CHECK_ANSWERS);
