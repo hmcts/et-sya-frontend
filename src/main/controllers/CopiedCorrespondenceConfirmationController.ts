3c229287e4632da839a4d2782e571e59203c91bb
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2023-07-31 13:56:15 +0100

    Update CopiedCorrespondenceConfirmationController

--- src/main/controllers/CopiedCorrespondenceConfirmationController.ts
+++ src/main/controllers/CopiedCorrespondenceConfirmationController.ts
@@ -2,17 +2,22 @@ import { Response } from 'express';
 
 import { AppRequest } from '../definitions/appRequest';
 import { PageUrls, TranslationKeys } from '../definitions/constants';
+import { AnyRecord } from '../definitions/util-types';
 
 import { setUrlLanguage } from './helpers/LanguageHelper';
 
 export default class CopiedCorrespondenceConfirmationController {
   public get(req: AppRequest, res: Response): void {
+    const translations: AnyRecord = {
+      ...req.t(TranslationKeys.COMMON, { returnObjects: true }),
+      ...req.t(TranslationKeys.COPIED_CORRESPONDENCE_CONFIRMATION, { returnObjects: true }),
+    };
+
     const applicationType = '[Application type]';
     const fileNameOfSupportingDoc = '[file_name_of_supporting_doc]';
     const redirectUrl = setUrlLanguage(req, PageUrls.CHECKLIST);
     res.render(TranslationKeys.COPIED_CORRESPONDENCE_CONFIRMATION, {
-      ...req.t(TranslationKeys.COMMON, { returnObjects: true }),
-      ...req.t(TranslationKeys.COPIED_CORRESPONDENCE_CONFIRMATION, { returnObjects: true }),
+      ...translations,
       redirectUrl,
       applicationType,
       fileNameOfSupportingDoc,
