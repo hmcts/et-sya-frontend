e9369c26f9f3df6b03c835acbe4b45813b91adad
Jack Reeve <jack@reevies.tech>
2023-08-23 13:24:12 +0100

    Removed passing res to helper functions. Handling redirects in the controller

--- src/test/unit/controller/JudgmentDetailsController.test.ts
+++ src/test/unit/controller/JudgmentDetailsController.test.ts
@@ -1,16 +1,30 @@
+import axios from 'axios';
+
 import JudgmentDetailsController from '../../../main/controllers/JudgmentDetailsController';
 import { CaseWithId } from '../../../main/definitions/case';
 import { SendNotificationType } from '../../../main/definitions/complexTypes/sendNotificationTypeItem';
-import { PageUrls, ResponseRequired } from '../../../main/definitions/constants';
+import { ErrorPages, PageUrls, ResponseRequired, TranslationKeys } from '../../../main/definitions/constants';
 import commonRaw from '../../../main/resources/locales/en/translation/common.json';
 import judgmentDetailsRaw from '../../../main/resources/locales/en/translation/judgment-details.json';
+import * as caseService from '../../../main/services/CaseService';
+import { CaseApi } from '../../../main/services/CaseService';
+import { mockDocumentDetailsResponseData } from '../mocks/mockDocumentDetailsResponse';
+import { mockGenericTseCollection } from '../mocks/mockGenericTseCollection';
 import { mockRequestWithTranslation } from '../mocks/mockRequest';
 import { mockResponse } from '../mocks/mockResponse';
 
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+const getCaseApiMock = jest.spyOn(caseService, 'getCaseApi');
+const api = new CaseApi(mockedAxios);
+
+const documentRejection = Promise.reject(new Error('Mocked failure to get document metadata'));
+
 describe('Judgment Details Controller', () => {
+  getCaseApiMock.mockReturnValue(api);
   const translationJsons = { ...judgmentDetailsRaw, ...commonRaw };
 
-  it('should get judgment details page', () => {
+  it('should get judgment details page', async () => {
     const userCase: Partial<CaseWithId> = {
       sendNotificationCollection: [
         {
@@ -30,7 +44,32 @@ describe('Judgment Details Controller', () => {
     request.params.id = '1';
 
     const controller = new JudgmentDetailsController();
-    controller.get(request, response);
-    expect(response.render).toBeDefined();
+    await controller.get(request, response);
+    expect(response.render).toHaveBeenCalledWith(TranslationKeys.JUDGMENT_DETAILS, expect.anything());
+  });
+
+  it.each([
+    { name: 'application', url: 'uuid1' },
+    { name: 'response', url: 'uuid2' },
+    { name: 'decision', url: 'uuid3' },
+  ])('should redirect to not found page when $type document cannot be resolved', async args => {
+    api.getDocumentDetails = jest
+      .fn()
+      .mockImplementation(docId =>
+        docId === args.url ? documentRejection : Promise.resolve(mockDocumentDetailsResponseData)
+      );
+
+    const userCase: Partial<CaseWithId> = {
+      genericTseApplicationCollection: mockGenericTseCollection.slice(0, 1),
+    };
+
+    const response = mockResponse();
+    const request = mockRequestWithTranslation({ userCase }, translationJsons);
+    request.url = PageUrls.JUDGMENT_DETAILS;
+    request.params.id = '1';
+
+    const controller = new JudgmentDetailsController();
+    await controller.get(request, response);
+    expect(response.redirect).toHaveBeenCalledWith(ErrorPages.NOT_FOUND);
   });
 });
