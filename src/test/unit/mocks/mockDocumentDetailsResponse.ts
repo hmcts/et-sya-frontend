796ec7a9735c7fe72e30d7bedb65319e81d7b21b
JackReevies <43950645+JackReevies@users.noreply.github.com>
2023-08-24 11:37:36 +0100

    Removed passing res to helper functions. Handling redirects in the controller (#956)

---
+++ src/test/unit/mocks/mockDocumentDetailsResponse.ts
@@ -0,0 +1,19 @@
+import { DocumentDetailsResponse } from '../../../main/definitions/api/documentDetailsResponse';
+
+export const mockDocumentDetailsResponse: DocumentDetailsResponse = {
+  createdOn: '2000-01-01',
+  size: 420,
+  mimeType: 'application/pdf',
+  classification: 'PUBLIC',
+  originalDocumentName: 'mockDocumentName',
+  createdBy: 'mockCreatedBy',
+  lastModifiedBy: 'mockLastModifiedBy',
+  modifiedOn: '2000-01-01',
+  metadata: {
+    jurisdiction: 'EMPLOYMENT',
+    case_id: '1',
+    case_type_id: '1',
+  },
+};
+
+export const mockDocumentDetailsResponseData = { data: mockDocumentDetailsResponse };
