48435b7eee82a1747eae10b543c77c1dad9a475b
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2023-09-08 09:24:42 +0100

    Temporarily removing the path and disabling test for urgent fix

--- src/main/utils/getLegacyUrlFromLng.ts
+++ src/main/utils/getLegacyUrlFromLng.ts
@@ -3,6 +3,7 @@ import config from 'config';
 export default function getLegacyUrlFromLng(path: string, lng = 'en'): URL['href'] {
   const url: string = config.get('services.et1Legacy.url');
   const legacyUrl = new URL(`${url}`);
-  legacyUrl.pathname = lng + path;
+  // Temporarily disabling the language and path so it hit's the shutter page
+  // legacyUrl.pathname = lng + path;
   return legacyUrl.href;
 }
