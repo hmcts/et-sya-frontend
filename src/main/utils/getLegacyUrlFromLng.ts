072aade9bba6326948c13a771576f1b80a52b198
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2023-09-08 09:43:16 +0100

    prettier

--- src/main/utils/getLegacyUrlFromLng.ts
+++ src/main/utils/getLegacyUrlFromLng.ts
@@ -1,6 +1,7 @@
 import config from 'config';
 
-export default function getLegacyUrlFromLng(path: string, lng = 'en'): URL['href'] { // eslint-disable-line
+export default function getLegacyUrlFromLng(path: string, lng = 'en'): URL['href'] {
+  // eslint-disable-line
   const url: string = config.get('services.et1Legacy.url');
   const legacyUrl = new URL(`${url}`);
   // Temporarily disabling the language and path so it hit's the shutter page
