4c63c85929f9a0f49d0edf21a6fec5a7c73b8bab
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2023-09-08 09:39:34 +0100

    es-lint

--- src/main/utils/getLegacyUrlFromLng.ts
+++ src/main/utils/getLegacyUrlFromLng.ts
@@ -1,6 +1,6 @@
 import config from 'config';
 
-export default function getLegacyUrlFromLng(path: string, lng = 'en'): URL['href'] {
+export default function getLegacyUrlFromLng(path: string, lng = 'en'): URL['href'] { // eslint-disable-line
   const url: string = config.get('services.et1Legacy.url');
   const legacyUrl = new URL(`${url}`);
   // Temporarily disabling the language and path so it hit's the shutter page
