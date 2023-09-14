9cd2c1b3d14fcfa1957a451e467247b1093e8d49
lvo700 <lorr05@qub.ac.uk>
2023-09-14 15:10:25 +0100

    Update controller

--- src/main/controllers/ContactTheTribunalController.ts
+++ src/main/controllers/ContactTheTribunalController.ts
@@ -7,6 +7,7 @@ import { FormContent } from '../definitions/form';
 import { AnyRecord } from '../definitions/util-types';
 
 import { getPageContent } from './helpers/FormHelpers';
+import { getLanguageParam } from './helpers/RouterHelpers';
 
 /**
  * Controller for contact-the-tribunal page with a list of applications to start
@@ -16,7 +17,11 @@ export default class ContactTheTribunalController {
     const translations: AnyRecord = {
       ...req.t(TranslationKeys.CONTACT_THE_TRIBUNAL, { returnObjects: true }),
     };
-
+    const languageParam = getLanguageParam(req.url);
+    const showAllSectionsText = translations.showAllSections;
+    const hideAllSectionsText = translations.hideAllSections;
+    const showSectionText = translations.showSection;
+    const hideSectionText = translations.hideSection;
     const applicationsAccordionItems = applications.map(application => {
       const label = translations.sections[application].label;
       return {
@@ -24,15 +29,11 @@ export default class ContactTheTribunalController {
           text: label,
         },
         content: {
-          html:
-            "<p class='govuk-body'>" +
-            translations.sections[application].body +
-            '</p> <br>' +
-            "<a class='govuk-link govuk-body' href='/contact-the-tribunal/" +
-            application +
-            "'>" +
-            label +
-            '</a>',
+          bodyText: translations.sections[application].body,
+          link: {
+            href: `/contact-the-tribunal/${application}${languageParam}`,
+            text: label,
+          },
         },
       };
     });
@@ -45,6 +46,10 @@ export default class ContactTheTribunalController {
       ...content,
       hideContactUs: true,
       applicationsAccordionItems,
+      showAllSectionsText,
+      hideAllSectionsText,
+      showSectionText,
+      hideSectionText,
     });
   }
 }
