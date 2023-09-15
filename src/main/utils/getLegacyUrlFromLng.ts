19c3ba32afb62ca7ff06cdd601454d673ff8dc65
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2023-09-15 11:06:15 +0100

    Revert "Temporarily removing the path and disabling test for urgent fix (#963)"
    This reverts commit 22208f946442825c3c4a9a2dae0fd9f51c36492b.

--- src/main/utils/getLegacyUrlFromLng.ts
+++ src/main/utils/getLegacyUrlFromLng.ts
@@ -1,9 +1,8 @@
 import config from 'config';
-/* eslint-disable */
+
 export default function getLegacyUrlFromLng(path: string, lng = 'en'): URL['href'] {
   const url: string = config.get('services.et1Legacy.url');
   const legacyUrl = new URL(`${url}`);
-  // Temporarily disabling the language and path so it hit's the shutter page
-  // legacyUrl.pathname = lng + path;
+  legacyUrl.pathname = lng + path;
   return legacyUrl.href;
 }
