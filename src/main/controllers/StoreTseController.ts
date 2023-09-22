cfb425d0b450ef18c37ebf2c707bda2e4c3f0726
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2023-09-22 19:52:46 +0100

    Update StoreTseController

--- src/main/controllers/StoreTseController.ts
+++ src/main/controllers/StoreTseController.ts
@@ -1,26 +1,37 @@
 import { Response } from 'express';
 
 import { AppRequest } from '../definitions/appRequest';
-import { PageUrls } from '../definitions/constants';
+import { ErrorPages, PageUrls } from '../definitions/constants';
 import { HubLinkNames, HubLinkStatus } from '../definitions/hub';
 import { getLogger } from '../logger';
 
 import { clearTseFields, handleUpdateHubLinksStatuses } from './helpers/CaseHelpers';
+import { getLanguageParam } from './helpers/RouterHelpers';
 import { storeClaimantTse } from './helpers/StoreTseCaseHelpers';
 
 const logger = getLogger('StoreTseController');
 
 export default class StoreTseController {
   public get = async (req: AppRequest, res: Response): Promise<void> => {
+    const languageParam = getLanguageParam(req.url);
+    const userCase = req.session?.userCase;
+
     try {
-      const userCase = req.session?.userCase;
       userCase.hubLinksStatuses[HubLinkNames.RequestsAndApplications] = HubLinkStatus.STORED;
       await handleUpdateHubLinksStatuses(req, logger);
+    } catch (error) {
+      logger.info(error.message);
+      return res.redirect(`${ErrorPages.NOT_FOUND}${languageParam}`);
+    }
+
+    try {
       await storeClaimantTse(req, logger);
       clearTseFields(userCase);
     } catch (error) {
       logger.info(error.message);
+      return res.redirect(`${ErrorPages.NOT_FOUND}${languageParam}`);
     }
+
     return res.redirect(PageUrls.STORED_APPLICATION_CONFIRMATION);
   };
 }
