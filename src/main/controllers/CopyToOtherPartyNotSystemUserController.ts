2fcff9e11ecb32c737838c205e4121b26743b404
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2023-09-22 18:40:36 +0100

    Update CopyToOtherPartyNotSystemUserController

--- src/main/controllers/CopyToOtherPartyNotSystemUserController.ts
+++ src/main/controllers/CopyToOtherPartyNotSystemUserController.ts
@@ -1,84 +1,17 @@
 import { Response } from 'express';
 
-import { Form } from '../components/form/form';
-import { isContent2500CharsOrLess } from '../components/form/validator';
 import { AppRequest } from '../definitions/appRequest';
 import { YesOrNo } from '../definitions/case';
 import { PageUrls, TranslationKeys } from '../definitions/constants';
-import { FormContent, FormFields } from '../definitions/form';
 import { AnyRecord } from '../definitions/util-types';
 
 import { setUserCase } from './helpers/CaseHelpers';
 import { getCaptionTextForCopyToOtherParty, getRedirectPageUrlNotSystemUser } from './helpers/CopyToOtherPartyHelper';
 import { getCopyToOtherPartyError } from './helpers/ErrorHelpers';
-import { getPageContent } from './helpers/FormHelpers';
 import { getLanguageParam } from './helpers/RouterHelpers';
 import { getCancelLink } from './helpers/Rule92NotSystemUserHelper';
 
 export default class CopyToOtherPartyNotSystemUserController {
-  private readonly form: Form;
-
-  private readonly CopyToOtherPartyNotSystemUserContent: FormContent = {
-    fields: {
-      copyToOtherPartyYesOrNo: {
-        classes: 'govuk-radios',
-        id: 'copyToOtherPartyYesOrNo',
-        type: 'radios',
-        label: (l: AnyRecord): string => l.doYouWantToCopy,
-        labelHidden: false,
-        labelSize: 'm',
-        values: [
-          {
-            name: 'copyToOtherPartyYesOrNo',
-            label: (l: AnyRecord): string =>
-              '<p class="govuk-body">' +
-              l.yesIConfirmIWill +
-              '</p><p class="govuk-body"><strong>' +
-              l.important +
-              ':</strong> ' +
-              l.doNotSubmitYourApplication +
-              '</p><p class="govuk-body">' +
-              l.youShouldAlsoNotify +
-              '</p>',
-            value: YesOrNo.YES,
-          },
-          {
-            name: 'copyToOtherPartyYesOrNo',
-            label: (l: AnyRecord): string =>
-              '<p class="govuk-body">' +
-              l.noIDoNotWantTo +
-              '</p><p class="govuk-body"><strong>' +
-              l.important +
-              ':</strong> ' +
-              l.youMustTellTheTribunal +
-              '</p>',
-            value: YesOrNo.NO,
-            subFields: {
-              copyToOtherPartyText: {
-                id: 'copyToOtherPartyText',
-                name: 'copyToOtherPartyText',
-                type: 'textarea',
-                label: (l: AnyRecord): string => l.giveDetails,
-                labelSize: 's',
-                isPageHeading: true,
-                classes: 'govuk-textarea',
-                maxlength: 2500,
-                validator: isContent2500CharsOrLess,
-              },
-            },
-          },
-        ],
-      },
-    },
-    submit: {
-      text: (l: AnyRecord): string => l.continue,
-    },
-  };
-
-  constructor() {
-    this.form = new Form(<FormFields>this.CopyToOtherPartyNotSystemUserContent.fields);
-  }
-
   public post = async (req: AppRequest, res: Response): Promise<void> => {
     if (req.body.copyToOtherPartyYesOrNo === YesOrNo.YES) {
       req.body.copyToOtherPartyText = undefined;
@@ -96,11 +29,11 @@ export default class CopyToOtherPartyNotSystemUserController {
   };
 
   public get = (req: AppRequest, res: Response): void => {
-    const content = getPageContent(req, this.CopyToOtherPartyNotSystemUserContent, [
-      TranslationKeys.COMMON,
-      TranslationKeys.SIDEBAR_CONTACT_US,
-      TranslationKeys.COPY_TO_OTHER_PARTY_NOT_SYSTEM_USER,
-    ]);
+    const translations: AnyRecord = {
+      ...req.t(TranslationKeys.COMMON, { returnObjects: true }),
+      ...req.t(TranslationKeys.SIDEBAR_CONTACT_US, { returnObjects: true }),
+      ...req.t(TranslationKeys.COPY_TO_OTHER_PARTY_NOT_SYSTEM_USER, { returnObjects: true }),
+    };
 
     const captionTranslations: AnyRecord = {
       ...req.t(TranslationKeys.CONTACT_THE_TRIBUNAL, { returnObjects: true }),
@@ -108,7 +41,7 @@ export default class CopyToOtherPartyNotSystemUserController {
     };
 
     res.render(TranslationKeys.COPY_TO_OTHER_PARTY_NOT_SYSTEM_USER, {
-      ...content,
+      ...translations,
       applicationType: getCaptionTextForCopyToOtherParty(req, captionTranslations),
       cancelLink: getCancelLink(req),
     });
