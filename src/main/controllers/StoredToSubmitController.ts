3e3dfbf6c359e2e0724f8842d43d5c7b7164834d
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2023-09-22 17:59:06 +0100

    Update StoredToSubmitController

--- src/main/controllers/StoredToSubmitController.ts
+++ src/main/controllers/StoredToSubmitController.ts
@@ -1,24 +1,17 @@
 import { Response } from 'express';
 
-import { Form } from '../components/form/form';
-import { atLeastOneFieldIsChecked } from '../components/form/validator';
 import { AppRequest } from '../definitions/appRequest';
-import { YesOrNo } from '../definitions/case';
 import { ErrorPages, InterceptPaths, TranslationKeys } from '../definitions/constants';
-import { FormContent, FormFields } from '../definitions/form';
 import { AnyRecord } from '../definitions/util-types';
 import { getLogger } from '../logger';
 
 import { getTseApplicationDetails } from './helpers/ApplicationDetailsHelper';
-import { setUserCase } from './helpers/CaseHelpers';
 import {
   createDownloadLink,
   findSelectedGenericTseApplication,
   getDocumentLink,
   populateDocumentMetadata,
 } from './helpers/DocumentHelpers';
-import { returnSessionErrors } from './helpers/ErrorHelpers';
-import { assignFormData, getPageContent } from './helpers/FormHelpers';
 import { setUrlLanguage } from './helpers/LanguageHelper';
 import { getLanguageParam, returnNextPage } from './helpers/RouterHelpers';
 import { getAppDetailsLink, getCancelLink } from './helpers/Rule92NotSystemUserHelper';
@@ -26,41 +19,10 @@ import { getAppDetailsLink, getCancelLink } from './helpers/Rule92NotSystemUserH
 const logger = getLogger('StoredToSubmitController');
 
 export default class StoredToSubmitController {
-  private readonly form: Form;
-
-  private readonly StoredToSubmitContent: FormContent = {
-    fields: {
-      confirmCopied: {
-        id: 'confirmCopied',
-        label: l => l.haveYouCopied,
-        labelHidden: false,
-        labelSize: 'm',
-        type: 'checkboxes',
-        hint: l => l.iConfirmThatIHaveCopied,
-        validator: atLeastOneFieldIsChecked,
-        values: [
-          {
-            name: 'confirmCopied',
-            label: l => l.yesIConfirm,
-            value: YesOrNo.YES,
-          },
-        ],
-      },
-    },
-    submit: {
-      text: (l: AnyRecord): string => l.submitBtn,
-    },
-  };
-
-  constructor() {
-    this.form = new Form(<FormFields>this.StoredToSubmitContent.fields);
-  }
-
   public post = async (req: AppRequest, res: Response): Promise<void> => {
-    setUserCase(req, this.form);
-    const errors = returnSessionErrors(req, this.form);
-    if (errors.length > 0) {
-      req.session.errors = errors;
+    const confirmCopied = req.body.confirmCopied;
+    if (!confirmCopied || (confirmCopied as []).length === 0) {
+      req.session.errors = [{ errorType: 'required', propertyName: 'confirmCopied' }];
       return res.redirect(req.url);
     }
     req.session.errors = [];
@@ -87,26 +49,26 @@ export default class StoredToSubmitController {
       }
     }
 
-    const translations: AnyRecord = {
+    const appTranslations: AnyRecord = {
       ...req.t(TranslationKeys.YOUR_APPLICATIONS, { returnObjects: true }),
       ...req.t(TranslationKeys.APPLICATION_DETAILS, { returnObjects: true }),
     };
 
-    const content = getPageContent(req, this.StoredToSubmitContent, [
-      TranslationKeys.COMMON,
-      TranslationKeys.STORED_TO_SUBMIT,
-    ]);
-
-    assignFormData(req.session.userCase, this.form.getFormFields());
+    const translations: AnyRecord = {
+      ...req.t(TranslationKeys.COMMON, { returnObjects: true }),
+      ...req.t(TranslationKeys.STORED_TO_SUBMIT, { returnObjects: true }),
+    };
 
     res.render(TranslationKeys.STORED_TO_SUBMIT, {
-      ...content,
-      applicationType: translations.applicationTo + translations[selectedApplication.value.type],
-      appContent: getTseApplicationDetails(selectedApplication, translations, createDownloadLink(document)),
+      ...translations,
+      applicationType: appTranslations.applicationTo + appTranslations[selectedApplication.value.type],
+      appContent: getTseApplicationDetails(selectedApplication, appTranslations, createDownloadLink(document)),
       viewCorrespondenceLink: getAppDetailsLink(req.params.appId, getLanguageParam(req.url)),
       document,
       viewCorrespondenceFileLink: getDocumentLink(document),
       cancelLink: getCancelLink(req),
+      errors: req.session.errors,
+      errorMessage: translations.errors.confirmCopied.required,
     });
   };
 }
