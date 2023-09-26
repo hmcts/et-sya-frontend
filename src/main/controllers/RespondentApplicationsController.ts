82724c313089e7d6f1b8aa1c6273c9bea32b76e9
marcjudge <marc.judge@hmcts.net>
2023-09-26 10:12:43 +0100

    Tidy up translations

--- src/main/controllers/RespondentApplicationsController.ts
+++ src/main/controllers/RespondentApplicationsController.ts
@@ -9,7 +9,6 @@ import { getLogger } from '../logger';
 import { getFlagValue } from '../modules/featureFlag/launchDarkly';
 import { getCaseApi } from '../services/CaseService';
 
-import { retrieveCurrentLocale } from './helpers/ApplicationTableRecordTranslationHelper';
 import { getPageContent } from './helpers/FormHelpers';
 import {
   getRespondentApplications,
@@ -25,21 +24,11 @@ export default class RespondentApplicationsController {
       ));
 
       const respondentApplications = getRespondentApplications(userCase);
-
-      respondentApplications?.forEach(app => {
-        app.value.date = new Date(app.value?.date).toLocaleDateString(retrieveCurrentLocale(req.url), {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-        });
-      });
-
       const translations: AnyRecord = {
-        ...req.t(TranslationKeys.RESPONDENT_APPLICATIONS, { returnObjects: true }),
         ...req.t(TranslationKeys.CONTACT_THE_TRIBUNAL, { returnObjects: true }),
         ...req.t(TranslationKeys.CITIZEN_HUB, { returnObjects: true }),
+        ...req.t(TranslationKeys.RESPONDENT_APPLICATIONS, { returnObjects: true }),
       };
-
       populateRespondentItemsWithRedirectLinksCaptionsAndStatusColors(respondentApplications, req.url, translations);
 
       const content = getPageContent(req, {} as FormContent, [
