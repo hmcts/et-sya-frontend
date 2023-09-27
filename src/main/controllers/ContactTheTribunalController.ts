552fc40d719f2835e7cde808b5a1521bb7b7e10b
lvo700 <lorr05@qub.ac.uk>
2023-09-27 13:52:26 +0100

    Add feature flag

--- src/main/controllers/ContactTheTribunalController.ts
+++ src/main/controllers/ContactTheTribunalController.ts
@@ -5,6 +5,7 @@ import { TranslationKeys } from '../definitions/constants';
 import applications from '../definitions/contact-applications';
 import { FormContent } from '../definitions/form';
 import { AnyRecord } from '../definitions/util-types';
+import { getFlagValue } from '../modules/featureFlag/launchDarkly';
 
 import { getPageContent } from './helpers/FormHelpers';
 import { getLanguageParam } from './helpers/RouterHelpers';
@@ -13,11 +14,12 @@ import { getLanguageParam } from './helpers/RouterHelpers';
  * Controller for contact-the-tribunal page with a list of applications to start
  */
 export default class ContactTheTribunalController {
-  public get(req: AppRequest, res: Response): void {
+  public async get(req: AppRequest, res: Response): Promise<void> {
     const translations: AnyRecord = {
       ...req.t(TranslationKeys.CONTACT_THE_TRIBUNAL, { returnObjects: true }),
     };
     const languageParam = getLanguageParam(req.url);
+    const welshEnabled = await getFlagValue('welsh-language', null);
     const showAllSectionsText = translations.showAllSections;
     const hideAllSectionsText = translations.hideAllSections;
     const showSectionText = translations.showSection;
@@ -50,6 +52,7 @@ export default class ContactTheTribunalController {
       hideAllSectionsText,
       showSectionText,
       hideSectionText,
+      welshEnabled,
     });
   }
 }
--- src/test/unit/controller/ContactTheTribunalController.test.ts
+++ src/test/unit/controller/ContactTheTribunalController.test.ts
@@ -3,14 +3,18 @@ import { TranslationKeys } from '../../../main/definitions/constants';
 import contactTheTribunal from '../../../main/resources/locales/en/translation/contact-the-tribunal.json';
 import { mockRequestWithTranslation } from '../mocks/mockRequest';
 import { mockResponse } from '../mocks/mockResponse';
+import * as LaunchDarkly from '../../../main/modules/featureFlag/launchDarkly';
+
 
 describe('Contact Application Controller', () => {
-  it('should render contact application page', () => {
+  it('should render contact application page', async () => {
+    const mockLdClient = jest.spyOn(LaunchDarkly, 'getFlagValue');
+    mockLdClient.mockResolvedValue(true);
     const controller = new ContactTheTribunalController();
     const response = mockResponse();
     const request = mockRequestWithTranslation({}, contactTheTribunal);
 
-    controller.get(request, response);
+    await controller.get(request, response);
     expect(response.render).toHaveBeenCalledWith(TranslationKeys.CONTACT_THE_TRIBUNAL, expect.anything());
   });
 });
