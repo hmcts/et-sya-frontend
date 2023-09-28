70c013ee51e4eee3cb355470c6435f0f4e403b93
Brendan McHugh <brendan.mchugh@hmcts.net>
2023-09-28 10:41:27 +0100

    test: make unit test an aynsc function

--- src/test/unit/controller/YourAppsToTheTribunalController.test.ts
+++ src/test/unit/controller/YourAppsToTheTribunalController.test.ts
@@ -9,11 +9,11 @@ describe('Apps To The Tribunal Controller', () => {
   };
   const mockClient = jest.spyOn(LaunchDarkly, 'getFlagValue');
   mockClient.mockResolvedValue(true);
-  it('should render the applications page', () => {
+  it('should render the applications page', async () => {
     const controller = new YourAppsToTheTribunalController();
     const response = mockResponse();
     const request = mockRequest({ t });
-    controller.get(request, response);
+    await controller.get(request, response);
     expect(response.render).toHaveBeenCalledWith('your-applications', expect.anything());
   });
 });
