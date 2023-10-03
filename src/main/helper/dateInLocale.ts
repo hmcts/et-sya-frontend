3ae51f5d538e6814662f77dcb3ea7ce2e049127a
Brendan McHugh <brendan.mchugh@hmcts.net>
2023-10-03 14:23:21 +0100

    update comment for dateInLocale

--- src/main/helper/dateInLocale.ts
+++ src/main/helper/dateInLocale.ts
@@ -1,6 +1,6 @@
 import { retrieveCurrentLocale } from '../controllers/helpers/ApplicationTableRecordTranslationHelper';
 
-// returns Welsh or English representation of a Date instance in long format based on parm in url
+// returns Welsh or English representation of a Date instance in long format based on param in url
 export const dateInLocale = (dateToTransform: Date, url: string): string => {
   return new Date(dateToTransform).toLocaleDateString(retrieveCurrentLocale(url), {
     year: 'numeric',
@@ -9,9 +9,11 @@ export const dateInLocale = (dateToTransform: Date, url: string): string => {
   });
 };
 
-// Converts datestring to a date in the locale, returns the original string if date invalid / not parsed
+// Converts datestring to correct language (Welsh or English)
+// Checks if the string can be parsed into a valid date instance,
+// if not will return original string.
 // '2022-03-25', 'url?lng=cy' => '25 Mawrth 2022',
-// 'unknown string', 'url' => 'unknown string'
+// 'unknown string', 'url?lng=cy' => 'unknown string'
 export const datesStringToDateInLocale = (dateString: string, url: string): string => {
   if (isNaN(Date.parse(dateString)) === false) {
     return dateInLocale(new Date(dateString), url);
