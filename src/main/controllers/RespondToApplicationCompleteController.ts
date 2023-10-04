23ed1c658c6654cf78cc41fcdf7be3b229da2ec9
lvo700 <lorr05@qub.ac.uk>
2023-10-04 14:53:32 +0100

    Add feature flag

--- src/main/controllers/RespondToApplicationCompleteController.ts
+++ src/main/controllers/RespondToApplicationCompleteController.ts
@@ -3,11 +3,12 @@ import { Response } from 'express';
 import { AppRequest } from '../definitions/appRequest';
 import { YesOrNo } from '../definitions/case';
 import { TranslationKeys } from '../definitions/constants';
-
 import { getLanguageParam } from './helpers/RouterHelpers';
+import { getFlagValue } from '../modules/featureFlag/launchDarkly';
 
 export default class RespondToApplicationCompleteController {
-  public get(req: AppRequest, res: Response): void {
+  public get = async (req: AppRequest, res: Response): Promise<void> => {
+    const welshEnabled = await getFlagValue('welsh-language', null);
     const languageParam = getLanguageParam(req.url);
     const redirectUrl = `/citizen-hub/${req.session.userCase?.id}${languageParam}`;
 
@@ -16,6 +17,7 @@ export default class RespondToApplicationCompleteController {
       ...req.t(TranslationKeys.RESPOND_TO_APPLICATION_COMPLETE, { returnObjects: true }),
       rule92: YesOrNo.YES,
       redirectUrl,
+      welshEnabled,
     });
   }
 }
