224290a01a93937123a23533c9fc5e9f4982c94d
Brendan McHugh <brendan.mchugh@hmcts.net>
2023-10-16 14:06:16 +0100

    fix failing unit test

--- src/test/unit/controller/helpers/FormHelper.test.ts
+++ src/test/unit/controller/helpers/FormHelper.test.ts
@@ -3,7 +3,8 @@ import { mockHearingCollection } from '../../mocks/mockHearing';
 
 describe('createRadioBtnsForHearings - create radio buttons for selecting a hearing', () => {
   it('should return a label, name and value for each hearing', () => {
-    const radioButtons = createRadioBtnsForHearings(mockHearingCollection);
+    const collection = [...mockHearingCollection];
+    const radioButtons = createRadioBtnsForHearings(collection);
     expect(radioButtons[0]).toEqual(
       expect.objectContaining({
         label: expect.any(String),
@@ -14,23 +15,19 @@ describe('createRadioBtnsForHearings - create radio buttons for selecting a hear
   });
 
   it('should return undefined if no hearings are present for future dates', () => {
-    mockHearingCollection[0].value.hearingDateCollection[0].value.listedDate = new Date('2022-07-04T14:00:00.000');
-    const radioButtons = createRadioBtnsForHearings(mockHearingCollection);
+    const collection = [...mockHearingCollection];
+    collection[0].value.hearingDateCollection[0].value.listedDate = new Date('2022-07-04T14:00:00.000');
+    const radioButtons = createRadioBtnsForHearings(collection);
     expect(radioButtons).toEqual(undefined);
   });
 });
 
 describe('createLabelForHearing - produce a formatted label with hearing venue and date', () => {
   it('should return a label, with hearing type, location and formatted date', () => {
-    const label = createLabelForHearing(mockHearingCollection[0]);
-    mockHearingCollection[0].value.hearingDateCollection[0].value.listedDate = new Date('2024-07-04T14:00:00.000');
+    const collection = [...mockHearingCollection];
+    collection[0].value.hearingDateCollection[0].value.listedDate = new Date('2038-07-04T14:00:00.000');
+    const label = createLabelForHearing(collection[0]);
 
-    expect(label).toEqual('Hearing - RCJ - 4 Jul 2024');
-  });
-
-  it('should return undefined if no hearings are present for future dates', () => {
-    mockHearingCollection[0].value.hearingDateCollection[0].value.listedDate = new Date('2022-07-04T14:00:00.000');
-    const radioButtons = createRadioBtnsForHearings(mockHearingCollection);
-    expect(radioButtons).toEqual(undefined);
+    expect(label).toEqual('Hearing - RCJ - 4 Jul 2038');
   });
 });
