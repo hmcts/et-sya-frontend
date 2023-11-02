e5eb0643eecf3a03521cb43321c2aa9d155b71c8
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2023-11-02 12:10:32 +0000

    RET-4062 - Update API links (#1081)

--- src/main/definitions/constants.ts
+++ src/main/definitions/constants.ts
@@ -294,12 +294,12 @@ export const JavaApiUrls = {
   RESPOND_TO_APPLICATION: 'cases/respond-to-application',
   CHANGE_APPLICATION_STATUS: 'cases/change-application-status',
   SUBMIT_CLAIMANT_APPLICATION: 'cases/submit-claimant-application',
-  SUBMIT_STORED_CLAIMANT_APPLICATION: 'cases/submit-stored-claimant-application',
-  SUBMIT_STORED_RESPOND_TO_APPLICATION: 'cases/submit-stored-respond-to-application',
+  SUBMIT_STORED_CLAIMANT_APPLICATION: 'store/submit-stored-claimant-application',
+  SUBMIT_STORED_RESPOND_TO_APPLICATION: 'store/submit-stored-respond-to-application',
   TRIBUNAL_RESPONSE_VIEWED: 'cases/tribunal-response-viewed',
   ADD_RESPONSE_TO_SEND_NOTIFICATION: '/sendNotification/add-response-send-notification',
   UPDATE_NOTIFICATION_STATE: '/sendNotification/update-notification-state',
-  SUBMIT_STORED_RESPOND_TO_TRIBUNAL: '/sendNotification/submit-stored-respond-to-tribunal',
+  SUBMIT_STORED_RESPOND_TO_TRIBUNAL: '/store/submit-stored-respond-to-tribunal',
   UPDATE_ADMIN_DECISION_STATE: '/tseAdmin/update-admin-decision-state',
 } as const;
 
