96c552c00fd65ab6783a45c846dd3bd433e2a1ca
Brendan McHugh <brendan.mchugh@hmcts.net>
2023-11-22 15:43:05 +0000

    feat: add bundles feature flag to all docs page

--- src/main/controllers/AllDocumentsController.ts
+++ src/main/controllers/AllDocumentsController.ts
@@ -2,10 +2,11 @@ import { Response } from 'express';
 
 import { AppRequest } from '../definitions/appRequest';
 import { DocumentTypeItem } from '../definitions/complexTypes/documentTypeItem';
-import { AllDocumentTypes, PageUrls, TranslationKeys } from '../definitions/constants';
+import { AllDocumentTypes, FEATURE_FLAGS, PageUrls, TranslationKeys } from '../definitions/constants';
 import { FormContent } from '../definitions/form';
 import { AnyRecord } from '../definitions/util-types';
 import { getLogger } from '../logger';
+import { getFlagValue } from '../modules/featureFlag/launchDarkly';
 
 import { createSortedDocumentsMap, prepareTableRows } from './helpers/AllDocumentsHelper';
 import { createDownloadLink, getDocumentsAdditionalInformation } from './helpers/DocumentHelpers';
@@ -18,7 +19,6 @@ export default class AllDocumentsController {
     const userCase = req.session?.userCase;
     req.session.documentDownloadPage = PageUrls.ALL_DOCUMENTS;
     const languageParam = getLanguageParam(req.url);
-
     const translations: AnyRecord = {
       ...req.t(TranslationKeys.CONTACT_THE_TRIBUNAL, { returnObjects: true }),
       ...req.t(TranslationKeys.ALL_DOCUMENTS, { returnObjects: true }),
@@ -35,10 +35,13 @@ export default class AllDocumentsController {
     let sortedDocuments = undefined;
     let tribunalDocuments = undefined;
     let acasClaimantRespondentTableRows = undefined;
+    const bundlesEnabled = await getFlagValue(FEATURE_FLAGS.BUNDLES, null);
 
     const docCollection = userCase.documentCollection?.length ? userCase.documentCollection : [];
-    const bundlesClaimantDocs = userCase.bundlesClaimantDocs?.length ? userCase.bundlesClaimantDocs : [];
-    const bundlesRespondentDocs = userCase.bundlesRespondentDocs?.length ? userCase.bundlesRespondentDocs : [];
+    const bundlesClaimantDocs =
+      userCase.bundlesClaimantDocs?.length && bundlesEnabled ? userCase.bundlesClaimantDocs : [];
+    const bundlesRespondentDocs =
+      userCase.bundlesRespondentDocs?.length && bundlesEnabled ? userCase.bundlesRespondentDocs : [];
     const allDocs = [...docCollection, ...bundlesClaimantDocs, ...bundlesRespondentDocs];
 
     /**
--- src/test/unit/controller/AllDocumentsController.test.ts
+++ src/test/unit/controller/AllDocumentsController.test.ts
@@ -1,10 +1,13 @@
 import AllDocumentsController from '../../../main/controllers/AllDocumentsController';
 import { TranslationKeys } from '../../../main/definitions/constants';
+import * as LaunchDarkly from '../../../main/modules/featureFlag/launchDarkly';
 import { mockRequest } from '../mocks/mockRequest';
 import { mockResponse } from '../mocks/mockResponse';
 
 describe('AllDocumentsController', () => {
   it('should render the All documents page', async () => {
+    const mockLdClient = jest.spyOn(LaunchDarkly, 'getFlagValue');
+    mockLdClient.mockResolvedValue(true);
     const controller = new AllDocumentsController();
     const response = mockResponse();
     const request = mockRequest({});
