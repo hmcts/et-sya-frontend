40fad8319be6b1bae478b1b1ff76adafccdcda78
Brendan McHugh <brendan.mchugh@hmcts.net>
2023-11-27 17:56:30 +0000

    test: add before each hearing collection

--- src/test/unit/mocks/mockHearing.ts
+++ src/test/unit/mocks/mockHearing.ts
@@ -73,41 +73,6 @@ export const mockHearingCollection = [
             hearingTimingFinish: new Date('2023-11-13T11:00:00.000'),
           },
         },
-        {
-          id: '234',
-          value: {
-            listedDate: new Date('2024-07-04T14:00:00.000'),
-            Hearing_status: 'Listed',
-            hearingVenueDay: {
-              value: {
-                code: 'Field House',
-                label: 'Field House',
-              },
-              list_items: [
-                {
-                  code: 'Field House',
-                  label: 'Field House',
-                },
-                {
-                  code: 'Fox Court rm 1',
-                  label: 'Fox Court rm 1',
-                },
-                {
-                  code: 'London Central',
-                  label: 'London Central',
-                },
-                {
-                  code: 'RCJ',
-                  label: 'RCJ',
-                },
-              ],
-              selectedCode: 'Field House',
-              selectedLabel: 'Field House',
-            },
-            hearingTimingStart: new Date('2023-11-13T11:00:00.000'),
-            hearingTimingFinish: new Date('2023-11-13T11:00:00.000'),
-          },
-        },
       ],
     },
   },
