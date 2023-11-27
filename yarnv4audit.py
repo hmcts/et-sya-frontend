41bce2b6d9ea9d49f4dd7b527adb974e1b5daf05
Daniel Furnivall <danielfurnivall@gmail.com>
2023-11-27 11:00:13 +0000

    slight change to allow script to work on both CI and local

--- yarnv4audit.py
+++ yarnv4audit.py
@@ -146,6 +146,9 @@ def decide_what_to_print(unsuppressed_vulnerabilities, unneeded_suppressions):
         print_suppressions(unneeded_suppressions)
 
 def build_parent_json_for_cosmosDB(vulnerabilities, suppressions):
+    if os.environ.ci != 'True':
+        print("Not running in CI, skipping parent JSON block")
+        return
     parent_block = {"suppressed_vulnerabilities": [s.to_json() for s in suppressions],
                     "unsuppressed_vulnerabilities": [v.to_json() for v in vulnerabilities]}
     return json.dumps(parent_block, indent=2)
