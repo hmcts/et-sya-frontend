dc18124cf238934a5e0884b3c60da549c0cc439e
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2024-02-08 16:49:08 +0000

    Remove Textphone

--- src/test/end-to-end/helpers/commonFlowContent.js
+++ src/test/end-to-end/helpers/commonFlowContent.js
@@ -25,8 +25,6 @@ async function verifyMakeAClaimToAnEmploymentTribunal() {
   I.see('Telephone: 0300 123 1024');
   I.see('Telephone: 0300 303 5176 (Welsh language)');
   I.see('Telephone: 0300 790 6234 (Scotland)');
-  I.see('Textphone: 18001 0300 123 1024 (England and Wales)');
-  I.see('Textphone: 18001 0300 790 6234 (Scotland)');
   I.see('Monday to Friday, 9am to 5pm');
   I.see('Find out about call charges');
 }
