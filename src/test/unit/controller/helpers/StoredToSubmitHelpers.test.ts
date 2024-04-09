4da71802f18a084d5080ef4f34c3202a58ec6870
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2024-04-09 13:48:36 +0100

    Update StoredToSubmitHelpers.test.ts

--- src/test/unit/controller/helpers/StoredToSubmitHelpers.test.ts
+++ src/test/unit/controller/helpers/StoredToSubmitHelpers.test.ts
@@ -1,6 +1,7 @@
 import {
   findSelectedPseResponse,
   findSelectedSendNotification,
+  putSelectedAppToUserCase,
 } from '../../../../main/controllers/helpers/StoredToSubmitHelpers';
 import { YesOrNo } from '../../../../main/definitions/case';
 import {
@@ -50,5 +51,35 @@ describe('findSelectedPseResponse', () => {
     const expected = pseResponseType[0];
     const actual = findSelectedPseResponse(sendNotificationTypeItem[0].value.respondStoredCollection, '23456');
     expect(actual).toEqual(expected);
+  });
+});
+
+describe('putSelectedAppToUserCase', () => {
+  it('should return userCase with selectedGenericTseApplication mapping', () => {
+    const req = mockRequest({});
+    const userCase = req.session.userCase;
+    const testDoc1 = {
+      document_url: 'http://dm-store:8080/documents/10dbc31c-5bf6-4ecf-9ad7-6bbf58492afa',
+      document_filename: 'testDoc1.pdf',
+      document_binary_url: 'http://dm-store:8080/documents/10dbc31c-5bf6-4ecf-9ad7-6bbf58492afa/binary',
+    };
+    userCase.selectedGenericTseApplication = {
+      id: '23456',
+      value: {
+        applicant: Applicant.CLAIMANT,
+        date: '2 March 2023',
+        type: 'Change my personal details',
+        details: 'details text',
+        documentUpload: testDoc1,
+        copyToOtherPartyYesOrNo: YesOrNo.YES,
+      },
+    };
+
+    putSelectedAppToUserCase(userCase);
+    expect(userCase.contactApplicationType).toEqual('change-details');
+    expect(userCase.contactApplicationText).toEqual('details text');
+    expect(userCase.contactApplicationFile).toEqual(testDoc1);
+    expect(userCase.copyToOtherPartyYesOrNo).toEqual(YesOrNo.YES);
+    expect(userCase.copyToOtherPartyText).toEqual(undefined);
   });
 });
