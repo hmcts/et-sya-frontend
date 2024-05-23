7000e867d0b43cd0c8d87319872a53d536c3a71e
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2024-05-23 13:24:10 +0100

    HearingDocumentUploadController

--- src/main/controllers/HearingDocumentFileController.ts
+++ src/main/controllers/HearingDocumentFileController.ts
@@ -2,13 +2,16 @@ import { Response } from 'express';
 
 import { AppRequest } from '../definitions/appRequest';
 import { PageUrls } from '../definitions/constants';
+import { getLogger } from '../logger';
 
-import { getLanguageParam } from './helpers/RouterHelpers';
+import { getLanguageParam, returnSafeRedirectUrl } from './helpers/RouterHelpers';
+
+const logger = getLogger('HearingDocumentFileController');
 
 export default class HearingDocumentFileController {
   public get = (req: AppRequest, res: Response): void => {
     req.session.userCase.hearingDocument = undefined;
     const url = PageUrls.HEARING_DOCUMENT_UPLOAD.replace(':appId', req.params.appId) + getLanguageParam(req.url);
-    res.redirect(url);
+    res.redirect(returnSafeRedirectUrl(req, url, logger));
   };
 }
