6f09d0b7877302e5d83324b96831c333162e025c
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2024-07-18 14:49:39 +0100

    Update HearingDetailsController.ts

--- src/main/controllers/HearingDetailsController.ts
+++ src/main/controllers/HearingDetailsController.ts
@@ -3,19 +3,13 @@ import { Response } from 'express';
 import { AppRequest } from '../definitions/appRequest';
 import { TranslationKeys } from '../definitions/constants';
 import { FormContent } from '../definitions/form';
-import { AnyRecord } from '../definitions/util-types';
 import { getFlagValue } from '../modules/featureFlag/launchDarkly';
 
 import { getPageContent } from './helpers/FormHelpers';
-import { getHearingDetails } from './helpers/HearingHelpers';
 
 export default class HearingDetailsController {
   public get = async (req: AppRequest, res: Response): Promise<void> => {
     const userCase = req.session.userCase;
-
-    const translations: AnyRecord = {
-      ...req.t(TranslationKeys.HEARING_DETAILS, { returnObjects: true }),
-    };
 
     const content = getPageContent(req, <FormContent>{}, [TranslationKeys.COMMON]);
 
@@ -23,7 +17,7 @@ export default class HearingDetailsController {
 
     res.render(TranslationKeys.HEARING_DETAILS, {
       ...content,
-      hearingContent: getHearingDetails(userCase.hearingCollection, translations),
+      hearingCollection: userCase.hearingCollection,
       welshEnabled,
     });
   };
--- src/main/controllers/helpers/HearingHelpers.ts
+++ src/main/controllers/helpers/HearingHelpers.ts
@@ -1,7 +1,5 @@
-import { HearingDateCollection, HearingModel } from '../../definitions/api/caseApiResponse';
-import { SummaryListRow, addSummaryRow } from '../../definitions/govuk/govukSummaryList';
+import { HearingModel } from '../../definitions/api/caseApiResponse';
 import { HearingNotification } from '../../definitions/hearingNotification';
-import { AnyRecord } from '../../definitions/util-types';
 
 export const setNextListedDate = (hearingCollection: HearingModel[]): HearingNotification => {
   // Check if hearingCollection is not empty
@@ -10,7 +8,7 @@ export const setNextListedDate = (hearingCollection: HearingModel[]): HearingNot
   }
 
   const now = new Date();
-  const nextHearing: HearingNotification | undefined = undefined;
+  let nextHearing: HearingNotification = undefined;
 
   // Extract all listed dates from the hearing collection
   hearingCollection.forEach(hearing => {
@@ -18,78 +16,15 @@ export const setNextListedDate = (hearingCollection: HearingModel[]): HearingNot
       hearing.value.hearingDateCollection.forEach(dateCollection => {
         const listedDate = new Date(dateCollection.value.listedDate);
         // Check if the listedDate is in the future
-        if (
-          listedDate &&
-          listedDate > now &&
-          (nextHearing.nextHearingDate === undefined || listedDate < nextHearing.nextHearingDate)
-        ) {
-          nextHearing.nextHearingDate = listedDate;
-          nextHearing.hearingId = hearing.id;
+        if (listedDate && listedDate > now && (!nextHearing || listedDate < nextHearing.nextHearingDate)) {
+          nextHearing = {
+            nextHearingDate: listedDate,
+            hearingId: hearing.id,
+          };
         }
       });
     }
   });
 
   return nextHearing;
-};
-
-export const getHearingModelWithEarliestDate = (hearingModels: HearingModel[]): HearingModel => {
-  let earliestHearingModel: HearingModel | undefined;
-  let earliestDate: Date | null = null;
-
-  hearingModels.forEach(hearing => {
-    const earliestHearing = getEarliestFutureHearing(hearing.value.hearingDateCollection);
-
-    if (earliestHearing) {
-      const listedDate = earliestHearing.value.listedDate;
-      if (!earliestDate || listedDate < earliestDate) {
-        earliestDate = listedDate;
-        earliestHearingModel = hearing;
-      }
-    }
-  });
-
-  return earliestHearingModel;
-};
-
-export const getEarliestFutureHearing = (hearingDateCollection: HearingDateCollection[]): HearingDateCollection => {
-  // Get current date
-  const currentDate = new Date();
-
-  // Filter dates in the future
-  const futureDates = hearingDateCollection.filter(hearing => hearing.value.listedDate > currentDate);
-
-  // Find the earliest date
-  let earliestDate: Date | null = null;
-  let earliestHearing: HearingDateCollection | undefined;
-
-  futureDates.forEach(hearing => {
-    if (!earliestDate || hearing.value.listedDate < earliestDate) {
-      earliestDate = hearing.value.listedDate;
-      earliestHearing = hearing;
-    }
-  });
-
-  return earliestHearing;
-};
-
-export const getHearingDetails = (hearingCollection: HearingModel[], translations: AnyRecord): SummaryListRow[] => {
-  const rows: SummaryListRow[] = [];
-  hearingCollection.forEach(h => rows.push(...getFromEachHearingCollection(h, translations)));
-  return rows;
-};
-
-const getFromEachHearingCollection = (hearingModel: HearingModel, translations: AnyRecord): SummaryListRow[] => {
-  const hearing = hearingModel.value;
-  const rows: SummaryListRow[] = [];
-  rows.push(addSummaryRow(translations.hearingType, hearing.Hearing_type));
-  rows.push(addSummaryRow(translations.hearingVenue, hearing.Hearing_venue.value.label));
-  rows.push(addSummaryRow(translations.hearingFormat, hearing.hearingFormat.join(', ')));
-  rows.push(addSummaryRow(translations.hearingPublicPrivate, hearing.hearingPublicPrivate));
-  rows.push(addSummaryRow(translations.hearingNumber, hearing.hearingNumber));
-  rows.push(addSummaryRow(translations.hearingEstLengthNum, hearing.hearingEstLengthNum));
-  rows.push(addSummaryRow(translations.hearingEstLengthNumType, hearing.hearingEstLengthNum));
-  rows.push(addSummaryRow(translations.hearingSitAlone, hearing.hearingSitAlone));
-  rows.push(addSummaryRow(translations.judge, hearing.Hearing_judge_name));
-  return rows;
 };
