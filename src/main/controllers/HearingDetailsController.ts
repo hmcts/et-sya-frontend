8bfa78f30edbe66f90702352d026169cdf3c249e
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2024-09-09 16:26:42 +0100

    Create HearingDetailsController

---
+++ src/main/controllers/HearingDetailsController.ts
@@ -0,0 +1,33 @@
+import { Response } from 'express';
+
+import { AppRequest } from '../definitions/appRequest';
+import { TranslationKeys } from '../definitions/constants';
+import { AnyRecord } from '../definitions/util-types';
+import { getFlagValue } from '../modules/featureFlag/launchDarkly';
+
+import { getHearingCollection } from './helpers/HearingHelpers';
+
+export default class HearingDetailsController {
+  public get = async (req: AppRequest, res: Response): Promise<void> => {
+    const userCase = req.session.userCase;
+
+    const welshEnabled = await getFlagValue('welsh-language', null);
+
+    const translations: AnyRecord = {
+      ...req.t(TranslationKeys.CITIZEN_HUB, { returnObjects: true }),
+    };
+
+    res.render(TranslationKeys.HEARING_DETAILS, {
+      ...req.t(TranslationKeys.COMMON, { returnObjects: true }),
+      ...req.t(TranslationKeys.SIDEBAR_CONTACT_US, { returnObjects: true }),
+      ...req.t(TranslationKeys.CITIZEN_HUB, { returnObjects: true }),
+      ...req.t(TranslationKeys.HEARING_DETAILS, { returnObjects: true }),
+      hearingDetailsCollection: getHearingCollection(
+        userCase.hearingCollection,
+        userCase.sendNotificationCollection,
+        translations
+      ),
+      welshEnabled,
+    });
+  };
+}
