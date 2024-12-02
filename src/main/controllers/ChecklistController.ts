8b6cba12699427c6ae0f075c2fbbed7402744427
Lahiru Oshara Hinguruduwa <35135059+LahiruOshara@users.noreply.github.com>
2024-12-02 20:33:23 +0000

    enable secure flags test CHECKLIST

--- src/main/controllers/ChecklistController.ts
+++ src/main/controllers/ChecklistController.ts
@@ -4,10 +4,17 @@ import { AppRequest } from '../definitions/appRequest';
 import { PageUrls, TranslationKeys } from '../definitions/constants';
 
 import { setUrlLanguage } from './helpers/LanguageHelper';
+import { getLanguageParam } from './helpers/RouterHelpers';
 
 export default class ChecklistController {
   public get(req: AppRequest, res: Response): void {
     const redirectUrl = setUrlLanguage(req, PageUrls.LIP_OR_REPRESENTATIVE);
+    const languageParam = getLanguageParam(req.url);
+    res.cookie('i18next', languageParam, {
+      secure: true,
+      sameSite: 'strict',
+      httpOnly: true,
+    });
     res.render(TranslationKeys.CHECKLIST, {
       ...req.t(TranslationKeys.COMMON, { returnObjects: true }),
       ...req.t(TranslationKeys.CHECKLIST, { returnObjects: true }),
