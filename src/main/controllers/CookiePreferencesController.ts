99c959b429fbfaf80a9816828cc3e1f1ee8f3052
Lahiru Oshara Hinguruduwa <35135059+LahiruOshara@users.noreply.github.com>
2024-12-03 11:41:34 +0000

    cookies page i18next secure flag

--- src/main/controllers/CookiePreferencesController.ts
+++ src/main/controllers/CookiePreferencesController.ts
@@ -2,8 +2,16 @@ import { Request, Response } from 'express';
 
 import { PageUrls, TranslationKeys } from '../definitions/constants';
 
+import { getLanguage } from './helpers/RouterHelpers';
+
 export default class CookiePreferencesController {
   public get(req: Request, res: Response): void {
+    const lang = getLanguage(req.url);
+    res.cookie('i18next', lang, {
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: true,
+    });
     res.render(TranslationKeys.COOKIE_PREFERENCES, {
       ...req.t(TranslationKeys.COMMON, { returnObjects: true }),
       ...req.t(TranslationKeys.COOKIE_PREFERENCES, { returnObjects: true }),
