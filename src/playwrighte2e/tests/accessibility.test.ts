171fd1af626be4822bee40aeae5a5055176b2bed
gokul-sol <gokul.sridharan@HMCTS.NET>
2025-03-18 12:55:50 +0000

    Add accessibility tests

---
+++ src/playwrighte2e/tests/accessibility.test.ts
@@ -0,0 +1,45 @@
+/* eslint-disable jest/no-done-callback,jest/expect-expect */
+
+import { PageUrls } from '../../main/definitions/constants';
+import { Page, test, expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
+
+const envUrl = process.env.TEST_URL || 'https://et-sya.aat.platform.hmcts.net';
+const ignoredPages = ['/pension', '/pay', '/new-job-pay', '/compensation', PageUrls.CITIZEN_HUB];
+
+async function expectNoErrors(page:Page): Promise<void> {
+  const accessibilityScanResults = await new AxeBuilder({ page })
+  .withTags([
+    'wcag2a',
+    'wcag2aa',
+    'wcag21a',
+    'wcag21aa',
+    'wcag22a',
+    'wcag22aa'
+  ])
+  .analyze();
+  const errors = accessibilityScanResults.violations;
+
+  if (errors.length > 0) {
+    const errorsAsJson = `${JSON.stringify(errors, null, 2)}`;
+    console.warn(`There are accessibility issues: \n${errorsAsJson}\n`);
+  }
+
+  expect(errors.length).toBe(0);
+}
+
+function testAccessibility(url: string): void {
+  test(`Page ${url} should have no accessibility errors`, async ({ page }) => {
+    if (!ignoredPages.includes(url)) {
+      const pageUrl = envUrl + url;
+      await page.goto(pageUrl);
+      await expectNoErrors(page);
+    }
+  });
+}
+
+test.describe('SYA Accessibility',{tag: '@Accessibility'}, () => {
+  Object.values({ ...PageUrls, CITIZEN_HUB: '/citizen-hub/a11y' }).forEach(url => {
+    testAccessibility(url);
+  });
+});
