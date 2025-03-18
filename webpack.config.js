d33c17d1457f66053fb31fe99f817b30bee525fc
gokul-sol <gokul.sridharan@HMCTS.NET>
2025-03-18 17:08:31 +0000

    update transpile boolean

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
           {
             loader: 'ts-loader',
             options: {
-              transpileOnly: true,
+              transpileOnly: false,
             },
           },
         ],
