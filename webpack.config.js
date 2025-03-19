d2a3396c7e6e7687aa1ae53252b9816be9bd3b2a
gokul-sol <gokul.sridharan@HMCTS.NET>
2025-03-19 09:25:23 +0000

    suppress webpack error

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
           {
             loader: 'ts-loader',
             options: {
-              transpileOnly: false,
+              transpileOnly: true,
             },
           },
         ],
