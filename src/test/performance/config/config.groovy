4ee70ff0f9680c9f66e628261d8846e9805969eb
Dean Cullen <Dean_cullen@next.co.uk>
2025-05-28 18:11:26 +0100

    Updated config

--- src/test/performance/config/config.groovy
+++ src/test/performance/config/config.groovy
@@ -21,7 +21,7 @@ this.dynatraceMetricIngestEndpoint = "api/v2/metrics/ingest"
 this.dynatraceTriggerSyntheticEndpoint = "api/v2/synthetic/executions/batch"
 this.dynatraceSyntheticPerfTest = "SYNTHETIC_TEST-008CAF328F244320"
 this.dynatraceDashboardId = "a4576442-06a9-4a76-baa5-5342a525679f"
-this.dynatraceEntitySelector = 'type(service),tag(\"[Kubernetes]namespace:et\"),tag(\"Environment:PERF\"),entityId(\"SERVICE-894163B308FBDD78\")'
+this.dynatraceEntitySelector = 'type(service),tag(\\"[Kubernetes]namespace:et\\"),tag(\\"Environment:PERF\\"),entityId(\\"SERVICE-894163B308FBDD78\\")'
 
 //type(service),tag(\\"[Kubernetes]namespace:et\\"),tag(\\"Environment:PERF\\"),entityId(\\"SERVICE-894163B308FBDD78\\")
 echo "Inside the config file: ${dynatraceTriggerSyntheticEndpoint}"
