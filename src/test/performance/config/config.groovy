119e5f622f50c09fd5bdcf06c6f070c3340c8079
Dean Cullen <Dean_cullen@next.co.uk>
2025-06-09 19:29:28 +0100

    Updated config

--- src/test/performance/config/config.groovy
+++ src/test/performance/config/config.groovy
@@ -1,6 +1,6 @@
 // Performance in pipelines 
 
-//Perf Vars (*Need to add to config file)
+//Perf Vars (*Need to add to env vars*)
 this.dynatraceApiHost = "https://yrk32651.live.dynatrace.com/"
 this.dynatraceEventIngestEndpoint = "api/v2/events/ingest"
 this.dynatraceMetricIngestEndpoint = "api/v2/metrics/ingest"
@@ -9,7 +9,7 @@ this.dynatraceUpdateSyntheticEndpoint = "api/v1/synthetic/monitors/"
 this.dynatraceMetricType = 'nodejs'
 this.dynatraceMetricTag = 'namespace:et'
 
-def setEnvironmentConfig(String envName) {
+this.setEnvironmentConfig{ String envName -> 
     if (envName == "preview") {
         this.dynatraceSyntheticPerfTest = "SYNTHETIC_TEST-008CAF328F244320"
         this.dynatraceDashboardId = "a4576442-06a9-4a76-baa5-5342a525679f"
@@ -37,4 +37,5 @@ def setEnvironmentConfig(String envName) {
 //type(service),tag(\\"[Kubernetes]namespace:et\\"),tag(\\"Environment:PERF\\"),entityId(\\"SERVICE-894163B308FBDD78\\")
 echo "Inside the config file: ${dynatraceTriggerSyntheticEndpoint}"
 
+return this
 
