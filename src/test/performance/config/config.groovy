273add511e2dabb0d06673b3aafab7c0e6025e97
Dean Cullen <Dean_cullen@next.co.uk>
2025-06-09 10:47:16 +0100

    Update

--- src/test/performance/config/config.groovy
+++ src/test/performance/config/config.groovy
@@ -6,11 +6,8 @@ this.dynatraceEventIngestEndpoint = "api/v2/events/ingest"
 this.dynatraceMetricIngestEndpoint = "api/v2/metrics/ingest"
 this.dynatraceTriggerSyntheticEndpoint = "api/v2/synthetic/executions/batch"
 this.dynatraceUpdateSyntheticEndpoint = "api/v1/synthetic/monitors/"
-
 this.dynatraceSyntheticPerfTest = "SYNTHETIC_TEST-008CAF328F244320"
 this.dynatraceDashboardId = "a4576442-06a9-4a76-baa5-5342a525679f"
-
-
 this.dynatraceDashboardURL = "https://yrk32651.live.dynatrace.com/#dashboard;id=a4576442-06a9-4a76-baa5-5342a525679f;applyDashboardDefaults=true"
 this.dynatraceEntitySelector = 'type(service),tag(\\"[Kubernetes]namespace:et\\"),tag(\\"Environment:PERF\\"),entityId(\\"SERVICE-894163B308FBDD78\\")'
 this.dynatraceMetricType = 'nodejs'
