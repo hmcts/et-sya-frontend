f5d56e18f929a5086a9116da7f4f463c3ad7c0f8
lahiru.hinguruduwa <oshara19@gmail.com>
2025-06-17 11:48:05 +0100

    ut

--- src/main/controllers/AppointLegalRepController.ts
+++ src/main/controllers/AppointLegalRepController.ts
@@ -9,18 +9,18 @@ export default class AppointLegalRepController {
   public get = async (req: AppRequest, res: Response): Promise<void> => {
     const content = {
       ...req.t(TranslationKeys.COMMON, { returnObjects: true }),
-      ...req.t(TranslationKeys.APPOINT_LEGAL_REPRESENTATIVE_SCREEN, { returnObjects: true }),
     };
 
     const languageParam = getLanguageParam(req.url);
     const userCase = req.session.userCase;
+    const allDocumentsUrl = PageUrls.ALL_DOCUMENTS;
 
     res.render(TranslationKeys.APPOINT_LEGAL_REPRESENTATIVE, {
       ...content,
       currentUrl: '/appoint-legal-representative',
       languageParam,
       userCase,
-      PageUrls,
+      allDocumentsUrl,
     });
   };
 }
