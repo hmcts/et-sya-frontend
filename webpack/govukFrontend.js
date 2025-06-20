0bae5bab7c97fd9a7e5a86bbc0c7054b3a06426e
Mike-Brain <Michael.brain1@justice.gov.uk>
2025-06-20 10:54:20 +0100

    RET-5856
    * upgrade govuk-frontend to the latest version
    * add govukRebrand flag
    * update hmctsProgressBar and add styles manually

--- webpack/govukFrontend.js
+++ webpack/govukFrontend.js
@@ -3,7 +3,7 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const packageJson = path.resolve('node_modules', 'govuk-frontend', 'package.json');
-const root = path.resolve(packageJson, '..', 'govuk');
+const root = path.resolve(packageJson, '..', 'dist', 'govuk');
 const sass = path.resolve(root, 'all.scss');
 const javascript = path.resolve(root, 'all.js');
 const components = path.resolve(root, 'components');
@@ -15,6 +15,7 @@ const copyGovukTemplateAssets = new CopyWebpackPlugin({
   patterns: [
     { from: images, to: 'assets/images' },
     { from: fonts, to: 'assets/fonts' },
+    { from: path.resolve(assets, 'rebrand'), to: 'assets/rebrand' }, // Add this line
   ],
 });
 
