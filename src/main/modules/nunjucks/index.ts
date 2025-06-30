aa314e1586484d1c2f17dbab4dd8440ee819e703
Mike-Brain <Michael.brain1@justice.gov.uk>
2025-06-30 16:27:31 +0100

    RET-5856
    * create and update header component
    * govukRebrand added within nunjucks
    * fixed issue with footer styling

--- src/main/modules/nunjucks/index.ts
+++ src/main/modules/nunjucks/index.ts
@@ -153,6 +153,7 @@ export class Nunjucks {
       nunEnv.addGlobal('dateStringToLocale', (dateToTransform: string) =>
         datesStringToDateInLocale(dateToTransform, req.url)
       );
+      nunEnv.addGlobal('govukRebrand', true);
       next();
     });
   }
--- webpack/govukFrontend.js
+++ webpack/govukFrontend.js
@@ -15,7 +15,7 @@ const copyGovukTemplateAssets = new CopyWebpackPlugin({
   patterns: [
     { from: images, to: 'assets/images' },
     { from: fonts, to: 'assets/fonts' },
-    { from: path.resolve(assets, 'rebrand'), to: 'assets/rebrand' }, // Add this line
+    { from: path.resolve(assets, 'rebrand'), to: 'assets/rebrand' },
   ],
 });
 
