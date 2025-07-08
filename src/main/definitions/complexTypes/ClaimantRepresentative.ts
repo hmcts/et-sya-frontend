a7f6b1de318c097c1bd9e5c78e9f10e9b5c9fd59
lahiru.hinguruduwa <oshara19@gmail.com>
2025-07-08 16:40:07 +0100

    org name fetch issue fix

--- src/main/definitions/complexTypes/ClaimantRepresentative.ts
+++ src/main/definitions/complexTypes/ClaimantRepresentative.ts
@@ -3,4 +3,5 @@ import { Organisation } from './Organisation';
 export interface ClaimantRepresentative {
   name_of_representative?: string;
   myHmctsOrganisation?: Organisation;
+  name_of_organisation?: string;
 }
