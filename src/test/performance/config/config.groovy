2cfd40cc30d05bdc9464852e71e2d73de615227c
Dean Cullen <Dean_cullen@next.co.uk>
2025-07-23 13:18:52 +0100

    Added correct entity selector

--- src/test/performance/config/config.groovy
+++ src/test/performance/config/config.groovy
@@ -14,7 +14,7 @@ this.dynatraceEntitySelector = ""
 this.dynatraceSyntheticTestPreview = "SYNTHETIC_TEST-58D99F542AAB721C"
 this.dynatraceDashboardIdPreview = "a4576442-06a9-4a76-baa5-5342a525679f"
 this.dynatraceDashboardURLPreview = "https://yrk32651.live.dynatrace.com/#dashboard;id=a4576442-06a9-4a76-baa5-5342a525679f;applyDashboardDefaults=true"
-this.dynatraceEntitySelectorPreview = 'type(service),tag(\\"[Kubernetes]namespace:et\\"),tag(\\"Environment:PERF\\"),entityId(\\"SERVICE-894163B308FBDD78\\")'
+this.dynatraceEntitySelectorPreview = 'type(service),tag(\\"[Kubernetes]namespace:et\\"),tag(\\"Environment:PREVIEW\\"),entityId(\\"SERVICE-85A65FBFF3C9F37E\\")'
 //AAT Config
 this.dynatraceSyntheticTestAAT = "SYNTHETIC_TEST-5EEFBE93947768B3"
 this.dynatraceDashboardIdAAT = "a529a685-8c36-4e8c-8137-67de8bfcf104"
