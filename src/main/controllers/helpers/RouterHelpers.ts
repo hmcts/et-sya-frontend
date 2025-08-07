6ed4865810ca130b032fb547b72c82e249d83330
lahiru.hinguruduwa <oshara19@gmail.com>
2025-08-07 09:01:23 +0100

    add new method isValidUrl

--- src/main/controllers/helpers/RouterHelpers.ts
+++ src/main/controllers/helpers/RouterHelpers.ts
@@ -4,8 +4,10 @@ import { Request, Response } from 'express';
 import { LoggerInstance } from 'winston';
 
 import { AppRequest } from '../../definitions/appRequest';
-import { ErrorPages, PageUrls, languages } from '../../definitions/constants';
+import { ErrorPages, LegacyUrls, PageUrls, languages } from '../../definitions/constants';
 import { FormFields } from '../../definitions/form';
+
+import { setUrlLanguage } from './LanguageHelper';
 
 export const handleSaveAsDraft = (res: Response): void => {
   return res.redirect(PageUrls.CLAIM_SAVED);
@@ -26,16 +28,29 @@ export const conditionalRedirect = (
 };
 
 export const returnNextPage = (req: AppRequest, res: Response, redirectUrl: string): void => {
-  return res.redirect(handleReturnUrl(req, redirectUrl));
+  const nextPage = req.session.returnUrl ?? redirectUrl;
+  req.session.returnUrl = undefined;
+  const checkedPage = isValidUrl(nextPage) ? nextPage : setUrlLanguage(req, ErrorPages.NOT_FOUND);
+  return res.redirect(checkedPage);
 };
 
-const handleReturnUrl = (req: AppRequest, redirectUrl: string): string => {
-  let nextPage = redirectUrl;
-  if (req.session.returnUrl) {
-    nextPage = req.session.returnUrl;
-    req.session.returnUrl = undefined;
+const isValidUrl = (url: string): boolean => {
+  const urlStr: string[] = url.split('?');
+  const baseUrl: string = urlStr[0];
+  const legacyUrlValues: string[] = Object.values(LegacyUrls);
+  if (legacyUrlValues.includes(baseUrl) || baseUrl === '/' || baseUrl === '#') {
+    return true;
   }
-  return nextPage;
+  const validUrls = Object.values(PageUrls);
+  for (const validUrl of validUrls) {
+    if (validUrl === '/' || validUrl === '#') {
+      continue;
+    }
+    if (baseUrl.includes(validUrl)) {
+      return true;
+    }
+  }
+  return false;
 };
 
 export const returnValidUrl = (redirectUrl: string, validUrls: string[]): string => {
