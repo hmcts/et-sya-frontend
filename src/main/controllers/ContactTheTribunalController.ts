057785bd7e088b348de78e6b543e9c1db8b82a61
Mike-Brain <Michael.brain1@justice.gov.uk>
2025-10-29 16:03:40 +0000

    Refactor userCase access in ContactTheTribunalController
    Revert changes for AttachmentController as userCase should never be null or undefined, this check should be enough to cover any potential issues

--- src/main/controllers/ContactTheTribunalController.ts
+++ src/main/controllers/ContactTheTribunalController.ts
@@ -18,6 +18,7 @@ export default class ContactTheTribunalController {
     const welshEnabled = await getFlagValue('welsh-language', null);
     const bundlesEnabled = await getFlagValue(FEATURE_FLAGS.BUNDLES, null);
     const DOCUMENTS = 'documents';
+    const { userCase } = req.session;
 
     const translations: AnyRecord = {
       ...req.t(TranslationKeys.CONTACT_THE_TRIBUNAL, { returnObjects: true }),
@@ -26,11 +27,10 @@ export default class ContactTheTribunalController {
     const languageParam = getLanguageParam(req.url);
     let applicationsToDisplay;
 
-    // if bundles not enabled or no hearings in future then remove documents from displayed application types
     const allowBundlesFlow =
       bundlesEnabled &&
-      req.session?.userCase?.hearingCollection?.length &&
-      createRadioBtnsForHearings(req.session.userCase.hearingCollection)?.length;
+      userCase.hearingCollection?.length &&
+      createRadioBtnsForHearings(userCase.hearingCollection)?.length;
 
     if (!allowBundlesFlow) {
       applicationsToDisplay = applications.filter(app => app !== DOCUMENTS);
