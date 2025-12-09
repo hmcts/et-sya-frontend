7716013c0141dac064d670723e92a2f621ef8e19
Lahiru Oshara Hinguruduwa <35135059+LahiruOshara@users.noreply.github.com>
2025-12-09 14:26:53 +0530

    Ret-5801 (#2159)
    * add new method isValidUrl
    
    * fix build
    
    * RET-5801 (#2124)
    
    * add new method isValidUrl
    
    * fix build
    
    * Prevent open redirects to external sites
    
    * Prevent open redirects to external sites
    
    * fix redirect fortify
    
    * fix CaseStateCheck redirect
    
    * unit tests
    
    * fix route test failures
    
    * ut fixes
    
    * Ret 5801 (#2126)
    
    * add new method isValidUrl
    
    * fix build
    
    * unit tests
    
    * fix route test failures
    
    * ut fixes
    
    * test push
    
    * test push
    
    * validate logic modify
    
    * fix complex urls
    
    * remove double validation
    
    * remove double validation
    
    * dynamic url revisit logic
    
    * fix dynamic path issues
    
    * skip dynamic urls
    
    * only static urls
    
    * fix case state check issue
    
    * fix case state check issue
    
    * handle dynamic url patterns
    
    * refactor
    
    * modify validation
    
    * refactor
    
    * add length validation
    
    * remove dynamic validation
    
    * fix case state check
    
    * fix case state check
    
    * add dynamic
    
    * test
    
    * fix test
    
    * fix build
    
    * Fix merge conflict
    
    ---------
    
    Co-authored-by: Harpreet Jhita <harpreet.jhita@justice.gov.uk>

--- src/main/controllers/helpers/RouterHelpers.ts
+++ src/main/controllers/helpers/RouterHelpers.ts
@@ -4,8 +4,18 @@ import { Request, Response } from 'express';
 import { LoggerInstance } from 'winston';
 
 import { AppRequest } from '../../definitions/appRequest';
-import { ErrorPages, PageUrls, languages } from '../../definitions/constants';
+import {
+  DefaultValues,
+  ErrorPages,
+  LegacyUrls,
+  PageUrls,
+  VALID_DYNAMIC_URL_BASES,
+  languages,
+} from '../../definitions/constants';
 import { FormFields } from '../../definitions/form';
+import NumberUtils from '../../utils/NumberUtils';
+import StringUtils from '../../utils/StringUtils';
+import UrlUtils from '../../utils/UrlUtils';
 
 export const handleSaveAsDraft = (res: Response): void => {
   return res.redirect(PageUrls.CLAIM_SAVED);
@@ -25,32 +35,78 @@ export const conditionalRedirect = (
   return matchingValues?.some(v => v === condition);
 };
 
+// Main function to redirect to the next page
 export const returnNextPage = (req: AppRequest, res: Response, redirectUrl: string): void => {
-  return res.redirect(handleReturnUrl(req, redirectUrl));
-};
-
-const handleReturnUrl = (req: AppRequest, redirectUrl: string): string => {
   let nextPage = redirectUrl;
   if (req.session.returnUrl) {
     nextPage = req.session.returnUrl;
     req.session.returnUrl = undefined;
   }
-  return nextPage;
+  return res.redirect(returnValidUrl(nextPage));
 };
 
-export const returnValidUrl = (redirectUrl: string, validUrls: string[]): string => {
-  for (const url of validUrls) {
-    const welshUrl = url + languages.WELSH_URL_PARAMETER;
-    const englishUrl = url + languages.ENGLISH_URL_PARAMETER;
-    if (redirectUrl === url) {
-      return url;
-    } else if (redirectUrl === welshUrl) {
-      return welshUrl;
-    } else if (redirectUrl === englishUrl) {
-      return englishUrl;
+export const returnValidUrl = (redirectUrl: string, validUrls?: string[]): string => {
+  validUrls = validUrls ?? Object.values(PageUrls);
+  validUrls.push(LegacyUrls.ET1, LegacyUrls.ET1_BASE);
+
+  const urlStr = redirectUrl.split('?');
+  const baseUrl = urlStr[0];
+
+  // Check static URLs
+  for (let validUrl of validUrls) {
+    if (baseUrl === validUrl) {
+      const parameters = UrlUtils.getRequestParamsFromUrl(redirectUrl);
+      for (const param of parameters) {
+        if (param !== DefaultValues.CLEAR_SELECTION_URL_PARAMETER) {
+          validUrl = addParameterToUrl(validUrl, param);
+        }
+      }
+      return validUrl;
     }
   }
+
+  // Check dynamic patterns
+  const urlParts = baseUrl.split('/');
+  let returnUrl = '';
+  for (const urlPart of urlParts) {
+    const matchedUrlPart = VALID_DYNAMIC_URL_BASES.find(url => url === urlPart);
+    if (matchedUrlPart) {
+      returnUrl += `/${matchedUrlPart}`;
+    } else {
+      if (NumberUtils.isNumericValue(urlPart) && urlPart.length <= 20) {
+        returnUrl += `/${urlPart}`;
+      }
+    }
+  }
+
+  if (returnUrl) {
+    const parameters = UrlUtils.getRequestParamsFromUrl(redirectUrl);
+    for (const param of parameters) {
+      if (param !== DefaultValues.CLEAR_SELECTION_URL_PARAMETER) {
+        returnUrl = addParameterToUrl(returnUrl, param);
+      }
+    }
+    return returnUrl;
+  }
+
   return ErrorPages.NOT_FOUND;
+};
+
+export const addParameterToUrl = (url: string, parameter: string): string => {
+  if (StringUtils.isBlank(url)) {
+    return DefaultValues.STRING_EMPTY;
+  }
+  if (StringUtils.isBlank(parameter)) {
+    return url;
+  }
+  if (!url.includes(parameter)) {
+    if (url.includes(DefaultValues.STRING_QUESTION_MARK)) {
+      url = url + DefaultValues.STRING_AMPERSAND + parameter;
+    } else {
+      url = url + DefaultValues.STRING_QUESTION_MARK + parameter;
+    }
+  }
+  return url;
 };
 
 export const validateLanguageParam = (lng: string): boolean => {
