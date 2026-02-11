1e89c6b80379e338789821b0e2e6f8c034321052
lahiru.hinguruduwa <oshara19@gmail.com>
2026-02-11 17:01:54 +0000

    change text capitalisation

--- src/test/end-to-end/pages/claimantApplications/claimantApplications.js
+++ src/test/end-to-end/pages/claimantApplications/claimantApplications.js
@@ -3,10 +3,10 @@
 module.exports = async function () {
   // eslint-disable-next-line @typescript-eslint/no-this-alias
   const I = this;
-  I.see('ET1 Claims');
+  I.see('ET1 claims');
   I.see('Start a new claim');
-  I.see('Draft Claims');
+  I.see('Draft claims');
   I.see("For privacy purposes, a draft claim will be deleted 12 months after the date it's created.");
-  I.see('Submitted Claims');
+  I.see('Submitted claims');
   I.click("//table[2]//tr[1]//a[.='View']");
 };
