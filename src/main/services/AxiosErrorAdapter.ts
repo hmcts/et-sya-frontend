272483b9c3b9745d7bceb847be83e397b7581df3
Mike-Brain <Michael.brain1@justice.gov.uk>
2026-02-19 12:42:51 +0000

    Update error handling in AxiosErrorAdapter and modify response messages in your-details-cya.json

--- src/main/services/AxiosErrorAdapter.ts
+++ src/main/services/AxiosErrorAdapter.ts
@@ -2,12 +2,15 @@ import { AxiosError } from 'axios';
 
 // Get details from an axios error
 export const axiosErrorDetails = (
-  axiosError: AxiosError<{ error: string }>,
+  axiosError: AxiosError<{ error: string; message: string }>,
   context?: { action?: string; caseId?: string; [key: string]: any }
 ): string => {
   let errorMessage = axiosError.message;
   if (axiosError.response?.data?.error) {
     errorMessage += `, ${axiosError.response.data.error}`;
+  }
+  if (axiosError.response?.data?.message) {
+    errorMessage += `, ${axiosError.response.data.message}`;
   }
   if (context) {
     const contextStr = Object.entries(context)
