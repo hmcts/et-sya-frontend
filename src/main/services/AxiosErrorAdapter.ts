3d84d061e53d4236b7e8ccb56bbe42322e855507
Mike-Brain <Michael.brain1@justice.gov.uk>
2026-02-19 16:13:49 +0000

    Update axiosErrorDetails to include trace in error handling

--- src/main/services/AxiosErrorAdapter.ts
+++ src/main/services/AxiosErrorAdapter.ts
@@ -2,15 +2,15 @@ import { AxiosError } from 'axios';
 
 // Get details from an axios error
 export const axiosErrorDetails = (
-  axiosError: AxiosError<{ error: string; message: string }>,
+  axiosError: AxiosError<{ error: string; message: string; trace: string }>,
   context?: { action?: string; caseId?: string; [key: string]: any }
 ): string => {
   let errorMessage = axiosError.message;
   if (axiosError.response?.data?.error) {
-    errorMessage += `, ${axiosError.response.data.error}`;
-  }
-  if (axiosError.response?.data?.message) {
-    errorMessage += `, ${axiosError.response.data.message}`;
+    const errorDetail: string = axiosError.response.data.message
+      ? axiosError.response.data.message
+      : axiosError.response.data.trace;
+    errorMessage += `, ${errorDetail}`;
   }
   if (context) {
     const contextStr = Object.entries(context)
