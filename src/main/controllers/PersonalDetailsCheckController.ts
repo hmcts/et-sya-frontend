d6f55d88e2b05ee308f5e85403cd0ad5a3420b05
lahiru.hinguruduwa <oshara19@gmail.com>
2026-05-05 15:43:59 +0100

    completing the section and mandatory questions

--- src/main/controllers/PersonalDetailsCheckController.ts
+++ src/main/controllers/PersonalDetailsCheckController.ts
@@ -9,50 +9,63 @@ import { TranslationKeys } from '../definitions/constants';
 import { FormContent, FormFields } from '../definitions/form';
 import { DefaultRadioFormFields, saveForLaterButton, submitButton } from '../definitions/radios';
 import { AnyRecord } from '../definitions/util-types';
-import { getLogger } from '../logger';
+import { Logger, getLogger } from '../logger';
 
 import { handlePostLogic } from './helpers/CaseHelpers';
 import { assignFormData, getPageContent } from './helpers/FormHelpers';
 import { getClaimStepsUrl } from './helpers/RouterHelpers';
 
-const logger = getLogger('PersonalDetailsCheckController');
-
 export default class PersonalDetailsCheckController {
-  private readonly form: Form;
-  private readonly personalDetailsCheckContent: FormContent = {
-    fields: {
-      personalDetailsCheck: {
-        ...DefaultRadioFormFields,
-        label: (l: AnyRecord): string => l.legend,
-        labelHidden: false,
-        labelSize: 'xl',
-        isPageHeading: true,
-        hint: (l: AnyRecord): string => l.hint,
-        id: 'tasklist-check',
-        classes: 'govuk-radios',
-        validator: isFieldFilledIn,
-      },
-    },
-    submit: submitButton,
-    saveForLater: saveForLaterButton,
-  };
+  private readonly checkFieldName: string;
+  private readonly checkTranslationKey: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private readonly validateFn: (userCase: Record<string, any>) => boolean;
+  private readonly getNextPageFn: (req: AppRequest) => string;
+  private readonly logger: Logger;
+  protected readonly form: Form;
+  protected readonly formContent: FormContent;
 
-  constructor() {
-    this.form = new Form(<FormFields>this.personalDetailsCheckContent.fields);
+  constructor(
+    checkFieldName = 'personalDetailsCheck',
+    checkTranslationKey: string = TranslationKeys.TASK_LIST_CHECK,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    validateFn: (userCase: Record<string, any>) => boolean = validatePersonalDetails,
+    getNextPageFn: (req: AppRequest) => string = getClaimStepsUrl,
+    loggerName = 'PersonalDetailsCheckController'
+  ) {
+    this.checkFieldName = checkFieldName;
+    this.checkTranslationKey = checkTranslationKey;
+    this.validateFn = validateFn;
+    this.getNextPageFn = getNextPageFn;
+    this.logger = getLogger(loggerName);
+    this.formContent = {
+      fields: {
+        [checkFieldName]: {
+          ...DefaultRadioFormFields,
+          label: (l: AnyRecord): string => l.legend,
+          labelHidden: false,
+          labelSize: 'xl',
+          isPageHeading: true,
+          hint: (l: AnyRecord): string => l.hint,
+          id: 'tasklist-check',
+          classes: 'govuk-radios',
+          validator: isFieldFilledIn,
+        },
+      },
+      submit: submitButton,
+      saveForLater: saveForLaterButton,
+    };
+    this.form = new Form(<FormFields>this.formContent.fields);
   }
 
   public post = async (req: AppRequest, res: Response): Promise<void> => {
-    if (req.body?.personalDetailsCheck === 'Yes') {
-      const userCase = req.session?.userCase;
-      const isValid = validatePersonalDetails(userCase);
+    if (req.body?.[this.checkFieldName] === 'Yes') {
+      const isValid = this.validateFn(req.session?.userCase);
 
       req.session.errors = [];
       if (!isValid) {
-        req.session.errors.push({ propertyName: 'personalDetailsCheck', errorType: 'invalid' });
-        const content = getPageContent(req, this.personalDetailsCheckContent, [
-          TranslationKeys.COMMON,
-          TranslationKeys.TASK_LIST_CHECK,
-        ]);
+        req.session.errors.push({ propertyName: this.checkFieldName, errorType: 'invalid' });
+        const content = getPageContent(req, this.formContent, [TranslationKeys.COMMON, this.checkTranslationKey]);
 
         return res.render(TranslationKeys.TASK_LIST_CHECK, {
           ...content,
@@ -60,15 +73,12 @@ export default class PersonalDetailsCheckController {
       }
     }
 
-    await handlePostLogic(req, res, this.form, logger, getClaimStepsUrl(req));
+    await handlePostLogic(req, res, this.form, this.logger, this.getNextPageFn(req));
   };
 
   @CaseStateCheck()
   public get = (req: AppRequest, res: Response): void => {
-    const content = getPageContent(req, this.personalDetailsCheckContent, [
-      TranslationKeys.COMMON,
-      TranslationKeys.TASK_LIST_CHECK,
-    ]);
+    const content = getPageContent(req, this.formContent, [TranslationKeys.COMMON, this.checkTranslationKey]);
     assignFormData(req.session.userCase, this.form.getFormFields());
     res.render(TranslationKeys.TASK_LIST_CHECK, {
       ...content,
