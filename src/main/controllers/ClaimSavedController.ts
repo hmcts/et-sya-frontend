d11e044d8567ead31c5a8121035f073a96d1708b
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2026-05-28 15:35:00 +0100

    Update ClaimSavedController.ts

--- src/main/controllers/ClaimSavedController.ts
+++ src/main/controllers/ClaimSavedController.ts
@@ -8,6 +8,7 @@ import { getLanguageParam } from './helpers/RouterHelpers';
 export default class ClaimSavedController {
   public get(req: AppRequest, res: Response): void {
     res.render('claim-saved', {
+      ...req.t('common', { returnObjects: true }),
       ...req.t('claim-saved', { returnObjects: true }),
       PageUrls,
       languageParam: getLanguageParam(req.url),
