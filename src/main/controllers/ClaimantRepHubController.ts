ece8920b43be8e66cd8a877e1d4314e577fc5a82
Lahiru Oshara Hinguruduwa <35135059+LahiruOshara@users.noreply.github.com>
2026-06-04 09:05:37 +0100

    qa fixes 2 (#2471)
    * ret-6371 content update
    
    * discrimination type page link fix
    
    * RET-6362 content update
    
    * RET-6363 content update
    
    * RET-6365 content update
    
    * RET-6367 content update
    
    * RET-6466 content update
    
    * RET-6467 content update
    
    * RET-6470 content update
    
    * RET-6470 content update
    
    * address comments
    
    * address comments
    
    * address comments
    
    * address comments

--- src/main/controllers/ClaimantRepHubController.ts
+++ src/main/controllers/ClaimantRepHubController.ts
@@ -1,6 +1,7 @@
 import { Response } from 'express';
 
 import { AppRequest } from '../definitions/appRequest';
+import { YesOrNo } from '../definitions/case';
 import { PageUrls, TranslationKeys } from '../definitions/constants';
 import {
   HubLinkNames,
@@ -14,21 +15,55 @@ import { fromApiFormat } from '../helper/ApiFormatter';
 import { getLogger } from '../logger';
 import { getCaseApi } from '../services/CaseService';
 
-import { handleUpdateHubLinksStatuses } from './helpers/CaseHelpers';
+import { getApplicationsWithTribunalOrderOrRequest } from './helpers/AdminNotificationHelper';
 import {
+  clearPrepareDocumentsForHearingFields,
+  clearTseFields,
+  handleUpdateHubLinksStatuses,
+} from './helpers/CaseHelpers';
+import {
+  activateRespondentApplicationsLink,
+  checkIfRespondentIsSystemUser,
   getAcknowledgementAlert,
+  getClaimantAppsAndUpdateStatusTag,
   getHubLinksUrlMap,
+  getStoredPendingBannerList,
   shouldHubLinkBeClickable,
+  shouldShowClaimantTribunalResponseReceived,
+  shouldShowJudgmentReceived,
   shouldShowRejectionAlert,
+  shouldShowRespondentAcknowledgement,
+  shouldShowRespondentApplicationReceived,
+  shouldShowRespondentRejection,
+  shouldShowRespondentResponseReceived,
   shouldShowSubmittedAlert,
   updateHubLinkStatuses,
+  userCaseContainsGeneralCorrespondence,
 } from './helpers/CitizenHubHelper';
 import { getProgressBarItems } from './helpers/CitizenHubProgressBarHelper';
+import { isClaimantRepresentedByOrganisation } from './helpers/ContactTheTribunalHelper';
+import { shouldShowHearingBanner } from './helpers/HearingHelpers';
+import {
+  activateJudgmentsLink,
+  getAllAppsWithDecisions,
+  getDecisionBannerContent,
+  getDecisions,
+  getJudgmentBannerContent,
+  getJudgments,
+} from './helpers/JudgmentHelpers';
 import { getLanguageParam } from './helpers/RouterHelpers';
+import {
+  activateTribunalOrdersAndRequestsLink,
+  filterOutSpecialNotifications,
+  getClaimantTribunalResponseBannerContent,
+  setNotificationBannerData,
+  shouldShowNotificationsBanner,
+} from './helpers/TribunalOrderOrRequestDetailsHelper';
+import { getRespondentApplications, getRespondentBannerContent } from './helpers/TseRespondentApplicationHelpers';
+import { getMultiplePanelData, showMutipleData } from './helpers/multiples/MultiplePanelHelper';
 
 const logger = getLogger('ClaimantRepHubController');
 
-// Rep hub has an additional "About you" section at the front
 const repSectionIndexToLinkNames: HubLinkNames[][] = [...sectionIndexToLinkNames];
 
 export default class ClaimantRepHubController {
@@ -45,8 +80,17 @@ export default class ClaimantRepHubController {
 
     const userCase = req.session.userCase;
     const languageParam = getLanguageParam(req.url);
+    const claimantRepresentedByOrganisation = isClaimantRepresentedByOrganisation(userCase);
+
+    clearTseFields(userCase);
+    clearPrepareDocumentsForHearingFields(userCase);
+    req.session.documentDownloadPage = undefined;
+
+    const sendNotificationCollection = userCase?.sendNotificationCollection;
 
     const translations: AnyRecord = {
+      ...req.t(TranslationKeys.RESPONDENT_APPLICATION_DETAILS, { returnObjects: true }),
+      ...req.t(TranslationKeys.YOUR_APPLICATIONS, { returnObjects: true }),
       ...req.t(TranslationKeys.COMMON, { returnObjects: true }),
       ...req.t(TranslationKeys.CITIZEN_HUB, { returnObjects: true }),
       ...req.t(TranslationKeys.CLAIMANT_REP_HUB, { returnObjects: true }),
@@ -58,7 +102,38 @@ export default class ClaimantRepHubController {
     }
 
     const hubLinksStatuses = userCase.hubLinksStatuses;
+
+    getClaimantAppsAndUpdateStatusTag(userCase);
+
+    if (
+      userCase?.documentCollection?.length ||
+      userCaseContainsGeneralCorrespondence(userCase.sendNotificationCollection)
+    ) {
+      await handleUpdateHubLinksStatuses(req, logger);
+    }
+
+    const allApplications = userCase?.genericTseApplicationCollection;
+    const respondentApplications = getRespondentApplications(userCase);
+    activateRespondentApplicationsLink(respondentApplications, userCase);
+
+    let decisions = undefined;
+    let appsAndDecisions = undefined;
+    if (allApplications?.filter(it => it.value.adminDecision?.length)) {
+      decisions = getDecisions(userCase);
+      appsAndDecisions = getAllAppsWithDecisions(userCase);
+    }
+
+    let judgments = undefined;
+    if (userCase?.sendNotificationCollection?.length) {
+      judgments = getJudgments(userCase);
+    }
+
+    activateJudgmentsLink(judgments, decisions, req);
+    await activateTribunalOrdersAndRequestsLink(sendNotificationCollection, req.session?.userCase);
+
     updateHubLinkStatuses(userCase, hubLinksStatuses);
+
+    const isRespondentSystemUser = checkIfRespondentIsSystemUser(userCase);
 
     const sections = repSectionIndexToLinkNames.map((linkNames, index) => ({
       title: (l: AnyRecord): string => l[`section${index + 1}`],
@@ -68,13 +143,12 @@ export default class ClaimantRepHubController {
           linkTxt: (l: AnyRecord): string => l[linkName],
           status: (l: AnyRecord): string => l[status],
           shouldShow: shouldHubLinkBeClickable(status, linkName),
-          url: () => getHubLinksUrlMap(false, languageParam).get(linkName),
+          url: () => getHubLinksUrlMap(isRespondentSystemUser, languageParam).get(linkName),
           statusColor: () => statusColorMap.get(status),
         };
       }),
     }));
 
-    // Prepend the "About you" section (static, not yet available)
     const aboutYouSection = {
       title: (l: AnyRecord): string => l.sectionAboutYou,
       links: [
@@ -88,7 +162,6 @@ export default class ClaimantRepHubController {
       ],
     };
 
-    // Prepend "View claimant contact details" to The Claim section (index 0 → section1 in repSections)
     sections[0].links.push({
       linkTxt: (l: AnyRecord): string => l.viewClaimantContactDetails,
       status: (l: AnyRecord): string => l[HubLinkStatus.READY_TO_VIEW],
@@ -99,16 +172,70 @@ export default class ClaimantRepHubController {
 
     const allSections = [aboutYouSection, ...sections];
 
+    const notifications = setNotificationBannerData(userCase?.sendNotificationCollection, req.url);
+    const generalNotifications = filterOutSpecialNotifications(notifications);
+
+    const respAppsReceived = shouldShowRespondentApplicationReceived(allApplications);
+    let respondentBannerContent = undefined;
+    if (respAppsReceived) {
+      respondentBannerContent = getRespondentBannerContent(respondentApplications, translations, languageParam);
+    }
+
+    let judgmentBannerContent = undefined;
+    let decisionBannerContent = undefined;
+    const claimantTribunalResponseBannerContent = getClaimantTribunalResponseBannerContent(
+      notifications,
+      languageParam
+    );
+
+    if (userCase.hubLinksStatuses[HubLinkNames.TribunalJudgements] !== HubLinkStatus.NOT_YET_AVAILABLE) {
+      judgmentBannerContent = getJudgmentBannerContent(judgments, languageParam);
+      decisionBannerContent = getDecisionBannerContent(appsAndDecisions, translations, languageParam);
+    }
+
+    const showMultipleData = await showMutipleData(userCase);
+    const showNoLongerRepresentedNotification = userCase?.claimantRepresentativeRemoved === YesOrNo.YES;
+
     res.render(TranslationKeys.CLAIMANT_REP_HUB, {
-      ...translations,
+      ...req.t(TranslationKeys.COMMON, { returnObjects: true }),
+      ...req.t(TranslationKeys.CITIZEN_HUB, { returnObjects: true }),
+      ...req.t(TranslationKeys.SIDEBAR_CONTACT_US, { returnObjects: true }),
+      ...req.t(TranslationKeys.CLAIMANT_REP_HUB, { returnObjects: true }),
       PageUrls,
       userCase,
-      languageParam,
       progressBarItems: getProgressBarItems(userCase, translations, req.url),
       sections: allSections,
+      respondentBannerContent,
+      judgmentBannerContent,
+      decisionBannerContent,
+      claimantTribunalResponseBannerContent,
+      hideContactUs: true,
       showSubmittedAlert: shouldShowSubmittedAlert(userCase),
+      claimantRepresented: userCase.claimantRepresentative,
       showAcknowledgementAlert: getAcknowledgementAlert(userCase, hubLinksStatuses),
       showRejectionAlert: shouldShowRejectionAlert(userCase, hubLinksStatuses),
+      showRespondentResponseReceived: shouldShowRespondentResponseReceived(allApplications),
+      showClaimantTribunalResponseReceived: shouldShowClaimantTribunalResponseReceived(notifications),
+      showRespondentApplicationReceived: respAppsReceived,
+      showRespondentRejection: shouldShowRespondentRejection(userCase, hubLinksStatuses),
+      showRespondentAcknowledgement: shouldShowRespondentAcknowledgement(userCase, hubLinksStatuses),
+      showJudgmentReceived: shouldShowJudgmentReceived(userCase, hubLinksStatuses),
+      showNotificationsBanner: shouldShowNotificationsBanner(generalNotifications),
+      respondentIsSystemUser: isRespondentSystemUser,
+      adminNotifications: getApplicationsWithTribunalOrderOrRequest(allApplications, translations, languageParam),
+      storedPendingApplication: getStoredPendingBannerList(
+        userCase.tseApplicationStoredCollection,
+        allApplications,
+        notifications,
+        languageParam
+      ),
+      showHearingBanner: shouldShowHearingBanner(userCase?.sendNotificationCollection),
+      notifications: generalNotifications,
+      languageParam,
+      showMultipleData,
+      multiplePanelData: await getMultiplePanelData(userCase, translations, showMultipleData),
+      showNoLongerRepresentedNotification,
+      claimantRepresentedByOrganisation,
     });
   };
 }
